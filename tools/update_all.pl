62a4c528a49147cfe13203cb6a715ee8814d9339
Craig Knudsen <craig@k5n.us>
2002-08-13 15:58:47 +0000

    Initial version.

---
+++ tools/update_all.pl
@@ -0,0 +1,22 @@
+#!/usr/local/bin/perl
+#
+# Update each translation file using update_translation.pl.
+#
+#######################################################################
+
+$transdir = "../translations";
+
+opendir ( DIR, $transdir ) || die "Error opening $transdir: $!";
+@files = readdir ( DIR );
+closedir ( DIR );
+
+# ignore everything except .txt files
+@files = grep ( /.txt$/, @files );
+
+foreach $f ( @files ) {
+  print "update_translation.pl $f\n";
+  print `perl update_translation.pl $f`;
+}
+print "Done.\n";
+exit 0;
+
