254fc0188497869d65de98889f4f57c99a02001a
Craig Knudsen <craig@k5n.us>
2002-08-13 15:46:25 +0000

    New files...

---
+++ tools/translation_summary.pl
@@ -0,0 +1,36 @@
+#!/usr/local/bin/perl
+#
+# Examine all translation files to create a report that shows how
+# many translations are missing from each translation file.
+#
+#######################################################################
+
+$inc_dir = "../includes";
+
+$transdir = "../translations";
+
+opendir ( DIR, $transdir ) || die "Error opening $transdir: $!";
+@files = readdir ( DIR );
+closedir ( DIR );
+
+# ignore everything except .txt files
+@files = grep ( /.txt$/, @files );
+
+# header
+printf "%-20s %s\n", "Language file", "No. missing translations";
+
+foreach $f ( @files ) {
+  $out = `perl check_translation.pl ../translations/$f`;
+  if ( $out =~ / missing./ ) {
+    # missing some translations
+    @lines = split ( /\n/, $out );
+    ( $l ) = grep ( / translation.s. missing/, @lines );
+    if ( $l =~ /^(\d+) / ) {
+      printf "%-20s %d\n", $f . ":", $1;
+    }
+  } else {
+    # all translations found :-)
+    printf "%-20s %s\n", $f . ":", "Complete";
+  }
+}
+
