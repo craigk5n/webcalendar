70e0491da82cc3ee6dcbeb648ebeff3c3fc93eac
Craig Knudsen <craig@k5n.us>
2002-12-13 04:03:46 +0000

    Renamed timebar to "Week (Timebar)"

--- views_edit.php
+++ views_edit.php
@@ -84,10 +84,10 @@ if ( $newview ) {
         <?php etranslate("Week (Users horizontal)"); ?>
       <OPTION VALUE="V" <?php if ( $viewtype == "V" ) echo "SELECTED";?> >
         <?php etranslate("Week (Users vertical)"); ?>
+      <OPTION VALUE="T" <?php if ( $viewtype == "T" ) echo "SELECTED";?> >
+        <?php etranslate("Week (Timebar)"); ?>
       <OPTION VALUE="M" <?php if ( $viewtype == "M" ) echo "SELECTED";?> >
         <?php etranslate("Month"); ?>
-      <OPTION VALUE="T" <?php if ( $viewtype == "T" ) echo "SELECTED";?> >
-        <?php etranslate("Timebar"); ?>
       </SELECT>
       </TD></TR>
 <TR><TD VALIGN="top">
