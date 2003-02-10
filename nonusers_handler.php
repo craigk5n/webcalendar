ac17714ee5c0577a0310ba25144ba3106a54e96d
jhoov <jhoov>
2003-02-10 22:07:04 +0000

    Needed to delete more than just webcal_nonuser_cals.

--- nonusers_handler.php
+++ nonusers_handler.php
@@ -12,9 +12,48 @@ if ( ! $is_admin ) {
 $error = "";
 
 if ( $action == "Delete" || $action == translate ("Delete") ) {
- // delete this nonuser calendar
-    if ( ! dbi_query ( "DELETE FROM webcal_nonuser_cals " .
-     "WHERE cal_login = '$id'" ) )
+  // delete this nonuser calendar
+  $user = $id;
+
+  // Get event ids for all events this user is a participant
+  $events = array ();
+  $res = dbi_query ( "SELECT webcal_entry.cal_id " .
+    "FROM webcal_entry, webcal_entry_user " .
+    "WHERE webcal_entry.cal_id = webcal_entry_user.cal_id " .
+    "AND webcal_entry_user.cal_login = '$user'" );
+  if ( $res ) {
+    while ( $row = dbi_fetch_row ( $res ) ) {
+      $events[] = $row[0];
+    }
+  }
+
+  // Now count number of participants in each event...
+  // If just 1, then save id to be deleted
+  $delete_em = array ();
+  for ( $i = 0; $i < count ( $events ); $i++ ) {
+    $res = dbi_query ( "SELECT COUNT(*) FROM webcal_entry_user " .
+      "WHERE cal_id = " . $events[$i] );
+    if ( $res ) {
+      if ( $row = dbi_fetch_row ( $res ) ) {
+        if ( $row[0] == 1 )
+	  $delete_em[] = $events[$i];
+      }
+      dbi_free_result ( $res );
+    }
+  }
+  // Now delete events that were just for this user
+  for ( $i = 0; $i < count ( $delete_em ); $i++ ) {
+    dbi_query ( "DELETE FROM webcal_entry WHERE cal_id = " . $delete_em[$i] );
+  }
+
+  // Delete user participation from events
+  dbi_query ( "DELETE FROM webcal_entry_user WHERE cal_login = '$user'" );
+
+  // Delete any layers other users may have that point to this user.
+  dbi_query ( "DELETE FROM webcal_user_layers WHERE cal_layeruser = '$user'" );
+
+  // Delete user
+  if ( ! dbi_query ( "DELETE FROM webcal_nonuser_cals WHERE cal_login = '$user'" ) )
      $error = translate ("Database error") . ": " . dbi_error();
 
 } else {
