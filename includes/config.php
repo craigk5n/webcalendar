17ec5ff8d7662c091d83df29dc1aef843ea2765e
cknudsen <cknudsen>
2003-06-13 19:32:08 +0000

    Updated version.

--- includes/config.php
+++ includes/config.php
@@ -12,7 +12,7 @@
 //
 /////////////////////////////////////////////////////////////////
 
-$PROGRAM_NAME = "WebCalendar v0.9.40 (30 Nov 2002)";
+$PROGRAM_NAME = "WebCalendar v0.9.41dev-cvs (13 Jun 2003)";
 $PROGRAM_URL = "http://webcalendar.sourceforge.net/";
 
 // MySQL example
