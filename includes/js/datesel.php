c21c388886da8fa0a3191b5a1129f7416203341a
jhoov <jhoov>
2003-10-23 17:47:21 +0000

    Fixed Bug 826908: Dateselect didn't work

--- includes/js/datesel.php
+++ includes/js/datesel.php
@@ -1,8 +1,5 @@
 <?php
-$form = $_GET['form'];
-$month = $_GET['month'];
-$day = $_GET['day'];
-$year = $_GET['year'];
+global $form, $fmonth, $fday, $fyear;
 ?>
 
 <SCRIPT LANGUAGE="JavaScript">
@@ -10,11 +7,11 @@ function sendDate ( date ) {
   year = date.substring ( 0, 4 );
   month = date.substring ( 4, 6 );
   day = date.substring ( 6, 8 );
-  window.opener.document.<?php echo $form?>.<?php echo $day?>.selectedIndex = day - 1;
-  window.opener.document.<?php echo $form?>.<?php echo $month?>.selectedIndex = month - 1;
-  for ( i = 0; i < window.opener.document.<?php echo $form?>.<?php echo $year?>.length; i++ ) {
-    if ( window.opener.document.<?php echo $form?>.<?php echo $year?>.options[i].value == year ) {
-      window.opener.document.<?php echo $form?>.<?php echo $year?>.selectedIndex = i;
+  window.opener.document.<?php echo $form?>.<?php echo $fday?>.selectedIndex = day - 1;
+  window.opener.document.<?php echo $form?>.<?php echo $fmonth?>.selectedIndex = month - 1;
+  for ( i = 0; i < window.opener.document.<?php echo $form?>.<?php echo $fyear?>.length; i++ ) {
+    if ( window.opener.document.<?php echo $form?>.<?php echo $fyear?>.options[i].value == year ) {
+      window.opener.document.<?php echo $form?>.<?php echo $fyear?>.selectedIndex = i;
     }
   }
   window.close ();
