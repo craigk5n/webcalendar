e63e3b5a8059a97bc205f0cdf95204cd3532cd22
jhoov <jhoov>
2003-11-18 15:53:06 +0000

    Fixed bug 842402: group select offset wrong when nonusers listed at top

--- usersel.php
+++ usersel.php
@@ -61,6 +61,11 @@ print_header($INC);
 <?php
 
 $users = get_my_users ();
+if ($nonuser_enabled == "Y" ) {
+  $nonusers = get_nonuser_cals ();
+  $users = ($nonuser_at_top == "Y") ? array_merge($nonusers, $users) : array_merge($users, $nonusers);
+}
+
 for ( $i = 0; $i < count ( $users ); $i++ ) {
   $u = $users[$i]['cal_login'];
   echo "<OPTION VALUE=\"$u\" ";
