dfa917090e8fdf72a369e6c70b06346049463c1d
musus <musus>
2004-04-02 13:21:45 +0000

    transition to xHTML compliance.  making tags & attributes lowercase.

--- assistant_edit.php
+++ assistant_edit.php
@@ -12,21 +12,21 @@ print_header($INC);
 
 <FORM ACTION="assistant_edit_handler.php" METHOD="POST" NAME="editentryform">
 <?php
-  if ($user) echo "<input type=\"hidden\" name=\"user\" value=\"$user\">";
+  if ($user) echo "<input type=\"hidden\" name=\"user\" value=\"$user\" />";
   if ( $is_nonuser_admin ) {
     nonuser_load_variables ( $user, "nonuser" );
-    echo "<H2><FONT COLOR=\"$H2COLOR\">" . $nonuserfullname . " " . translate("Assistants")
-        ."<BR>\n<B>-- " . translate("Admin mode") . " --</B></FONT></H2>\n";
+    echo "<h2><font color=\"$H2COLOR\">" . $nonuserfullname . " " . translate("Assistants")
+        ."<br />\n<b>-- " . translate("Admin mode") . " --</b></font></h2>\n";
   } else {
-    echo "<H2><FONT COLOR=\"$H2COLOR\">" . translate("Your assistants") . "</FONT></H2>\n";
+    echo "<h2><font color=\"$H2COLOR\">" . translate("Your assistants") . "</font></h2>\n";
   }
 ?>
 
-<TABLE BORDER="0">
-<TR><TD VALIGN="top">
-<B><?php etranslate("Assistants"); ?>:</B></TD>
-<TD>
-<SELECT NAME="users[]" SIZE="10" MULTIPLE>
+<table border="0">
+<tr><td valign="top">
+<b><?php etranslate("Assistants"); ?>:</b></td>
+<td>
+<select name="users[]" size="10" MULTIPLE="MULTIPLE">
 <?php
   // get list of all users
   $users = get_my_users ();
@@ -43,33 +43,33 @@ print_header($INC);
     $u = $users[$i]['cal_login'];
     if ($u == $login ) continue;
     if ($u == '__public__' ) continue;
-    echo "<OPTION VALUE=\"$u\" ";
+    echo "<option value=\"$u\" ";
     if ( ! empty ( $assistantuser[$u] ) ) {
-      echo "SELECTED";
+      echo "SELECTED=\"SELECTED\"";
     }
     echo "> " . $users[$i]['cal_fullname'];
   }
 ?>
-</SELECT>
+</select>
 <?php
 if ( $groups_enabled == "Y" ) {
-  echo "<INPUT TYPE=\"button\" ONCLICK=\"selectUsers()\" VALUE=\"" .
+  echo "<input type=\"button\" onclick=\"selectUsers()\" value=\"" .
     translate("Select") . "...\">";
 }
-echo "</TD></TR>\n";
+echo "</td></tr>\n";
 ?>
-</TD></TR>
-<TR><TD COLSPAN="2">
-<BR><BR>
-<CENTER>
-<INPUT TYPE="submit" NAME="action" VALUE="<?php etranslate("Save"); ?>" >
+</td></tr>
+<tr><td colspan="2">
+<br /><br />
+<center>
+<input type="submit" name="action" value="<?php etranslate("Save"); ?>" />
 
-</CENTER>
-</TD></TR>
-</TABLE>
+</center>
+</td></tr>
+</table>
 
-</FORM>
+</form>
 
 <?php print_trailer(); ?>
-</BODY>
-</HTML>
+</body>
+</html>
