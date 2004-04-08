e8af69fc7beddf0216947b595ad09cc8f9947d6c
musus <musus>
2004-04-08 08:18:59 +0000

    replacing html formatting with css styles & continuing work on getting code to validate

--- help_index.php
+++ help_index.php
@@ -3,20 +3,20 @@ include_once 'includes/init.php';
 print_header('','','',true);
 ?>
 
-<H2><FONT COLOR="<?php echo $H2COLOR;?>"><?php etranslate("Help Index")?></FONT></H2>
+<h2 style="color:<?php echo $H2COLOR;?>;"><?php etranslate("Help Index")?></h2>
 
-<UL>
-<LI><A HREF="help_edit_entry.php"><?php etranslate("Adding/Editing Calendar Entries")?></A>
-<LI><A HREF="help_layers.php"><?php etranslate("Layers")?></A>
-<LI><A HREF="help_import.php"><?php etranslate("Import")?></A>
-<LI><A HREF="help_pref.php"><?php etranslate("Preferences")?></A>
-<?php if ( $is_admin ) { ?>
-<LI><A HREF="help_admin.php"><?php etranslate("System Settings")?></A>
-<?php } ?>
-<LI><A HREF="help_bug.php"><?php etranslate("Report Bug")?></A>
-</UL>
+<ul>
+	<li><a href="help_edit_entry.php"><?php etranslate("Adding/Editing Calendar Entries")?></a></li>
+	<li><a href="help_layers.php"><?php etranslate("Layers")?></a></li>
+	<li><a href="help_import.php"><?php etranslate("Import")?></a></li>
+	<li><a href="help_pref.php"><?php etranslate("Preferences")?></a></li>
+	<?php if ( $is_admin ) { ?>
+		<li><a href="help_admin.php"><?php etranslate("System Settings")?></a></li>
+	<?php } ?>
+	<li><a href="help_bug.php"><?php etranslate("Report Bug")?></a></li>
+</ul>
 
 <?php include_once "includes/help_trailer.php"; ?>
 
-</BODY>
-</HTML>
+</body>
+</html>
