4490944f7f0f24f577a058b43537feb9b7216814
Craig Knudsen <craig@k5n.us>
2004-04-21 03:51:12 +0000

    Allow admin users to import events into other user's calendars.

--- import.php
+++ import.php
@@ -52,6 +52,50 @@ function end_tabs() {
   echo "  </div>\n";
   echo "</div>\n";
 }
+
+// Generate the selection list for calendar user selection.
+// Only ask for calendar user if user is an administrator.
+// We may enhance this in the future to allow
+// - selection of more than one user
+// - non-admin users this functionality
+function print_user_list () {
+  global $single_user, $is_admin, $nonuser_enabled, $login,
+    $is_nonuser_admin, $is_assistant;
+
+  if ( $single_user == "N" && $is_admin ) {
+    $userlist = get_my_users ();
+    if ($nonuser_enabled == "Y" ) {
+      $nonusers = get_nonuser_cals ();
+      $userlist = ($nonuser_at_top == "Y") ?
+        array_merge($nonusers, $userlist) : array_merge($userlist, $nonusers);
+    }
+    $num_users = 0;
+    $size = 0;
+    $users = "";
+    for ( $i = 0; $i < count ( $userlist ); $i++ ) {
+      $l = $userlist[$i]['cal_login'];
+      $size++;
+      $users .= "<option value=\"" . $l . "\"";
+      if ( $id > 0 ) {
+        if ( ! empty ( $participants[$l] ) )
+          $users .= " selected=\"selected\"";
+      } else {
+        if ( $l == $login && ! $is_assistant  && ! $is_nonuser_admin )
+          $users .= " selected=\"selected\"";
+      }
+      $users .= "> " . $userlist[$i]['cal_fullname'] . "</option>\n";
+    }
+  
+    if ( $size > 50 )
+      $size = 15;
+    else if ( $size > 5 )
+      $size = 5;
+    print "<tr><td valign=\"top\"><b>" . translate("Calendar") . "</b></td>\n";
+    print "<td><select name=\"calUser\" size=\"$size\">$users\n";
+    print "</select>";
+    print "</td></tr>\n";
+  }
+}
 ?>
 
 <h2 style="color:<?= $H2COLOR;?>;">Import</h2>
@@ -67,12 +111,13 @@ if ( $tab == 0 ) {
 <?php etranslate("This form will allow you to import entries from the Palm Desktop Datebook."); ?>
 <br /><br />
 <input type="hidden" name="ImportType" value="PALMDESKTOP" />
-<b><?php etranslate("Exclude private records")?>:</b>
-<input type="radio" name="exc_private" value="1" CHECKED="CHECKED"><?php etranslate("Yes")?>
+<table border="0">
+<tr><td><b><?php etranslate("Exclude private records")?>:</b></td>
+<td><input type="radio" name="exc_private" value="1" CHECKED="CHECKED"><?php etranslate("Yes")?>
 <input type="radio" name="exc_private" value="0"><?php etranslate("No")?>
-<br /><br />
-<table style="border-width:0px;">
-<tr><td style="font-weight:bold;"><?php etranslate("Datebook File")?>:</td>
+</td></tr>
+<?php print_user_list(); ?>
+<tr><td><b><?php etranslate("Datebook File")?>:</b></td>
   <td><input type="file" name="FileName" size="45" maxlength="50" /></td></tr>
 <tr><td colspan="2"><input type="submit" value="<?php etranslate("Import")?>" />
 <input type="button" value="<?php etranslate("Help")?>..." onclick="window.open ( 'help_import.php', 'cal_help', 'dependent,menubar,scrollbars,height=400,width=400');" />
@@ -88,8 +133,9 @@ if ( $tab == 0 ) {
 <?php etranslate("This form will import vCalendar (.vcs) 1.0 events");?>.
 <br /><br />
 <input type="hidden" name="ImportType" value="VCAL" />
-<table style="border-width:0px;">
-<tr><td style="font-weight:bold;"><?php etranslate("vCal File")?>:</td>
+<table border="0">
+<?php print_user_list(); ?>
+<tr><td><b><?php etranslate("vCal File")?>:</b></td>
   <td><input type="file" name="FileName" size="45" maxlength=50" /></td></tr>
 <tr><td colspan="2"><input type="submit" value="<?php etranslate("Import")?>" />
 <input type="button" value="<?php etranslate("Help")?>..." onclick="window.open ( 'help_import.php', 'cal_help', 'dependent,menubar,scrollbars,height=400,width=400');" />
@@ -104,14 +150,15 @@ if ( $tab == 0 ) {
 <?php etranslate("This form will import iCalendar (.ics) events");?>.
 <br /><br />
 <input type="hidden" name="ImportType" value="ICAL" />
-<table style="border-width:0px;">
-<tr><td style="font-weight:bold;"><?php etranslate("iCal File")?>:</td>
+<table border="0">
+<tr><td><b><?php etranslate("iCal File")?>:</b></td>
   <td><input type="file" name="FileName" size="45" maxlength=50" /></td></tr>
-<tr><td style="font-weight:bold;"><?php etranslate("Overwrite Prior Import")?>:</td>
+<tr><td><b><?php etranslate("Overwrite Prior Import")?>:</b></td>
   <td><input type="radio" name="overwrite" value="Y" CHECKED="CHECKED" /> <?php etranslate("Yes");?>
   &nbsp;&nbsp;
   <input type="radio" name="overwrite" value="N"/> <?php etranslate("No");?>
    </td></tr>
+<?php print_user_list(); ?>
 <tr><td colspan="2"><input type="submit" value="<?php etranslate("Import")?>" />
 <input type="button" value="<?php etranslate("Help")?>..." onclick="window.open ( 'help_import.php', 'cal_help', 'dependent,menubar,scrollbars,height=400,width=400');" />
 </td></tr>
