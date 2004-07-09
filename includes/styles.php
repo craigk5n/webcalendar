3d94d07475e3aa413588009859746f9742191e6e
musus <musus>
2004-07-09 06:18:47 +0000

    moved things around to make it easier to read & keep CSS precidence of new styles intact with rules.  also added clearly maked sections for easier readability

--- includes/styles.php
+++ includes/styles.php
@@ -70,57 +70,6 @@
   text-decoration: none;
   background-color: #E7E7E7;
 }
-
-
-
-/* YEAR.PHP */
-/* contains ALL months in year.php */
-table.yearview {
-	border-width: 0px;
-}
-.yearview tr {
-	vertical-align: top;
-}
-.yearviewtitle {
-	text-align: center;
-}
-
-/* MINI CALENDARS */
-/* contains individual months in year.php */
-table.minical {
-	border-width: 0px;
-}
-td.month {
-	text-align: center;
-}
-/* contains the name of the month (i.e. January, June, December, etc) in year.php */
-td.month a {
-	font-family: <?php echo $GLOBALS['FONTS'] ?>;
-	font-size: 13px;
-	color: #B04040;
-	text-decoration: none;
-}
-/* formats the day name (i.e. Sun, Mon, etc) */
-/* used as "<tr class="day">" to format the cells WITHIN that row */
-/* NOTE: removing the "th" below will modify the appearance of mini calendars */
-.minical tr.day th {
-	font-size: 10px;
-	font-weight: normal;
-}
-/* cells in year.php that contain the numeric date */
-/* NOTE: removing the "td" below will modify the appearance of mini calendars */
-.minical tr.date td {
-	text-align: right;
-}
-/* links within cells with yearviewdate class */
-.minical tr.date a {
-	font-family: <?php echo $GLOBALS['FONTS'] ?>;
-	font-size: 13px;
-	text-decoration: none;
-}
-
-
-
 .weeknumber {
   font-family: <?php echo $GLOBALS['FONTS'] ?>;
   font-size: 10px;
@@ -167,8 +116,201 @@ td.month a {
   border-top: 1px solid <?php echo $GLOBALS['TABLEBG']; ?>;
   border-left: 1px solid <?php echo $GLOBALS['TABLEBG']; ?>;
 }
+a {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  color: <?php echo ( $GLOBALS['TEXTCOLOR'] == "" ? "#000000" : $GLOBALS['TEXTCOLOR'] ); ?>;
+  text-decoration: none;
+}
+a:hover {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  color: #0000FF;
+}
+.aboutinfo {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  color: #000000;
+  text-decoration: none;
+  font-size: 13px;
+}
+.popup {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+  color: <?php echo ( $GLOBALS['POPUP_FG'] == "" ? "#000000" : $GLOBALS['POPUP_FG'] ); ?>;
+  background-color: <?php echo $GLOBALS[POPUP_BG] ?>;
+  text-decoration: none;
+}
+.tooltip {
+	cursor: help;
+	text-decoration: none;
+	font-weight: bold;
+}
+.tooltipselect {
+	cursor: help;
+	text-decoration: none;
+	font-weight: bold;
+	vertical-align: top;
+}
+h2 {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 20px;
+  color: <?php echo $GLOBALS['H2COLOR'] ?>;
+}
+h3 {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 18px;
+}
+.pagetitle {
+	font-family: <?php echo $GLOBALS['FONTS'] ?>;
+	font-size: 24px;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+	font-weight: bold;
+}
+body {
+	color: <?php echo ( $GLOBALS['TEXTCOLOR'] == "" ? "#000000" : $GLOBALS['TEXTCOLOR'] ); ?>;
+	font-family: <?php echo $GLOBALS['FONTS'] ?>;
+	font-size: 12px;
+	background-color: <?php echo $GLOBALS['BGCOLOR'] ?>;
+}
+td {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+}
+p {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+}
+input {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+}
+select {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+}
+textarea {
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+}
+.dailymatrix {
+  cursor:pointer;
+  font-family: <?php echo $GLOBALS['FONTS'] ?>;
+  font-size: 12px;
+  text-decoration: none;
+}
+/* formats the left & right arrow images */
+.prevnext {
+	border-width: 0px;
+	width: 36px;
+	height: 32px;
+}
+.prevnextsmall {
+	width: 18px;
+	height: 18px;
+	border-width: 0px;
+}
+td.numericdate {
+	font-size: 10px;
+}
+.monthyear {
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+	font-weight: bold;
+	font-size: 24px;
+}
+.user {
+	font-size: 18px;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+	text-align: center;
+}
+.help {
+	vertical-align: top;
+	font-weight: bold;
+}
 
 
+
+/* ALL STYLES BELOW THIS LINE ARE NEW as of 8 July 2004 */
+/* ========================= */
+/* DAY.PHP */
+.dayview { }
+.dayviewtitle {
+	text-align: center;
+}
+/* contains the date (i.e. Monday, May 3, 2004) in day.php */
+td.dayviewtitle .date {
+	font-size: 24px;
+	font-weight: bold;
+	text-align: center;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+}
+td.dayviewtitle .user {
+	font-size: 18px;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+	font-weight: bold;
+	text-align: center;
+}
+
+/* ========================= */
+/* YEAR.PHP */
+/* contains ALL months in year.php */
+table.yearview {
+	border-width: 0px;
+}
+.yearview tr {
+	vertical-align: top;
+}
+.yearviewtitle {
+	text-align: center;
+}
+/* contains the year (i.e. 2004) in year.php */
+td.yearviewtitle .date {
+	font-size: 24px;
+	font-weight: bold;
+	text-align: center;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+}
+td.yearviewtitle .user {
+	font-size: 18px;
+	font-weight: bold;
+	color: <?php echo $GLOBALS['H2COLOR'] ?>;
+	text-align: center;
+}
+
+/* ========================= */
+/* MINI CALENDARS */
+/* contains individual months in year.php */
+table.minical {
+	border-width: 0px;
+}
+td.month {
+	text-align: center;
+}
+/* contains the name of the month (i.e. January, June, December, etc) in year.php */
+td.month a {
+	font-family: <?php echo $GLOBALS['FONTS'] ?>;
+	font-size: 13px;
+	color: #B04040;
+	text-decoration: none;
+}
+/* formats the day name (i.e. Sun, Mon, etc) */
+/* used as "<tr class="day">" to format the cells WITHIN that row */
+/* NOTE: removing the "th" below will modify the appearance of mini calendars */
+.minical tr.day th {
+	font-size: 10px;
+	font-weight: normal;
+}
+/* cells in year.php that contain the numeric date */
+/* NOTE: removing the "td" below will modify the appearance of mini calendars */
+.minical tr.date td {
+	text-align: right;
+}
+/* links within cells with yearviewdate class */
+.minical tr.date a {
+	font-family: <?php echo $GLOBALS['FONTS'] ?>;
+	font-size: 13px;
+	text-decoration: none;
+}
+
+/* ========================= */
+/* WEEK.PHP */
 table.weekview {
 	border: 0px;
 	width: 100%;
@@ -187,7 +329,7 @@ table.weekview {
 	font-family: <?php echo $GLOBALS['FONTS'] ?>;
 	background-color: <?php echo $GLOBALS['BGCOLOR'] ?>;
 	border-top: 1px solid <?php echo $GLOBALS['BGCOLOR'] ?>;
-	border-left: 1px solid <?php echo $GLOBALS['BGCOLOR'] ?>;
+	border-left: 1px solid <?php echo $GLOBALS['TABLEBG'] ?>;
 	border-right: 1px solid <?php echo $GLOBALS['TABLEBG']; ?>;
 	border-bottom: 1px solid <?php echo $GLOBALS['TABLEBG']; ?>;
 }
@@ -229,116 +371,10 @@ table.weekview {
 	vertical-align: top;
 	background-color: <?php echo ( $GLOBALS['TODAYCELLBG'] == "" ? "#C0C0C0" : $GLOBALS['TODAYCELLBG'] ); ?>;
 }
-
-
-a {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  color: <?php echo ( $GLOBALS['TEXTCOLOR'] == "" ? "#000000" : $GLOBALS['TEXTCOLOR'] ); ?>;
-  text-decoration: none;
-}
-a:hover {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  color: #0000FF;
-}
-.aboutinfo {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  color: #000000;
-  text-decoration: none;
-  font-size: 13px;
-}
-.popup {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-  color: <?php echo ( $GLOBALS['POPUP_FG'] == "" ? "#000000" : $GLOBALS['POPUP_FG'] ); ?>;
-  background-color: <?php echo $GLOBALS[POPUP_BG] ?>;
-  text-decoration: none;
-}
-.tooltip {
-  cursor: help;
-  text-decoration: none;
-  font-weight: bold;
-}
-h2 {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 20px;
-  color: <?php echo $GLOBALS['H2COLOR'] ?>;
-}
-h3 {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 18px;
-}
-.pagetitle {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 24px;
-  color: <?php echo $GLOBALS['H2COLOR'] ?>;
-  font-weight: bold;
-}
-body {
-  color: <?php echo ( $GLOBALS['TEXTCOLOR'] == "" ? "#000000" : $GLOBALS['TEXTCOLOR'] ); ?>;
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-  background-color: <?php echo $GLOBALS['BGCOLOR'] ?>;
-}
-td {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-}
-p {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-}
-input {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-}
-select {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-}
-textarea {
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-}
-.dailymatrix {
-  cursor:pointer;
-  font-family: <?php echo $GLOBALS['FONTS'] ?>;
-  font-size: 12px;
-  text-decoration: none;
-}
-/* formats the left & right arrow images */
-.prevnext {
+.new {
 	border-width: 0px;
-	width: 36px;
-	height: 32px;
-}
-.prevnextsmall {
-	width: 18px;
-	height: 18px;
-	border-width: 0px;
-}
-/* contains the year (i.e. 2004) in year.php */
-.year {
-	font-size: 24px;
-	font-weight: bold;
-	text-align: center;
-	color: <?php echo $GLOBALS['H2COLOR'] ?>;
-}
-td.numericdate {
-	font-size: 10px;
-}
-.monthyear {
-	color: <?php echo $GLOBALS['H2COLOR'] ?>;
-	font-weight: bold;
-	font-size: 24px;
-}
-.user {
-	font-size: 18px;
-	color: <?php echo $GLOBALS['H2COLOR'] ?>;
-	text-align: center;
-}
-.help {
-	vertical-align: top;
-	font-weight: bold;
+	width: 10px;
+	height: 10px;
 }
 -->
 </style>
