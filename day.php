c1abee12d6196114a20f33104a8a1fc6502b4908
cknudsen <cknudsen>
2004-08-05 02:36:57 +0000

    Updated week/day/month view to not include "target" in meta-refresh since it will not xhtml validate.

--- day.php
+++ day.php
@@ -31,7 +31,7 @@ $HeadX = '';
 if ( $auto_refresh == "Y" && ! empty ( $auto_refresh_time ) ) {
   $refresh = $auto_refresh_time * 60; // convert to seconds
   $HeadX = "<meta http-equiv=\"refresh\" content=\"$refresh; url=day.php?$u_url" .
-    "date=$nowYmd$caturl\" target=\"_self\" />\n";
+    "date=$nowYmd$caturl\" />\n";
 }
 $INC = array('js/popups.php');
 print_header($INC,$HeadX);
