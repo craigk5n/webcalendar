6c990ad2ac05b8cd82875f4ce5f14c0c20431394
Craig Knudsen <craig@k5n.us>
2004-08-11 21:55:35 +0000

    Moved makeVisible and makeInvisible JS functions from js/edit_entry.php into js/visible.php so the functions can be used by other pages (like admin.php).

---
+++ includes/js/visible.php
@@ -0,0 +1,47 @@
+<?php
+/*
+ * $Id
+ *
+ * The functions in this file can be used to make elements visible or
+ * hidden on the page.
+ *
+ */
+?>
+<script type="text/javascript">
+<!-- <![CDATA[
+// detect browser
+NS4 = (document.layers) ? 1 : 0;
+IE4 = (document.all) ? 1 : 0;
+// W3C stands for the W3C standard, implemented in Mozilla (and Netscape 6) and IE5
+W3C = (document.getElementById) ? 1 : 0;	
+
+function makeVisible ( name ) {
+  var ele;
+
+  if ( W3C ) {
+    ele = document.getElementById(name);
+  } else if ( NS4 ) {
+    ele = document.layers[name];
+  } else { // IE4
+    ele = document.all[name];
+  }
+
+  if ( NS4 ) {
+    ele.visibility = "show";
+  } else {  // IE4 & W3C & Mozilla
+    ele.style.visibility = "visible";
+  }
+}
+
+function makeInvisible ( name ) {
+  if (W3C) {
+    document.getElementById(name).style.visibility = "hidden";
+  } else if (NS4) {
+    document.layers[name].visibility = "hide";
+  } else {
+    document.all[name].style.visibility = "hidden";
+  }
+}
+
+//]]> -->
+</script>
