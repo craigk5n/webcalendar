e70c0942da5c334691e7b72eaa5244a116dbc5b1
musus <musus>
2004-10-13 09:39:09 +0000

    applying snazzy new year styling.  Thanks to Adam Roben for his hard work!

--- year.php
+++ year.php
@@ -15,37 +15,52 @@ function display_small_month ( $thismonth, $thisyear, $showyear ) {
   else
     $u_url = "";
 
-  echo "<table class=\"yearviewminical\" cellpadding=\"1\" cellspacing=\"2\">\n";
-  if ( $WEEK_START == "1" )
-    $wkstart = get_monday_before ( $thisyear, $thismonth, 1 );
-  else
-    $wkstart = get_sunday_before ( $thisyear, $thismonth, 1 );
+  //start the minical table for each month
+  echo "\n<table class=\"minical\">\n";
+	$monthstart = mktime(2,0,0,$thismonth,1,$thisyear);
+	$monthend = mktime(2,0,0,$thismonth + 1,0,$thisyear);
 
-  $monthstart = mktime(2,0,0,$thismonth,1,$thisyear);
-  $monthend = mktime(2,0,0,$thismonth + 1,0,$thisyear);
-  echo "<tr><td colspan=\"7\" class=\"month\">";
-  if ( empty ( $friendly ) ) {
-     echo "<a href=\"month.php?year=$thisyear&amp;month=$thismonth"
-       . $u_url . "\">";
-  }
-  echo month_name ( $thismonth - 1 ) .
-    ( empty ( $friendly ) ? "</a>" : "" ) . "</td></tr>\n";
-  echo "<tr class=\"day\">";
-  if ( $WEEK_START == 0 ) echo "<th>" .
-    weekday_short_name ( 0 ) . "</th>\n";
-  for ( $i = 1; $i < 7; $i++ ) {
-    echo "<th>" .
-      weekday_short_name ( $i ) . "</th>\n";
-  }
-  if ( $WEEK_START == 1 ) echo "<th>" .
-    weekday_short_name ( 0 ) . "</th>\n";
-  echo "</tr>\n";
+	//print the month name
+	//if not in printer-friendly mode, also link it to month view
+	echo "<caption>";
+		if ( empty ( $friendly ) ) {
+			echo "<a href=\"month.php?year=$thisyear&amp;month=$thismonth"
+				. $u_url . "\">";
+		}
+		echo month_name ( $thismonth - 1 ) .
+			( empty ( $friendly ) ? "</a>" : "" ) . 
+	"</caption>\n";
+	//determine if the week starts on sunday or monday
+	if ( $WEEK_START == "1" ) {
+		$wkstart = get_monday_before ( $thisyear, $thismonth, 1 );
+	} else {
+		$wkstart = get_sunday_before ( $thisyear, $thismonth, 1 );
+	}
+	
+	//print the headers to display the day of the week (sun, mon, tues, etc.)
+	echo "<tr class=\"day\">\n";
+		//if the week doesn't start on monday, print the day
+		if ( $WEEK_START == 0 ) echo "<th>" .
+			weekday_short_name ( 0 ) . 
+		"</th>\n";
+		//cycle through each day of the week until gone
+		for ( $i = 1; $i < 7; $i++ ) {
+			echo "<th>" .
+				weekday_short_name ( $i ) . 
+			"</th>\n";
+		}
+		//if the week DOES start on monday, print sunday
+		if ( $WEEK_START == 1 ) echo "<th>" .
+			weekday_short_name ( 0 ) . 
+		"</th>\n";
+	//end the header row
+	echo "</tr>\n";
   for ($i = $wkstart; date("Ymd",$i) <= date ("Ymd",$monthend);
     $i += (24 * 3600 * 7) ) {
-    echo "<tr class=\"numdate\">";
-    for ($j = 0; $j < 7; $j++) {
-      $date = $i + ($j * 24 * 3600);
-      $dateYmd = date ( "Ymd", $date );
+		echo "<tr class=\"numdate\">\n";
+     for ($j = 0; $j < 7; $j++) {
+       $date = $i + ($j * 24 * 3600);
+       $dateYmd = date ( "Ymd", $date );
       $hasEvents = false;
       if ( $boldDays ) {
         $ev = get_entries ( $user, $dateYmd, $get_unapproved );
@@ -57,23 +72,31 @@ function display_small_month ( $thismonth, $thisyear, $showyear ) {
             $hasEvents = true;
         }
       }
-      if ( $dateYmd >= date ("Ymd",$monthstart) &&
-        $dateYmd <= date ("Ymd",$monthend) ) {
-        echo "<td";
-        echo ( $hasEvents ? " class=\"highlight\">" : ">" );
-        if ( empty ( $friendly ) )
-	  echo "<a href=\"day.php?date=" .  $dateYmd . $u_url .  "\">";
-	echo date ( "j", $date );
-        if ( empty ( $friendly ) )
-          echo "</a>";
-        echo "</td>\n";
-      } else {
-        echo "<td>&nbsp;</td>\n";
-      }
-    }                 // end for $j
-    echo "</tr>\n";
-  }                         // end for $i
-  echo "</table>\n";
+       if ( $dateYmd >= date ("Ymd",$monthstart) &&
+         $dateYmd <= date ("Ymd",$monthend) ) {
+         echo "<td";
+		$wday = date ( "w", $date );
+		$class = array();  // array of classes this cell belongs to
+		if ( $wday == 0 || $wday == 6 ) $class[] = "weekend";
+		if ( $hasEvents ) $class[] = "hasevents";
+			$class = implode ( " ", $class ); // turn the array into a string
+		if ( strlen ( $class ) ) echo " class=\"$class\"";
+		if ( $dateYmd == date ( "Ymd" ) )
+			echo " id=\"today\"";
+			echo ">";
+		if ( empty ( $friendly ) )
+			echo "<a href=\"day.php?date=" .  $dateYmd . $u_url .  "\">";
+		echo date ( "d", $date );
+		if ( empty ( $friendly ) )
+			echo "</a>";
+		echo "</td>\n";
+		} else {
+			echo "<td class=\"empty\">&nbsp;</td>\n";
+		}
+	}                 // end for $j
+echo "</tr>\n";
+}                         // end for $i
+echo "</table>\n";
 }
 
 if ( empty ( $year ) )
@@ -110,58 +133,57 @@ $get_unapproved = ( $DISPLAY_UNAPPROVED == 'Y' );
 if ( $user == "__public__" )
   $get_unapproved = false;
 
-print_header();
-?>
-
-<div class="yearviewtitle">
-<?php if ( empty ( $friendly ) ) { ?>
-<a title="<?php etranslate("Previous")?>" href="year.php?year=<?php echo $prevYear; if ( ! empty ( $user ) ) echo "&amp;user=$user";?>"><img src="leftarrow.gif" class="prevnext" style="float:left; margin-left:3px; margin-top:7px;" alt="<?php etranslate("Previous")?>" /></a>
-<a title="<?php etranslate("Next")?>" href="year.php?year=<?php echo $nextYear; if ( ! empty ( $user ) ) echo "&amp;user=$user";?>"><img src="rightarrow.gif" class="prevnext" style="float:right; margin-right:3px; margin-top:7px;" alt="<?php etranslate("Next")?>" /></a>
-<?php } ?>
-<span class="date"><?php echo $thisyear ?></span>
-<span class="user"><?php
-  if ( $single_user == "N" ) {
-    echo "<br />\n";
-    if ( ! empty ( $user ) ) {
-      user_load_variables ( $user, "user_" );
-      echo $user_fullname;
-    } else
-      echo $fullname;
-    if ( $is_assistant )
-      echo "<br /><span style=\"font-weight:bold;\">-- " . translate("Assistant mode") . " --</span>";
-  }
-?></span>
+ print_header();
+ ?>
+ 
+<div class="title">
+	<?php if ( empty ( $friendly ) ) { ?>
+		<a title="<?php etranslate("Previous")?>" href="year.php?year=<?php echo $prevYear; if ( ! empty ( $user ) ) echo "&amp;user=$user";?>"><img src="leftarrow.gif" class="prevnext" style="float:left; margin-left:3px; margin-top:7px;" alt="<?php etranslate("Previous")?>" /></a>
+		<a title="<?php etranslate("Next")?>" href="year.php?year=<?php echo $nextYear; if ( ! empty ( $user ) ) echo "&amp;user=$user";?>"><img src="rightarrow.gif" class="prevnext" style="float:right; margin-right:3px; margin-top:7px;" alt="<?php etranslate("Next")?>" /></a>
+	<?php } ?>
+	<span class="date"><?php echo $thisyear ?></span>
+	<span class="user"><?php
+		if ( $single_user == "N" ) {
+			echo "<br />\n";
+			if ( ! empty ( $user ) ) {
+				user_load_variables ( $user, "user_" );
+				echo $user_fullname;
+			} else {
+				echo $fullname;
+			}
+			if ( $is_assistant )
+				echo "<br /><span style=\"font-weight:bold;\">-- " . translate("Assistant mode") . " --</span>";
+		}
+	?></span>
 </div>
 <br />
-
+ 
 <div align="center">
-<table class="yearview" cellspacing="1" cellpadding="2">
-<tr>
-<td><?php display_small_month(1,$year,False); ?></td>
-<td><?php display_small_month(2,$year,False); ?></td>
-<td><?php display_small_month(3,$year,False); ?></td>
-<td><?php display_small_month(4,$year,False); ?></td>
-</tr>
-<tr>
-<td><?php display_small_month(5,$year,False); ?></td>
-<td><?php display_small_month(6,$year,False); ?></td>
-<td><?php display_small_month(7,$year,False); ?></td>
-<td><?php display_small_month(8,$year,False); ?></td>
-</tr>
-<tr>
-<td><?php display_small_month(9,$year,False); ?></td>
-<td><?php display_small_month(10,$year,False); ?></td>
-<td><?php display_small_month(11,$year,False); ?></td>
-<td><?php display_small_month(12,$year,False); ?></td>
-</tr>
-</table>
+	<table class="main">
+		<tr><td>
+			<?php display_small_month(1,$year,False); ?></td><td>
+			<?php display_small_month(2,$year,False); ?></td><td>
+			<?php display_small_month(3,$year,False); ?></td><td>
+			<?php display_small_month(4,$year,False); ?>
+		</td></tr>
+		<tr><td>
+			<?php display_small_month(5,$year,False); ?></td><td>
+			<?php display_small_month(6,$year,False); ?></td><td>
+			<?php display_small_month(7,$year,False); ?></td><td>
+			<?php display_small_month(8,$year,False); ?>
+		</td></tr>
+		<tr><td>
+			<?php display_small_month(9,$year,False); ?></td><td>
+			<?php display_small_month(10,$year,False); ?></td><td>
+			<?php display_small_month(11,$year,False); ?></td><td>
+			<?php display_small_month(12,$year,False); ?>
+		</td></tr>
+	</table>
 </div>
 
 <br />
 <?php if ( empty ( $friendly ) ) {
-
-display_unapproved_events ( $login );
-
+	display_unapproved_events ( $login );
 ?>
 <br />
 <a title="<?php etranslate("Generate printer-friendly version")?>" class="printer" href="year.php?<?php
@@ -175,6 +197,5 @@ onmouseover="window.status = '<?php etranslate("Generate printer-friendly versio
 <?php }
 print_trailer();
 ?>
-
 </body>
 </html>
