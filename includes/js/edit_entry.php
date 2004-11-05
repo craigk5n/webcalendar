377486641c8eab77fcc216648c9d8660fcfdc3d3
musus <musus>
2004-11-05 14:34:47 +0000

    commiting new tabbed GUI

--- includes/js/edit_entry.php
+++ includes/js/edit_entry.php
@@ -1,8 +1,6 @@
 <?php
-global $groups_enabled,$WORK_DAY_START_HOUR;
-?>
-
-<script type="text/javascript">
+	global $groups_enabled,$WORK_DAY_START_HOUR;
+?><script type="text/javascript">
 <!-- <![CDATA[
 // do a little form verifying
 function validate_and_submit () {
@@ -81,7 +79,6 @@ function validate_and_submit () {
   return true;
 }
 
-
 function selectDate ( day, month, year, current ) {
   url = "datesel.php?form=editentryform&day=" + day +
     "&month=" + month + "&year=" + year;
@@ -91,9 +88,8 @@ function selectDate ( day, month, year, current ) {
     "width=200,height=200,resizable=yes,scrollbars=yes" );
 }
 
-
-<?php if ( $groups_enabled == "Y" ) { ?>
-function selectUsers () {
+<?php if ( $groups_enabled == "Y" ) { 
+?>function selectUsers () {
   // find id of user selection object
   var listid = 0;
   for ( i = 0; i < document.forms[0].elements.length; i++ ) {
@@ -117,41 +113,76 @@ function selectUsers () {
 }
 <?php } ?>
 
-
-// This function is called wheneve someone clicks on the
-// "All day event" / "Untimed Event" / "Timed Event" selection
-// box.  When the enabled all day, it clears all the time of day
-// and duration fields.  If they change their mind and turn it off, we
-// put the original values back for them.
-// This isn't necessary, but it helps show what the meaning of "all-day" is.
-function timetype_handler () {
+<?php	// This function is called when the event type combo box 
+	// is changed. If the user selectes "untimed event" or "all day event",
+	// the times & duration fields are hidden.
+	// If they change their mind & switch it back, the original 
+	// original values are put back for them
+?>function timetype_handler () {
   var i = document.forms[0].timetype.selectedIndex;
   var val = document.forms[0].timetype.options[i].text;
   //alert ( "val " + i + "  = " + val );
   // i == 1 when set to timed event
   if ( i != 1 ) {
     // Untimed/All Day
-    makeInvisible ( "timeentrystartprompt" );
     makeInvisible ( "timeentrystart" );
     if ( document.forms[0].duration_h ) {
-      makeInvisible ( "timeentrydurationprompt" );
       makeInvisible ( "timeentryduration" );
     } else {
-      makeInvisible ( "timeentryendprompt" );
       makeInvisible ( "timeentryend" );
     }
   } else {
     // Timed Event
-    makeVisible ( "timeentrystartprompt" );
     makeVisible ( "timeentrystart" );
     if ( document.forms[0].duration_h ) {
-      makeVisible ( "timeentrydurationprompt" );
       makeVisible ( "timeentryduration" );
     } else {
-      makeVisible ( "timeentryendprompt" );
       makeVisible ( "timeentryend" );
     }
   }
 }
+
+function rpttype_handler () {
+  var i = document.forms[0].rpttype.selectedIndex;
+  var val = document.forms[0].rpttype.options[i].text;
+  //alert ( "val " + i + "  = " + val );
+  //i == 0 when event does not repeat
+  if ( i != 0 ) {
+    // none (not repeating)
+    makeVisible ( "enddate" );
+    makeVisible ( "freq" );
+    if ( i == 2 ) {
+      makeVisible ( "day" );
+    } else {
+      makeInvisible ( "day" );
+    }
+  } else {
+    // Timed Event
+    makeInvisible ( "enddate" );
+    makeInvisible ( "freq" );
+    makeInvisible ( "day" );
+  }
+}
+
+var tabs = new Array();
+function showTab (name) {
+	if (! document.getElementById) { return true; }
+	for (var i=0; i<tabs.length; i++) {
+		var tname = tabs[i];
+		var tab = document.getElementById("tab_" + tname);
+		if (tab && tab.setAttribute) {
+			tab.setAttribute("class", (tname == name) ? "tabfor" : "tabbak");
+		}
+		var div = document.getElementById("tabscontent_" + tname);
+		if (div) {
+			div.style.display = (tname == name) ? "block" : "none";
+		}
+	}
+	return false;
+}
+tabs[1] = "details";
+tabs[2] = "sched";
+tabs[3] = "participants";
+tabs[4] = "pete";
 //]]> -->
 </script>
