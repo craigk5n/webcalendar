1f60fa639e5c465a0e03b21374e184e977b84d2a
musus <musus>
2004-11-10 04:11:02 +0000

    making search.php dynamic on the client-side for admins

--- search.php
+++ search.php
@@ -1,41 +1,28 @@
 <?php
-include_once 'includes/init.php';
+	include_once 'includes/init.php';
 
-if ( $groups_enabled == "Y" ) $INC = array('js/search.php');
-print_header($INC);
+	if ( $groups_enabled == "Y" )
+		$INC = array('js/search.php');
+	print_header($INC);
 ?>
+<h2><?php 
+	etranslate("Search"); 
+?></h2>
 
-<h2><?php if ( empty ( $advanced ) ) { etranslate("Search"); } else { etranslate ( "Advanced Search" ); } ?></h2>
+<form action="search_handler.php" method="post" name="searchformentry" style="margin-left:13px;">
 
-<form action="search_handler.php" method="post" name="searchformentry">
-
-<?php if ( empty ( $advanced ) ) { ?>
-
-<span style="font-weight:bold;"><?php etranslate("Keywords")?>:</span>
-<input type="text" name="keywords" size="30" />
-<input type="submit" value="<?php etranslate("Search")?>" />
-
-<br /><br />
-<a class="nav" href="search.php?advanced=1"><?php etranslate("Advanced Search") ?></a>
-
-<?php } else {
-$show_participants = ( $disable_participants_field != "Y" );
-if ( $is_admin )
-  $show_participants = true;
-if ( $login == "__public__" && $public_access_others != "Y" )
-  $show_participants = false;
-
+<label for="keywordsadv" style="font-weight:bold;"><?php etranslate("Keywords")?>:&nbsp;</label>
+<input type="text" name="keywords" id="keywordsadv" size="30" />&nbsp;
+<input type="submit" value="<?php etranslate("Search")?>" /><br />
+<?php 
+	if ( ($login == "__public__" && $public_access_others != "Y") || (! $is_admin) ) {
+		echo "</form>";
+	} else {
+		echo "<div id=\"advlink\"><a href=\"javascript:show('adv'); hide('advlink');\">advanced search</a></div>";
 ?>
-
-<input type="hidden" name="advanced" value="1" />
-
-<table style="border-width:0px;">
-<tr><td style="font-weight:bold;"><?php etranslate("Keywords")?>:</td>
-<td><input type="text" name="keywords" size="30" /></td>
-<td><input type="submit" value="<?php etranslate("Search")?>" /></td></tr>
-
-<?php if ( $show_participants ) { ?>
-<tr><td style="vertical-align:top; font-weight:bold;"><?php etranslate("Users"); ?></td>
+<table id="adv" style="display:none;">
+<tr><td style="vertical-align:top; text-align:right; font-weight:bold; width:60px;">
+	<?php etranslate("Users"); ?>:&nbsp;</td><td>
 <?php
   $users = get_my_users ();
   $size = 0;
@@ -53,18 +40,17 @@ if ( $login == "__public__" && $public_access_others != "Y" )
   else
     $size = count ( $users );
 ?>
-<td><select name="users[]" size="<?php echo $size;?>" multiple="multiple"><?php echo $out; ?></select>
+<select name="users[]" size="<?php echo $size;?>" multiple="multiple"><?php echo $out; ?></select>
 <?php 
   if ( $groups_enabled == "Y" ) {
-    echo "<input type=\"button\" onclick=\"selectUsers()\" value=\"" .
-      translate("Select") . "...\" />";
+   echo "<input type=\"button\" onclick=\"selectUsers()\" value=\"" .
+      translate("Select") . "...\" />\n";
   }
 ?>
 </td></tr>
-<?php } /* if show_participants */ ?>
 </table>
-<?php } ?>
 </form>
+<?php } ?>
 
 <?php print_trailer(); ?>
 </body>
