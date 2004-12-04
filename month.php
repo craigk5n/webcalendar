0dd591a7d23ceafacb1c07051dcd09496dd68cd4
Craig Knudsen <craig@k5n.us>
2004-12-04 05:31:33 +0000

    Fix for bug 1078176: url refresh goes to previous month (from patch 1078529.... thanks, ray)

--- month.php
+++ month.php
@@ -32,7 +32,7 @@ $HeadX = '';
 if ( $auto_refresh == "Y" && ! empty ( $auto_refresh_time ) ) {
   $refresh = $auto_refresh_time * 60; // convert to seconds
   $HeadX = "<meta http-equiv=\"refresh\" content=\"$refresh; url=month.php?$u_url" .
-    "date=$startdate$caturl\" />\n";
+    "year=$thisyear&amp;month=$thismonth$caturl\" />\n";
 }
 $INC = array('js/popups.php');
 print_header($INC,$HeadX);
