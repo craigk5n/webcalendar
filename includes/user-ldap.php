8ed39d6df7f9b74e0c7b5cc29beb9b549c91e387
jhoov <jhoov>
2004-12-10 18:02:48 +0000

    Cleaned up user-ldap.php: added connect_and_bind function, config  section cleanup, check all searches for attr array, etc.

--- includes/user-ldap.php
+++ includes/user-ldap.php
@@ -19,108 +19,102 @@ if (preg_match("/\/includes\//", $PHP_SELF)) {
 // need these functions and you will still need to add users to
 // webcal_user.
 
+
+/***************************** Config *******************************/
 // Set some global config variables about your system.
+// Next three are NOT yet implemented for LDAP
 $user_can_update_password = false;
 $admin_can_add_user = false;
 $admin_can_delete_user = false;
 
-// LDAP Server settings
-//
-// The name of the LDAP server or its address
-$ldap_server = "localhost"; 
-// The name or the number for the LDAP server port. By default an LDAP server
-// is on port 389
-$ldap_port = "389"; 
 
-// Global LDAP Parameters
+//------ LDAP General Server Settings ------//
 //
-// The base DN to make search in order to find users and informations like a
-// user list
-$ldap_base_dn = "o=compagny.com"; 
-// The ldap attribute used to find a user. The search will be made with
-// $ldap_login_attr=user_login
+// Name or address of the LDAP server 
+//  For SSL/TLS use 'ldaps://localhost'
+$ldap_server = 'localhost';          
+
+// Port LDAP listens on (default 389)        
+$ldap_port = '389';                   
+
+// base DN to search for users      
+$ldap_base_dn = 'ou=people,dc=company,dc=com';
+
+// The ldap attribute used to find a user (login). 
 // E.g., if you use cn,  your login might be "Jane Smith"
 //       if you use uid, your login might be "jsmith"
-$ldap_login_attr = "uid";
-// A user DN used to made the search for informations. This user must have the
-// correct rights to perform search.
-// If left empty ("") the search will be made in anonymous.
-// Check if you need an administravive right to make search
-$ldap_admin_dn = "";
-// The user password for search operations
-$ldap_admin_pwd = "";
+$ldap_login_attr = 'uid';
 
-// LDAP Search parameters
+// Account used to bind to the server and search for information. 
+// This user must have the correct rights to perform search.
+// If left empty the search will be made in anonymous.
 //
-// A LDAP filter to find a user list.
-$ldap_user_filter = "(objectclass=person)";
+// *** We do NOT recommend storing the root LDAP account info here ***
+$ldap_admin_dn = '';  // user DN
+$ldap_admin_pwd = ''; // user password
+
+
+//------ Admin Group Settings ------//
+//
+// A group name (complete DN) to find users with admin rights
+$ldap_admin_group_name = 'cn=webcal_admin,ou=group,dc=company,dc=com';
+
+// What type of group do we want (posixgroup, groupofnames, groupofuniquenames)
+$ldap_admin_group_type = 'posixgroup';
+
+// The LDAP attribute used to store member of a group
+$ldap_admin_group_attr = 'memberuid';
+
+
+//------ LDAP Search Settings ------//
+//
+// LDAP filter to find a user list.
+$ldap_user_filter = '(objectclass=person)';
+
 // Attributes to fetch from LDAP and corresponding user variables in the
 // application. Do change according to your LDAP Schema
 $ldap_user_attr = array( 
-  // LDAP attribute    //WebCalendar variable
-  "uid",         //login
-  "sn",          //lastname
-  "givenname",   //firstname
-  "cn",          //fullname
-  "mail"         //email
+  // LDAP attribute   //WebCalendar variable
+  'uid',              //login
+  'sn',               //lastname
+  'givenname',        //firstname
+  'cn',               //fullname
+  'mail'              //email
 );
 
-// Admin parameters
-//
-// A groupe name (complete DN) to find user with admin's rights
-$ldap_admin_group_name = "cn=webcal_admin,ou=groupes,o=compagny.com";
-// What type of group do we want (posixgroup, groupofnames, groupofuniquenames)
-$ldap_admin_group_type = "groupofnames";
-// The LDAP attribute used to store member of a group
-$ldap_admin_group_attr = "member";
+/*************************** End Config *****************************/
 
-// Misc Cleanup
-//
+
 // Convert group name to lower case to prevent problems
 $ldap_admin_group_attr = strtolower($ldap_admin_group_attr);
 $ldap_admin_group_type = strtolower($ldap_admin_group_type);
 
-// Function to search the dn of a given user
-// the error message will be placed in $error.
+// Function to search the dn of a given user the error message will 
+// be placed in $error.
 // params:
 //   $login - user login
 //   $dn - complete dn for the user (must be given by ref )
 // return:
 //   TRUE if the user is found, FALSE in other case
 function user_search_dn ( $login ,$dn ) {
-  global $error, $ldap_server, $ldap_port, $ldap_base_dn, $ldap_login_attr;
-  global $ldap_admin_dn,$ldap_admin_pwd;
+  global $error, $ds, $ldap_base_dn, $ldap_login_attr, $ldap_user_attr;
 
   $ret = false;
-  $ds = @ldap_connect ( $ldap_server, $ldap_port );
-  if ( $ds ) {
-    if ( $ldap_admin_dn != "") {
-      $r = @ldap_bind ( $ds, $ldap_admin_dn, $ldap_admin_pwd ); // bind as administrator
+  if ($r = connect_and_bind()) {
+    $sr = @ldap_search ( $ds, $ldap_base_dn, "($ldap_login_attr=$login)", $ldap_user_attr );
+    if (!$sr) {
+      $error = 'Error searching LDAP server: ' . ldap_error();
     } else {
-      $r = @ldap_bind ( $ds ); // bind as anonymous
-    }
-    if (!$r) {
-      $error = "Invalid Admin's login for LDAP Server";
-    } else {
-      $sr = @ldap_search ( $ds, $ldap_base_dn, "($ldap_login_attr=$login)");
-      if (!$sr) {
-        $error = "Error searching LDAP server: " . ldap_error();
+      $info = @ldap_get_entries ( $ds, $sr );
+      if ( $info['count'] != 1 ) {
+        $error = 'Invalid login';
       } else {
-        $info = @ldap_get_entries ( $ds, $sr );
-        if ( $info["count"] != 1 ) {
-          $error = translate ("Invalid login");
-        } else {
-          $ret = true;
-          $dn = $info[0]["dn"];
-          //echo "Found dn : $dn\n";
-        }
-        @ldap_free_result ( $sr );
+        $ret = true;
+        $dn = $info[0]['dn'];
       }
-      @ldap_close ( $ds );
+      @ldap_free_result ( $sr );
     }
-  } else {
-    $error = "Error connecting to LDAP server";
-    $ret = false;
+    @ldap_close ( $ds );
   }
   return $ret;
 }
@@ -140,21 +134,20 @@ function user_valid_login ( $login, $password ) {
   $ds = @ldap_connect ( $ldap_server, $ldap_port );
   if ( $ds ) {
     if ( user_search_dn ( $login, &$dn) ) {
-      $r = @ldap_bind ( $ds, $dn, $password ); // bind as the user. The LDAP
-      // Server will valide the login and passowrd
+      $r = @ldap_bind ( $ds, $dn, $password );
       if (!$r) {
-        $error = translate ("Invalid login") . ": " .
-          translate("incorrect password");
+        $error = 'Invalid login';
+        //$error .= ': incorrect password'; // uncomment for debugging
       } else {
         $ret = true;
       }
     } else {
-      $error = translate ("Invalid login") . ": " .
-        translate("no such user");
+      $error = 'Invalid login';
+      //$error .= ': no such user'; // uncomment for debugging
     }
     @ldap_close ( $ds );
   } else {
-    $error = "Error connecting to LDAP server";
+    $error = 'Error connecting to LDAP server';
   }
   return $ret;
 }
@@ -167,70 +160,54 @@ function user_valid_login ( $login, $password ) {
 //   $crypt_password - crypted user password
 // returns: true or false
 function user_valid_crypt ( $login, $crypt_password ) {
-  return true; // NOT YET IMPLEMENTED FOR LDAP
+  return true;
 }
 
-// Load info about a user (first name, last name, admin) and set
-// globally.
+// Load info about a user (first name, last name, admin) and set globally.
 // params:
 //   $user - user login
 //   $prefix - variable prefix to use
 function user_load_variables ( $login, $prefix ) {
-  global $error, $ldap_server, $ldap_port, $ldap_base_dn, $ldap_login_attr, $ldap_user_attr;
-  global $ldap_admin_dn, $ldap_admin_pwd, $PUBLIC_ACCESS_FULLNAME, $NONUSER_PREFIX;
+  global $error, $ds, $ldap_base_dn, $ldap_login_attr, $ldap_user_attr;
+  global $PUBLIC_ACCESS_FULLNAME, $NONUSER_PREFIX;
 
   if ($NONUSER_PREFIX && substr($login, 0, strlen($NONUSER_PREFIX) ) == $NONUSER_PREFIX ) {
     nonuser_load_variables ( $login, $prefix );
     return true;
   }
 
-  if ( $login == "__public__" ) {
-    $GLOBALS[$prefix . "login"] = $login;
-    $GLOBALS[$prefix . "firstname"] = "";
-    $GLOBALS[$prefix . "lastname"] = "";
-    $GLOBALS[$prefix . "is_admin"] = "N";
-    $GLOBALS[$prefix . "email"] = "";
-    $GLOBALS[$prefix . "fullname"] = $PUBLIC_ACCESS_FULLNAME;
-    $GLOBALS[$prefix . "password"] = "";
+  if ( $login == '__public__' ) {
+    $GLOBALS[$prefix . 'login'] = $login;
+    $GLOBALS[$prefix . 'firstname'] = '';
+    $GLOBALS[$prefix . 'lastname'] = '';
+    $GLOBALS[$prefix . 'is_admin'] = 'N';
+    $GLOBALS[$prefix . 'email'] = '';
+    $GLOBALS[$prefix . 'fullname'] = $PUBLIC_ACCESS_FULLNAME;
+    $GLOBALS[$prefix . 'password'] = '';
     return true;
   }
 
   $ret =  false;
-  $ds = @ldap_connect ( $ldap_server, $ldap_port );
-  if ( $ds ) {
-    if ( $ldap_admin_dn != "") {
-      // bind as administrator
-      $r = @ldap_bind ( $ds, $ldap_admin_dn, $ldap_admin_pwd );
+  if ($r = connect_and_bind()) {
+    $sr = @ldap_search ( $ds, $ldap_base_dn, "($ldap_login_attr=$login)", $ldap_user_attr );
+    if (!$sr) {
+      $error = 'Error searching LDAP server: ' . ldap_error();
     } else {
-      $r = @ldap_bind ( $ds ); // bind as anonymous
-    }
-    if (!$r) {
-      $error = "Invalid Admin's login for LDAP Server";
-    } else {
-      // search for user
-      $sr = @ldap_search ( $ds, $ldap_base_dn, "($ldap_login_attr=$login)",
-        $ldap_user_attr );
-      if (!$sr) {
-        $error = "Error searching LDAP server: " . ldap_error();
+      $info = @ldap_get_entries ( $ds, $sr );
+      if ( $info['count'] != 1 ) {
+        $error = 'Invalid login';
       } else {
-        $info = @ldap_get_entries ( $ds, $sr );
-        if ( $info["count"] != 1 ) {
-          $error = translate ("Invalid login");
-        } else {
-          $GLOBALS[$prefix . "login"] = $login;
-          $GLOBALS[$prefix . "firstname"] = $info[0][$ldap_user_attr[2]][0];
-          $GLOBALS[$prefix . "lastname"] = $info[0][$ldap_user_attr[1]][0];
-          $GLOBALS[$prefix . "email"] = $info[0][$ldap_user_attr[4]][0];
-          $GLOBALS[$prefix . "fullname"] = $info[0][$ldap_user_attr[3]][0];
-          $GLOBALS[$prefix . "is_admin"] = user_is_admin($login,get_admins());
-          $ret = true;
-        }
-        @ldap_free_result ( $sr );
+        $GLOBALS[$prefix . 'login'] = $login;
+        $GLOBALS[$prefix . 'firstname'] = $info[0][$ldap_user_attr[2]][0];
+        $GLOBALS[$prefix . 'lastname'] = $info[0][$ldap_user_attr[1]][0];
+        $GLOBALS[$prefix . 'email'] = $info[0][$ldap_user_attr[4]][0];
+        $GLOBALS[$prefix . 'fullname'] = $info[0][$ldap_user_attr[3]][0];
+        $GLOBALS[$prefix . 'is_admin'] = user_is_admin($login,get_admins());
+        $ret = true;
       }
+      @ldap_free_result ( $sr );
     }
     @ldap_close ( $ds );
-  } else {
-    $error = "Error connecting to LDAP server";
   }
   return $ret;
 }
@@ -243,11 +220,10 @@ function user_load_variables ( $login, $prefix ) {
 //   $lastname - last name
 //   $email - email address
 //   $admin - is admin? ("Y" or "N")
-function user_add_user ( $user, $password, $firstname, $lastname, $email,
-  $admin ) {
+function user_add_user ( $user, $password, $firstname, $lastname, $email, $admin ) {
   global $error;
 
-  $error = "Not yet supported.";
+  $error = 'Not yet supported.';
   return false;
 }
 
@@ -261,7 +237,7 @@ function user_add_user ( $user, $password, $firstname, $lastname, $email,
 function user_update_user ( $user, $firstname, $lastname, $email, $admin ) {
   global $error;
 
-  $error = "Not yet supported.";
+  $error = 'Not yet supported.';
   return false;
 }
 
@@ -272,7 +248,7 @@ function user_update_user ( $user, $firstname, $lastname, $email, $admin ) {
 function user_update_user_password ( $user, $password ) {
   global $error;
 
-  $error = "Not yet supported";
+  $error = 'Not yet supported';
   return false;
 }
 
@@ -282,63 +258,50 @@ function user_update_user_password ( $user, $password ) {
 // params:
 //   $user - user to delete
 function user_delete_user ( $user ) {
-  $error = "Not yet supported";
+  $error = 'Not yet supported';
   return false;
 }
 
 
 // Get a list of users and return info in an array.
+// returns: array of users
 function user_get_users () {
-  global $error, $ldap_server, $ldap_port, $ldap_base_dn, $ldap_user_attr;
-  global $ldap_admin_dn, $ldap_admin_pwd, $ldap_user_filter;
+  global $error, $ds, $ldap_base_dn, $ldap_user_attr, $ldap_user_filter;
   global $public_access, $PUBLIC_ACCESS_FULLNAME;
 
   $Admins = get_admins();
   $count = 0;
   $ret = array ();
-  if ( $public_access == "Y" )
+  if ( $public_access == 'Y' )
     $ret[$count++] = array (
-       "cal_login" => "__public__",
-       "cal_lastname" => "",
-       "cal_firstname" => "",
-       "cal_is_admin" => "N",
-       "cal_email" => "",
-       "cal_password" => "",
-       "cal_fullname" => $PUBLIC_ACCESS_FULLNAME );
-  $ds = @ldap_connect ( $ldap_server, $ldap_port );
-  if ( $ds ) {
-    if ( $ldap_admin_dn != "") {
-      // bind as administrator
-      $r = @ldap_bind ( $ds, $ldap_admin_dn, $ldap_admin_pwd );
+       'cal_login' => '__public__',
+       'cal_lastname' => '',
+       'cal_firstname' => '',
+       'cal_is_admin' => 'N',
+       'cal_email' => '',
+       'cal_password' => '',
+       'cal_fullname' => $PUBLIC_ACCESS_FULLNAME );
+
+  if ($r = connect_and_bind()) {
+    $sr = @ldap_search ( $ds, $ldap_base_dn, $ldap_user_filter, $ldap_user_attr );
+    if (!$sr) {
+      $error = 'Error searching LDAP server: ' . ldap_error();
     } else {
-      $r = @ldap_bind ( $ds ); // bind as anonymous
-    }
-    if (!$r) {
-      $error = "Invalid Admin's login for LDAP Server";
-    } else {
-      // search for user
-      $sr = @ldap_search ( $ds, $ldap_base_dn, $ldap_user_filter,
-        $ldap_user_attr );
       if ( (float)substr(PHP_VERSION,0,3) >= 4.2 ) ldap_sort ( $ds, $sr, $ldap_user_attr[3]);
       $info = @ldap_get_entries( $ds, $sr );
-      for ( $i = 0; $i < $info["count"]; $i++ ) {
+      for ( $i = 0; $i < $info['count']; $i++ ) {
         $ret[$count++] = array (
-          "cal_login" => $info[$i][$ldap_user_attr[0]][0],
-          "cal_lastname" => $info[$i][$ldap_user_attr[1]][0],
-          "cal_firstname" => $info[$i][$ldap_user_attr[2]][0],
-          "cal_email" => $info[$i][$ldap_user_attr[4]][0],
-          
-          // Something to do here : is_admin is needed in one page (admin page)
-          // as it generate a lot of search, we must do it another way
-          "cal_is_admin" => user_is_admin($info[$i][$ldap_user_attr[0]][0],$Admins),
-          "cal_fullname" => $info[$i][$ldap_user_attr[3]][0]
+          'cal_login' => $info[$i][$ldap_user_attr[0]][0],
+          'cal_lastname' => $info[$i][$ldap_user_attr[1]][0],
+          'cal_firstname' => $info[$i][$ldap_user_attr[2]][0],
+          'cal_email' => $info[$i][$ldap_user_attr[4]][0],
+          'cal_is_admin' => user_is_admin($info[$i][$ldap_user_attr[0]][0],$Admins),
+          'cal_fullname' => $info[$i][$ldap_user_attr[3]][0]
           );
       }
       @ldap_free_result($sr);
     }
     @ldap_close ( $ds );
-  } else {
-    $error = "Error connecting to LDAP server";
   }
   return $ret;
 }
@@ -347,72 +310,84 @@ function user_get_users () {
 // group in the LDAP Server
 // params:
 //   $values - the login name
-// returns Y if user is admin, N if not
+// returns: Y if user is admin, N if not
 function user_is_admin($values,$Admins) {
   if ( ! $Admins ) {
-    return "N";
+    return 'N';
   } else if (in_array ($values, $Admins)) {
-    return "Y";
+    return 'Y';
   } else {
-    return "N";
+    return 'N';
   }
 }
 
 // Searches $ldap_admin_group_name and returns an array of the group members.
 // Do this search only once per request.
+// returns: array of admins
 function get_admins() {
-  global $error, $ldap_server, $ldap_port;
-  global $ldap_admin_dn,$ldap_admin_pwd;
+  global $error, $ds, $ldap_user_attr, $cached_admins;
   global $ldap_admin_group_name,$ldap_admin_group_attr,$ldap_admin_group_type;
-  global $cached_admins;
 
-  if ( ! empty ( $cached_admins ) )
-    return $cached_admins;
-
+  if ( ! empty ( $cached_admins ) ) return $cached_admins;
   $cached_admins = array ();
 
-  $ds = @ldap_connect ( $ldap_server, $ldap_port );
-  if ( !$ds ) {
-    $error = "Error connecting to LDAP server";
-  } else {
-    if ( $ds ) {
-      if ( $ldap_admin_dn != "") {
-        // bind as administrator
-        $r = @ldap_bind ( $ds, $ldap_admin_dn, $ldap_admin_pwd );
-      } else {
-        $r = @ldap_bind ( $ds ); // bind as anonymous
+  if ($r = connect_and_bind()) {
+    $search_filter = "($ldap_admin_group_attr=*)";
+    $sr = @ldap_search ( $ds, $ldap_admin_group_name, $search_filter, $ldap_user_attr );
+    if (!$sr) {
+      $error = 'Error searching LDAP server: ' . ldap_error();
+    } else {
+      $admins = ldap_get_entries( $ds, $sr );
+      for( $x = 0; $x <= $admins[0][$ldap_admin_group_attr]['count']; $x ++ ) {
+       if ($ldap_admin_group_type != 'posixgroup') {
+          $cached_admins[] = stripdn($admins[0][$ldap_admin_group_attr][$x]);
+        } else {
+          $cached_admins[] = $admins[0][$ldap_admin_group_attr][$x];
+        }
       }
-      if (!$r) {
-        $error = "Invalid Admin's login for LDAP Server";
-      } else {
-	$search_filter = "($ldap_admin_group_attr=*)";
-        $sr = @ldap_search ( $ds, $ldap_admin_group_name, $search_filter );
-	$admins = ldap_get_entries( $ds, $sr );
-        for( $x = 0; $x <= $admins[0][$ldap_admin_group_attr]["count"]; $x ++ ) {
-	  if (strtolower($ldap_admin_group_type) != 'posixgroup') {
-            $cached_admins[] = stripdn($admins[0][$ldap_admin_group_attr][$x]);
-          } else {
-            $cached_admins[] = $admins[0][$ldap_admin_group_attr][$x];
-          }
-	}
-        @ldap_free_result($sr);
-      }
-      @ldap_close ( $ds );
+      @ldap_free_result($sr);
     }
+    @ldap_close ( $ds );
   }
-
-  $cached_admins_found = true;
   return $cached_admins;
 }
 
 // Strip everything but the username (uid) from a dn.
 //  params:
 //    $dn - the dn you want to strip the uid from.
+//  returns: string - userid
 //
 //  ex: stripdn(uid=jeffh,ou=people,dc=example,dc=com) returns jeffh
 function stripdn($dn){
-  list ($uid,$trash) = split (",", $dn, 2);
-  list ($trash,$user) = split ("=", $uid);
+  list ($uid,$trash) = split (',', $dn, 2);
+  list ($trash,$user) = split ('=', $uid);
   return($user);
 }
+
+// Connects and binds to the LDAP server
+// Tries to connect as $ldap_admin_dn if we set it.
+//  returns: bind result or false
+function connect_and_bind() {
+  global $ds, $error, $ldap_server, $ldap_port, $ldap_admin_dn, $ldap_admin_pwd;
+
+  $ret = false;
+  $ds = @ldap_connect ( $ldap_server, $ldap_port );
+  if ( $ds ) {
+    if ( $ldap_admin_dn != '') {
+      $r = @ldap_bind ( $ds, $ldap_admin_dn, $ldap_admin_pwd );
+    } else {
+      $r = @ldap_bind ( $ds );
+    }
+    if (!$r) {
+      $error = 'Invalid Admin login for LDAP Server';
+    } else {
+      $ret = $r;
+    }
+  } else {
+    $error = 'Error connecting to LDAP server';
+    $ret = false;
+  }
+  return $ret;
+}
+
 ?>
