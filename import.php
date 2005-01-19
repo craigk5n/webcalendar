be300c2e1a67415ba076b48b24e9fb2690723798
Craig Knudsen <craig@k5n.us>
2005-01-19 15:43:51 +0000

    Fixed bug: vCal import was completely broken (looking for type 'VCAL' when for was using 'vcal')

--- import.php
+++ import.php
@@ -86,7 +86,7 @@ function print_user_list () {
 	<label for="importtype"><?php etranslate("Import format")?>:</label></td><td>
 		<select name="ImportType" id="importtype">
 			<option value="PALMDESKTOP">Palm Desktop &lt; v4.1.1</option>
-			<option value="vcal">vCal</option>
+			<option value="VCAL">vCal</option>
 			<option value="ICAL">iCal</option>
 		</select>
 </td></tr>
