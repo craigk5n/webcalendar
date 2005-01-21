eb0d243858706665852f85c56e0cc4a5080865b2
Craig Knudsen <craig@k5n.us>
2005-01-21 05:34:34 +0000

    Updated to work with extractfaqs.html, a tool that pulls all the FAQs from the documents for use in another web page.

---
+++ docs/extractfaqs.pl
@@ -0,0 +1,130 @@
+#!/usr/bin/perl
+#
+# extractfaqs.pl
+#
+# Description:
+#	Extract FAQs from all the user documentation to create a single
+#	document that contains all FAQs.
+#	We don't put HTML header/trailer stuff since the intent is to
+#	embed the results into some other web page.
+#
+#
+# History:
+#	20-Jan-2005	Craig Knudsen <cknudsen@cknudsen.com>
+#			Created
+#
+#######################################################################
+
+
+my @files = ( );
+my @questions = ( );
+my @answers = ( );
+my @file = ( );
+
+for ( $i = 0; $i < @ARGV; $i++ ) {
+  if ( -f $ARGV[$i] ) {
+    push ( @files, $ARGV[$i] )
+  } else {
+    print STDERR "Ignoring $ARGV[$i]\n";
+  }
+}
+
+foreach $f ( @files )
+{
+  process_file ( $f );
+}
+
+# Do some regex replacements to both questions and answers.
+for ( $i = 0; $i < @questions; $i++ ) {
+  $questions[$i];
+  # remove the "new window" icons
+  $questions[$i] =~ s/<a href=\"\S+\"[^>]+><img[^>]+><\/a>//g;
+  $answers[$i] =~ s/<a href=\"\S+\"[^>]+><img[^>]+><\/a>//g;
+  # For 
+  if ( $answers[$i] =~ /<a href="#([a-z]+)">/ ) {
+    $answers[$i] = $` . "<a href=\"" .
+      makeCVSURL ( $file[$i], $1 ) . "\">" . $';
+  }
+  $answers[$i] =~ s/<a href=\"\S+\"[^>]+><img[^>]+><\/a>//g;
+}
+
+print "<ul>\n";
+for ( $i = 0; $i < @questions; $i++ ) {
+  $anchor = "faq_" . ( $i + 1 );
+  $q = $questions[$i];
+  $q =~ s/<a href=\"\S+\"[^>]+>.+<\/a>//g;
+  print "  <li><a href=\"#$anchor\">$q</a></li>\n";
+}
+print "</ul>\n<hr/>\n<dl>\n";
+
+for ( $i = 0; $i < @questions; $i++ ) {
+  $anchor = "faq_" . ( $i + 1 );
+  print "<dt><a name=\"$anchor\">$q</a></dt>\n";
+  print "<dd>$answers[$i]</dd>\n";
+}
+print "</dl>\n";
+
+
+exit 0;
+
+#######################################################################
+# Subroutines start below here...
+#######################################################################
+
+sub makeCVSURL {
+  my ( $f, $anchor ) = @_;
+
+  return "http://cvs.sourceforge.net/viewcvs.py/*checkout*/webcalendar/webcalendar/docs/" . $f .
+    "?rev=HEAD&content-type=text/html#" . $anchor;
+}
+
+
+sub process_file {
+  my ( $f ) = @_;
+
+  open ( F, $f ) || die "Error reading $f: ";
+  my $inFAQ = 0;
+  my $text = '';
+  while ( <F> ) {
+    if ( /START FAQ/ ) {
+      $inFAQ = 1;
+    } elsif ( /END FAQ/ ) {
+      $inFAQ = 0;
+    } else {
+      if ( $inFAQ ) {
+        $text .= $_;
+      }
+    }
+  }
+  close ( F );
+
+  # Now parse the text
+  @q = split ( /<dt>/, $text );
+  shift ( @q ); # ignore junk at beginning
+  foreach $q ( @q ) {
+    if ( $q =~ /<\/dt>/ ) {
+      $thisQ = $`;
+      $rest = $';
+      if ( $rest =~ /<dd>/ ) {
+        $rest = $';
+        if ( $rest =~ /<\/dd>/ ) {
+          $thisA = $`;
+          push ( @questions, $thisQ );
+          push ( @answers, $thisA );
+          push ( @file, $f );
+          #print STDERR "Question successfully parsed.\n";
+        } else {
+          print STDERR "Found no </dd> for question: $thisQ\n";
+        }
+      } else {
+        print STDERR "Found no <dd> for question: $thisQ\n";
+      }
+    } else {
+      print STDERR "Found no </dt>";
+    }
+  }
+}
+
+
+
+
