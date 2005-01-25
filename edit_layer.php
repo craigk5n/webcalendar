2d6733e1baea5c0d1021d14931ef5c5e6852d562
musus <musus>
2005-01-25 02:39:45 +0000

    making code easier to read, adding title attribute to links

--- edit_layer.php
+++ edit_layer.php
@@ -25,6 +25,7 @@ print_header($INC);
 <h2><?php
 if ( $updating_public )
   echo translate($PUBLIC_ACCESS_FULLNAME) . " ";
+
 if ( ! empty ( $layers[$id]['cal_layeruser'] ) )
   etranslate("Edit Layer");
 else
@@ -64,7 +65,7 @@ if ( $single_user == "N" ) {
   else if ( $size > 5 )
     $size = 5;
   if ( $size >= 1 ) {
-	print "<tr><td style=\"vertical-align:top; font-weight:bold;\">\n<label for=\"layeruser\">" .
+	print "<tr><td style=\"vertical-align:top;\">\n<label for=\"layeruser\">" .
       translate("Source") . ":</label></td><td>\n";
     print "<select name=\"layeruser\" id=\"layeruser\" size=\"1\">$users\n";
     print "</select>\n";
@@ -72,18 +73,18 @@ if ( $single_user == "N" ) {
   }
 }
 ?>
-
 <tr><td>
 	<label for="layercolor"><?php etranslate("Color")?>:</label></td><td>
 	<input type="text" name="layercolor" id="layercolor" size="7" maxlength="7" value="<?php echo empty ( $layers[$id]['cal_color'] ) ? "" :  $layers[$id]['cal_color']; ?>" />
 	<input type="button" onclick="selectColor('layercolor')" value="<?php etranslate("Select")?>..." />
 </td></tr>
-
 <tr><td style="font-weight:bold;">
 	<?php etranslate("Duplicates")?>:</td><td>
-	<label><input type="checkbox" name="dups" value="Y" <?php if ( ! empty ( $layers[$id]['cal_dups'] ) && $layers[$id]['cal_dups'] == 'Y') echo " checked=\"checked\""; ?> />&nbsp;<?php etranslate("Show layer events that are the same as your own")?></label>
+	<label><input type="checkbox" name="dups" value="Y"<?php 
+		if (! empty ($layers[$id]['cal_dups']) && $layers[$id]['cal_dups'] == 'Y') 
+			echo " checked=\"checked\"";
+	?> />&nbsp;<?php etranslate("Show layer events that are the same as your own")?></label>
 </td></tr>
-
 <tr><td colspan="2">
 	<input type="submit" value="<?php etranslate("Save")?>" />
 </td></tr>
@@ -91,11 +92,9 @@ if ( $single_user == "N" ) {
 // If a layer already exists put a 'Delete Layer' link
 if ( ! empty ( $layers[$id]['cal_layeruser'] ) ) { ?>
 <tr><td>
-	<br /><a href="del_layer.php?id=<?php echo $id; if ( $updating_public ) echo "&amp;public=1"; ?>" onclick="return confirm('<?php etranslate("Are you sure you want to delete this layer?")?>');"><?php etranslate("Delete layer")?></a><br />
+	<br /><a title="<?php etranslate("Delete layer")?>" href="del_layer.php?id=<?php echo $id; if ( $updating_public ) echo "&amp;public=1"; ?>" onclick="return confirm('<?php etranslate("Are you sure you want to delete this layer?")?>');"><?php etranslate("Delete layer")?></a><br />
 </td></tr>
-<?php
-}  // end 'Delete Layer' link
-?>
+<?php }  // end 'Delete Layer' link ?>
 </table>
 
 <?php if ( ! empty ( $layers[$id]['cal_layeruser'] ) )
