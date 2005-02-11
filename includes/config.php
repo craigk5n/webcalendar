9f4ab8a5bddeceab032f5cc8978617b35bdc0dfe
Craig Knudsen <craig@k5n.us>
2005-02-11 21:01:38 +0000

    Purge events was not deleting from webcal_entry_ext_user table.

--- includes/config.php
+++ includes/config.php
@@ -25,8 +25,8 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
  */
 
 
-$PROGRAM_VERSION = "v1.0RC2";
-$PROGRAM_DATE = "09 Feb 2005";
+$PROGRAM_VERSION = "v1.0RC3+CVS";
+$PROGRAM_DATE = "?? ??? 2005";
 $PROGRAM_NAME = "WebCalendar $PROGRAM_VERSION ($PROGRAM_DATE)";
 $PROGRAM_URL = "http://webcalendar.sourceforge.net/";
 
