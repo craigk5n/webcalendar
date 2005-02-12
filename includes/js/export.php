c9d697c0e6971521b6e54ca0002bee71edf17689
umcesrjones <umcesrjones>
2005-02-12 04:42:44 +0000

    Fixed PHP warnings for undefined variables and made popups follow cursor

--- includes/js/export.php
+++ includes/js/export.php
@@ -1,19 +1,28 @@
 <script type="text/javascript">
 <!-- <![CDATA[
-function selectDate ( day, month, year ) {
+function selectDate ( day, month, year, current, evt ) {
   // get currently selected month/year
   monthobj = eval ( 'document.exportform.' + month );
   curmonth = monthobj.options[monthobj.selectedIndex].value;
   yearobj = eval ( 'document.exportform.' + year );
   curyear = yearobj.options[yearobj.selectedIndex].value;
   date = curyear;
-  if ( curmonth < 10 )
+  if (document.getElementById) {
+    mX = evt.clientX   + 40;
+    mY = evt.clientY  + 120;
+  }
+  else {
+    mX = evt.pageX + 40;
+    mY = evt.pageY +130;
+  }
+	var MyPosition = 'scrollbars=no,toolbar=no,left=' + mX + ',top=' + mY + ',screenx=' + mX + ',screeny=' + mY ;	
+		if ( curmonth < 10 )
     date += "0";
   date += curmonth;
   date += "01";
-  url = "datesel.php?form=exportform&day=" + day +
-    "&month=" + month + "&year=" + year + "&date=" + date;
-  var colorWindow = window.open(url,"DateSelection","width=300,height=200,resizable=yes,scrollbars=yes");
+  url = "datesel.php?form=exportform&fday=" + day +
+    "&fmonth=" + month + "&fyear=" + year + "&date=" + date;
+  var colorWindow = window.open(url,"DateSelection","width=300,height=200," + MyPosition);
 }
 
 <?php //see the showTab function in includes/js/visible.php for common code shared by all pages
