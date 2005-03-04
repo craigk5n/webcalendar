fe036ec35a8934084f801cff6bb9b15eff16a5c6
umcesrjones <umcesrjones>
2005-03-04 22:06:00 +0000

    Fixed bug 1152863: Japanese strings munged and coding standards cleanup

--- includes/trailer.php
+++ includes/trailer.php
@@ -17,11 +17,13 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
 <div id="trailer">
 <form action="month.php" method="get" name="SelectMonth" id="monthform">
 <?php
-  if ( ! empty ( $user ) && $user != $login )
+  if ( ! empty ( $user ) && $user != $login ) {
     echo "<input type=\"hidden\" name=\"user\" value=\"$user\" />\n";
+  }
   if ( ! empty ( $cat_id ) && $categories_enabled == "Y"
-    && ( ! $user || $user == $login ) )
+    && ( ! $user || $user == $login ) ) {
     echo "<input type=\"hidden\" name=\"cat_id\" value=\"$cat_id\" />\n";
+  }
 ?>
 <label for="monthselect"><?php etranslate("Month")?>:&nbsp;</label>
 <select name="date" id="monthselect" onchange="document.SelectMonth.submit()">
@@ -44,8 +46,9 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
     }
     $d = mktime ( 3, 0, 0, $m, 1, $y );
     echo "<option value=\"" . date ( "Ymd", $d ) . "\"";
-    if ( date ( "Ymd", $d ) == $thisdate )
+    if ( date ( "Ymd", $d ) == $thisdate ) {
       echo " selected=\"selected\"";
+    }
     echo ">";
     echo date_to_str ( date ( "Ymd", $d ), $DATE_FORMAT_MY, false, true );
     echo "</option>\n";
@@ -57,11 +60,13 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
 
 <form action="week.php" method="get" name="SelectWeek" id="weekform">
 <?php
-  if ( ! empty ( $user ) && $user != $login )
+  if ( ! empty ( $user ) && $user != $login ) {
     echo "<input type=\"hidden\" name=\"user\" value=\"$user\" />\n";
+  }
   if ( ! empty ( $cat_id ) && $categories_enabled == "Y"
-    && ( ! $user || $user == $login ) )
+    && ( ! $user || $user == $login ) ) {
     echo "<input type=\"hidden\" name=\"cat_id\" value=\"$cat_id\" />\n";
+  }
 ?>
 <label for="weekselect"><?php etranslate("Week")?>:&nbsp;</label>
 <select name="date" id="weekselect" onchange="document.SelectWeek.submit()">
@@ -81,17 +86,19 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
   $d_time = mktime ( 3, 0, 0, $m, $d, $y );
   $thisdate = date ( "Ymd", $d_time );
   $wday = date ( "w", $d_time );
-  if ( $WEEK_START == 1 )
+  if ( $WEEK_START == 1 ) {
     $wkstart = mktime ( 3, 0, 0, $m, $d - ( $wday - 1 ), $y );
-  else
+  } else {
     $wkstart = mktime ( 3, 0, 0, $m, $d - $wday, $y );
+  }
   for ( $i = -7; $i <= 7; $i++ ) {
     $twkstart = $wkstart + ( 3600 * 24 * 7 * $i );
     $twkend = $twkstart + ( 3600 * 24 * 6 );
     echo "<option value=\"" . date ( "Ymd", $twkstart ) . "\"";
     if ( date ( "Ymd", $twkstart ) <= $thisdate &&
-      date ( "Ymd", $twkend ) >= $thisdate )
+      date ( "Ymd", $twkend ) >= $thisdate ) {
       echo " selected=\"selected\"";
+    }
     echo ">";
     printf ( "%s - %s",
       date_to_str ( date ( "Ymd", $twkstart ), $DATE_FORMAT_MD, false, true ),
@@ -105,11 +112,13 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
 
 <form action="year.php" method="get" name="SelectYear" id="yearform">
 <?php
-  if ( ! empty ( $user ) && $user != $login )
+  if ( ! empty ( $user ) && $user != $login ) {
     echo "<input type=\"hidden\" name=\"user\" value=\"$user\" />\n";
+  }
   if ( ! empty ( $cat_id ) && $categories_enabled == "Y"
-    && ( ! $user || $user == $login ) )
+    && ( ! $user || $user == $login ) ) {
     echo "<input type=\"hidden\" name=\"cat_id\" value=\"$cat_id\" />\n";
+  }
 ?>
 <label for="yearselect"><?php etranslate("Year")?>:&nbsp;</label>
 <select name="year" id="yearselect" onchange="document.SelectYear.submit()">
@@ -121,8 +130,9 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
   }
   for ( $i = $y - 4; $i < $y + 4; $i++ ) {
     echo "<option value=\"$i\"";
-    if ( $i == $y )
+    if ( $i == $y ) {
       echo " selected=\"selected\"";
+    }
     echo ">$i</option>\n";
   }
 ?>
@@ -135,103 +145,125 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
 <?php
   $can_add = ( $readonly == "N" );
   if ( $public_access == "Y" && $public_access_can_add != "Y" &&
-    $login == "__public__" )
+    $login == "__public__" ) {
     $can_add = false;
+  }
 
-  if ( ! empty ( $GLOBALS['STARTVIEW'] ) )
+  if ( ! empty ( $GLOBALS['STARTVIEW'] ) ) {
     $mycal = $GLOBALS['STARTVIEW'];
-  else
+  } else {
     $mycal = "index.php";
+  }
 
   // calc URL to today
   $todayURL = 'month.php';
   $reqURI = 'month.php';
-  if ( ! empty ( $GLOBALS['SCRIPT_NAME'] ) )
+  if ( ! empty ( $GLOBALS['SCRIPT_NAME'] ) ) {
     $reqURI = $GLOBALS['SCRIPT_NAME'];
-  else if ( ! empty ( $_SERVER['SCRIPT_NAME'] ) )
+  } else if ( ! empty ( $_SERVER['SCRIPT_NAME'] ) ) {
     $reqURI = $_SERVER['SCRIPT_NAME'];
+  }
   if ( ! strstr ( $reqURI, "month.php" ) &&
      ! strstr ( $reqURI, "week.php" ) &&
-     ! strstr ( $reqURI, "day.php" ) )
+     ! strstr ( $reqURI, "day.php" ) ) {
     $todayURL = 'day.php';
-  else
+  } else {
     $todayURL = $reqURI;
+  }
 
   if ( $single_user != "Y" ) {
-    if ( ! empty ( $user ) && $user != $login )
+    if ( ! empty ( $user ) && $user != $login ) {
       echo "<a title=\"" . 
-	translate("My Calendar") . "\" style=\"font-weight:bold;\" href=\"$mycal\">" . 
-	translate("Back to My Calendar") . "</a>";
-    else
+        translate("My Calendar") . "\" style=\"font-weight:bold;\" " .
+        "href=\"$mycal\">" . 
+        translate("Back to My Calendar") . "</a>";
+    } else {
       echo "<a title=\"" . 
-	translate("My Calendar") . "\" style=\"font-weight:bold;\" href=\"$mycal\">" . 
-	translate("My Calendar") . "</a>\n";
-
-    if ( ! empty ( $user ) && $user != $login )
+        translate("My Calendar") . "\" style=\"font-weight:bold;\" " .
+        "href=\"$mycal\">" . 
+        translate("My Calendar") . "</a>\n";
+    }
+    if ( ! empty ( $user ) && $user != $login ) {
       $todayURL .= '?user=' . $user;
+    }
     echo " | <a title=\"" . 
-    	translate("Today") . "\" style=\"font-weight:bold;\" href=\"$todayURL\">" . 
-	translate("Today") . "</a>\n";
-
-    if ( $login != '__public__' && $readonly == 'N' )
+      translate("Today") . "\" style=\"font-weight:bold;\" " .
+      "href=\"$todayURL\">" . 
+      translate("Today") . "</a>\n";
+    if ( $login != '__public__' && $readonly == 'N' ) {
       echo " | <a title=\"" . 
-	translate("Admin") . "\" style=\"font-weight:bold;\" href=\"adminhome.php\">" . 
-	translate("Admin") . "</a>\n";
+        translate("Admin") . "\" style=\"font-weight:bold;\" " .
+        "href=\"adminhome.php\">" . 
+        translate("Admin") . "</a>\n";
+    }
     if ( $login != "__public__" && $readonly == "N" &&
       ( $require_approvals == "Y" || $public_access == "Y" ) ) {
-	$url = 'list_unapproved.php';
-        if ($is_nonuser_admin) $url .= "?user=$user";
-	echo " | <a title=\"" . 
-		translate("Unapproved Events") . "\" href=\"$url\">" . 
-		translate("Unapproved Events") . "</a>\n";
+      $url = 'list_unapproved.php';
+      if ($is_nonuser_admin) {
+        $url .= "?user=$user";
+      }
+      echo " | <a title=\"" . 
+        translate("Unapproved Events") . "\" href=\"$url\">" . 
+        translate("Unapproved Events") . "</a>\n";
     }
     if ( $login == "__public__" && $public_access_others != "Y" ) {
       // don't allow them to see other people's calendar
-    } else if ( $allow_view_other == "Y" || $is_admin )
+    } else if ( $allow_view_other == "Y" || $is_admin ) {
       echo " | <a title=\"" . 
-		translate("Another User's Calendar") . "\" href=\"select_user.php\">" . 
-		translate("Another User's Calendar") . "</a>\n";
+        translate("Another User's Calendar") . "\" href=\"select_user.php\">" . 
+        translate("Another User's Calendar") . "</a>\n";
+    }
   } else {
     echo "<a title=\"" . 
-		translate("My Calendar") . "\" style=\"font-weight:bold;\" href=\"$mycal\">" . 
-		translate("My Calendar") . "</a>\n";
+      translate("My Calendar") . "\" style=\"font-weight:bold;\" " .
+      "href=\"$mycal\">" . 
+      translate("My Calendar") . "</a>\n";
     echo " | <a title=\"" . 
-		translate("Today") . "\" style=\"font-weight:bold;\" href=\"$todayURL\">" . 
-		translate("Today") . "</a>\n";
+      translate("Today") . "\" style=\"font-weight:bold;\" " .
+      "href=\"$todayURL\">" . 
+      translate("Today") . "</a>\n";
     if ( $readonly == 'N' ) {
       echo " | <a title=\"" . 
-		translate("Admin") . "\" style=\"font-weight:bold;\" href=\"adminhome.php\">" . 
-		translate("Admin") . "</a>\n";
+        translate("Admin") . "\" style=\"font-weight:bold;\" " .
+        "href=\"adminhome.php\">" . 
+        translate("Admin") . "</a>\n";
     }
   }
   // only display some links if we're viewing our own calendar.
   if ( empty ( $user ) || $user == $login ) {
     echo " | <a title=\"" . 
-	translate("Search") . "\" href=\"search.php\">" .
-	translate("Search") . "</a>\n";
-    if ( $login != '__public__' )
+      translate("Search") . "\" href=\"search.php\">" .
+      translate("Search") . "</a>\n";
+    if ( $login != '__public__' ) {
       echo " | <a title=\"" . 
-    	translate("Import") . "/" . translate("Export") . "\" href=\"import.php\">" . 
-	translate("Import") . "/" . translate("Export") . "</a>\n";
+        translate("Import") . "/" . translate("Export") . "\ " .
+        "href=\"import.php\">" . 
+        translate("Import") . "/" . translate("Export") . "</a>\n";
+    }
     if ( $can_add ) {
       echo " | <a title=\"" . 
-	translate("Add New Entry") . "\" href=\"edit_entry.php";
+        translate("Add New Entry") . "\" href=\"edit_entry.php";
       if ( ! empty ( $thisyear ) ) {
         print "?year=$thisyear";
-        if ( ! empty ( $thismonth ) )
+        if ( ! empty ( $thismonth ) ) {
           print "&amp;month=$thismonth";
-        if ( ! empty ( $thisday ) )
+        }
+        if ( ! empty ( $thisday ) ) {
           print "&amp;day=$thisday";
+        }
       }
       echo "\">" . 
-	translate("Add New Entry") . "</a>\n";
+        translate("Add New Entry") . "</a>\n";
     }
   }
   if ( $login != '__public__' ) {
     echo " | <a title=\"" . 
-	translate("Help") . "\" href=\"#\" onclick=\"window.open ( 'help_index.php', 'cal_help', 'dependent,menubar,scrollbars,height=400,width=400,innerHeight=420,outerWidth=420' );\" onmouseover=\"window.status='" . 
-	translate("Help") . "'\">" . 
-	translate("Help") . "</a>\n";
+      translate("Help") . "\" href=\"#\" onclick=\"window.open 
+      ( 'help_index.php', 'cal_help', 'dependent,menubar,scrollbars,
+      height=400,width=400,innerHeight=420,outerWidth=420' );\" 
+      onmouseover=\"window.status='" . 
+      translate("Help") . "'\">" . 
+      translate("Help") . "</a>\n";
   }
 ?>
 
@@ -244,7 +276,7 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
     if ( $i > 0 )
       echo " | ";
     echo "<a title=\"" . 
-	$views[$i]['cal_name'] . "\" href=\"";
+ $views[$i]['cal_name'] . "\" href=\"";
     if ( $views[$i]['cal_view_type'] == 'W' )
       echo "view_w.php?";
     elseif ( $views[$i]['cal_view_type'] == 'D' )
@@ -265,7 +297,7 @@ if ( ! empty ( $PHP_SELF ) && preg_match ( "/\/includes\//", $PHP_SELF ) ) {
     if ( ! empty ( $thisdate ) )
       echo "&amp;date=$thisdate";
     echo "\">" . 
-	$views[$i]['cal_name'] . "</a>\n";
+ $views[$i]['cal_name'] . "</a>\n";
   }
 ?>
 
@@ -286,15 +318,18 @@ if ( ! empty ( $user ) && $user != $login ) {
   $u_url = "";
 }
 if ( $res ) {
+  // Pass the current charset to avoid errors using Japanese
+  $charset = ( ! empty ( $LANGUAGE )?translate("charset"): "iso-8859-1" );
   while ( $row = dbi_fetch_row ( $res ) ) {
     if ( $found_report )
       echo " | ";
     else
       echo "<span class=\"prefix\">" . 
-      	translate("Reports") . ":</span>&nbsp;";
+       translate("Reports") . ":</span>&nbsp;";
     echo "<a title=\"" . 
-	htmlentities ( $row[0] ) . "\" href=\"report.php?report_id=$row[1]$u_url\">" . 
-	htmlentities ( $row[0] ) . "</a>\n";
+ htmlentities ( $row[0], ENT_COMPAT, $charset ) . 
+   "\" href=\"report.php?report_id=$row[1]$u_url\">" . 
+ htmlentities ( $row[0], ENT_COMPAT, $charset ) . "</a>\n";
     $found_report = true;
   }
   dbi_free_result ( $res );
@@ -306,10 +341,10 @@ if ( $res ) {
 <br />
 <?php
 if ( ! $use_http_auth ) {
-	if ( empty ( $login_return_path ) )
-		$login_url = "login.php";
-	else
-		$login_url = "login.php?return_path=$login_return_path";
+ if ( empty ( $login_return_path ) )
+  $login_url = "login.php";
+ else
+  $login_url = "login.php?return_path=$login_return_path";
 
   // Should we use another application's login/logout pages?
   if ( substr($GLOBALS['user_inc'],0,9) == 'user-app-' ) {  
@@ -324,18 +359,18 @@ if ( ! $use_http_auth ) {
     }
   }  
     
-	if ( strlen ( $login ) && $login != "__public__" ) {
-		echo "<span class=\"prefix\">" .
-			translate("Current User") . ":</span>&nbsp;$fullname&nbsp;(<a title=\"" . 
-			translate("Logout") . "\" href=\"$login_url\">" . 
-			translate("Logout") . "</a>)<br />\n";
-	} else {
-		echo "<span class=\"prefix\">" .
-			translate("Current User") . ":</span>&nbsp;" . 
-			translate("Public Access") . "&nbsp;(<a title=\"" . 
-			translate("Login") . "\" href=\"$login_url\">" . 
-			translate("Login") . "</a>)<br />\n";
-	}
+ if ( strlen ( $login ) && $login != "__public__" ) {
+  echo "<span class=\"prefix\">" .
+   translate("Current User") . ":</span>&nbsp;$fullname&nbsp;(<a title=\"" . 
+   translate("Logout") . "\" href=\"$login_url\">" . 
+   translate("Logout") . "</a>)<br />\n";
+ } else {
+  echo "<span class=\"prefix\">" .
+   translate("Current User") . ":</span>&nbsp;" . 
+   translate("Public Access") . "&nbsp;(<a title=\"" . 
+   translate("Login") . "\" href=\"$login_url\">" . 
+   translate("Login") . "</a>)<br />\n";
+ }
 }
   if ($nonuser_enabled == "Y" ) $admincals = get_nonuser_cals ($login);
   if ( $has_boss || ! empty ( $admincals[0] ) ) {
@@ -353,13 +388,15 @@ if ( ! $use_http_auth ) {
       // not use a user-created view because it might not display the
       // proper user's events.  (Fallback to month.php if this is true.)
       $xurl = get_preferred_view ( "", "user=$l" );
-      if ( strstr ( $xurl, "view_" ) )
+      if ( strstr ( $xurl, "view_" ) ) {
         $xurl = "month.php?user=$l";
+      }
       $groups .= "<a title=\"$f\" href=\"$xurl\">$f</a>";
     }
     print $groups;
   }
-  print "<br />\n<a title=\"" . $GLOBALS['PROGRAM_NAME'] . "\" id=\"programname\" href=\"$GLOBALS[PROGRAM_URL]\" target=\"_new\">" .
+  print "<br />\n<a title=\"" . $GLOBALS['PROGRAM_NAME'] . "\" " .
+    "id=\"programname\" href=\"$GLOBALS[PROGRAM_URL]\" target=\"_new\">" .
     $GLOBALS['PROGRAM_NAME'] . "</a>\n";
 ?>
 </div>
