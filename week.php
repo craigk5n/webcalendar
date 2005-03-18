52e4a8f9e0caae738db8971783d088175ed2243f
umcesrjones <umcesrjones>
2005-03-18 20:46:57 +0000

    Applied bug fix 1151442: Overlapping events display error in week.php

--- week.php
+++ week.php
@@ -43,7 +43,8 @@ if ( ! empty ( $auto_refresh ) && $auto_refresh == "Y" &&
   ! empty ( $auto_refresh_time ) ) {
   $refresh = $auto_refresh_time * 60; // convert to seconds
   $HeadX = "<meta http-equiv=\"refresh\" content=\"$refresh; url=week.php?$u_url" .
-    "date=$startdate$caturl" . ( ! empty ( $friendly ) ? "&amp;friendly=1" : "") . "\" />\n";
+    "date=$startdate$caturl" . 
+    ( ! empty ( $friendly ) ? "&amp;friendly=1" : "") . "\" />\n";
 }
 $INC = array('js/popups.php');
 print_header($INC,$HeadX);
@@ -60,7 +61,7 @@ for ( $i = 0; $i < 7; $i++ ) {
   $days[$i] = $wkstart + ( 24 * 3600 ) * $i;
   $weekdays[$i] = weekday_short_name ( ( $i + $WEEK_START ) % 7 );
   $header[$i] = $weekdays[$i] . "<br />\n" .
-     date_to_str ( date ( "Ymd", $days[$i] ), $DATE_FORMAT_MD, false );
+    date_to_str ( date ( "Ymd", $days[$i] ), $DATE_FORMAT_MD, false );
 }
 ?>
 
@@ -76,22 +77,32 @@ if ( $GLOBALS["DISPLAY_WEEKNUMBER"] == "Y" ) {
     translate("Week") . " " . week_number ( $wkstart ) . ")</span>";
 }
 ?>
-<a title="<?php etranslate("Previous")?>" class="prev" href="week.php?<?php echo $u_url; ?>date=<?php echo date("Ymd", $prev ) . $caturl;?>"><img src="leftarrow.gif" alt="<?php etranslate("Previous")?>" /></a>
+<a title="<?php etranslate("Previous")?>" 
+class="prev" href="week.php?<?php echo $u_url; ?>date=<?php echo 
+  date("Ymd", $prev ) . $caturl;?>"><img src="leftarrow.gif" 
+  alt="<?php etranslate("Previous")?>" /></a>
 
-<a title="<?php etranslate("Next")?>" class="next" href="week.php?<?php echo $u_url;?>date=<?php echo date ("Ymd", $next ) . $caturl;?>"><img src="rightarrow.gif" alt="<?php etranslate("Next")?>" /></a>
+<a title="<?php etranslate("Next")?>" class="next" 
+href="week.php?<?php echo $u_url;?>date=<?php echo 
+  date ("Ymd", $next ) . $caturl;?>"><img src="rightarrow.gif" 
+  alt="<?php etranslate("Next")?>" /></a>
 <span class="user"><?php
   if ( $single_user == "N" ) {
     echo "<br />$user_fullname";
   }
-  if ( $is_nonuser_admin )
+  if ( $is_nonuser_admin ) {
     echo "<br />-- " . translate("Admin mode") . " --";
-  if ( $is_assistant )
+  }
+  if ( $is_assistant ) {
     echo "<br />-- " . translate("Assistant mode") . " --";
+  }
 ?></span>
 <?php
-  if ( $categories_enabled == "Y" && (!$user || ($user == $login || $is_assistant ))) {
+  if ( $categories_enabled == "Y" && (!$user || ($user == $login || 
+    $is_assistant ))) {
     echo "<br /><br />\n";
-    print_category_menu('week', sprintf ( "%04d%02d%02d",$thisyear, $thismonth, $thisday ), $cat_id );
+    print_category_menu('week', sprintf ( "%04d%02d%02d",$thisyear, 
+      $thismonth, $thisday ), $cat_id );
   }
 ?></div>
 <br />
@@ -101,36 +112,36 @@ if ( $GLOBALS["DISPLAY_WEEKNUMBER"] == "Y" ) {
 <th class="empty">&nbsp;</th>
 <?php
 for ( $d = $start_ind; $d < $end_ind; $d++ ) {
-	$thiswday = date ( 'w', $days[$d] );
+  $thiswday = date ( 'w', $days[$d] );
 
-	$is_weekend = ( $thiswday == 0 || $thiswday == 6 );
+  $is_weekend = ( $thiswday == 0 || $thiswday == 6 );
 
-	if ( $is_weekend ) {
-		$class = "weekend";
-	} else {
-		$class = "";
-	}
+  if ( $is_weekend ) {
+    $class = "weekend";
+  } else {
+    $class = "";
+  }
 
-	if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
-		if ( $class != '' ) {
-			$class .= " ";
-		}
-		$class .= "today";
-	}
+  if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
+    if ( $class != '' ) {
+     $class .= " ";
+    }
+    $class .= "today";
+  }
 
-	echo "<th";
+  echo "<th";
 
-	if ( $class != '') {
-		echo " class=\"$class\"";
-	}
-	echo ">";
+  if ( $class != '') {
+    echo " class=\"$class\"";
+  }
+  echo ">";
 
   if ( $can_add ) {
     echo html_for_add_icon (  date ( "Ymd", $days[$d] ), "", "", $user );
   }
-	echo "<a href=\"day.php?" . $u_url .
-		"date=" . date ('Ymd', $days[$d] ) . $caturl . "\">" .
-		$header[$d] . "</a></th>\n";
+  echo "<a href=\"day.php?" . $u_url .
+    "date=" . date ('Ymd', $days[$d] ) . $caturl . "\">" .
+    $header[$d] . "</a></th>\n";
 }
 ?>
 </tr>
@@ -147,8 +158,9 @@ $last_slot = (int)( ( ( $WORK_DAY_END_HOUR - $TZ_OFFSET ) * 60 ) / $interval );
 
 $untimed_found = false;
 $get_unapproved = ( $GLOBALS["DISPLAY_UNAPPROVED"] == "Y" );
-if ( $login == "__public__" )
+if ( $login == "__public__" ) {
   $get_unapproved = false;
+}
 
 $all_day = array ();
 for ( $d = $start_ind; $d < $end_ind; $d++ ) {
@@ -174,8 +186,9 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
           $viewid = $rep[$cur_rep]['cal_id'];
           $viewname = $rep[$cur_rep]['cal_name'];
         }
-        if ( $rep[$cur_rep]['cal_duration'] == ( 24 * 60 ) )
+        if ( $rep[$cur_rep]['cal_duration'] == ( 24 * 60 ) ) {
           $all_day[$d] = 1;
+        }
         html_for_event_week_at_a_glance ( $viewid,
           $date, $rep[$cur_rep]['cal_time'],
           $viewname, $rep[$cur_rep]['cal_description'],
@@ -194,8 +207,9 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
         $viewid = $ev[$i]['cal_id'];
         $viewname = $ev[$i]['cal_name'];
       }
-      if ( $ev[$i]['cal_duration'] == ( 24 * 60 ) )
+      if ( $ev[$i]['cal_duration'] == ( 24 * 60 ) ) {
         $all_day[$d] = 1;
+      }
       html_for_event_week_at_a_glance ( $viewid,
         $date, $ev[$i]['cal_time'],
         $viewname, $ev[$i]['cal_description'],
@@ -215,8 +229,9 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
         $viewid = $rep[$cur_rep]['cal_id'];
         $viewname = $rep[$cur_rep]['cal_name'];
       }
-      if ( $rep[$cur_rep]['cal_duration'] == ( 24 * 60 ) )
+      if ( $rep[$cur_rep]['cal_duration'] == ( 24 * 60 ) ) {
         $all_day[$d] = 1;
+      }
       html_for_event_week_at_a_glance ( $viewid,
         $date, $rep[$cur_rep]['cal_time'],
         $viewname, $rep[$cur_rep]['cal_description'],
@@ -236,7 +251,10 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
     if ( $rowspan > 1 ) {
       if ( ! empty ( $hour_arr[$i] ) ) {
         if ( $rowspan_arr[$i] > 1 ) {
-          $rowspan_arr[$last_row] += ( $rowspan_arr[$i] - 1 );
+          if ( $rowspan_arr[$i] >=  $rowspan_arr[$last_row] ) {
+            $rowspan_arr[$last_row] += ( $rowspan_arr[$i] -
+              $rowspan_arr[$last_row]  + 1 );
+          } 
           $rowspan += ( $rowspan_arr[$i] - 1 );
         } else
           $rowspan_arr[$last_row] += $rowspan_arr[$i];
@@ -244,8 +262,9 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
         // yet start on different hours
         $start_time = $i;
         $diff_start_time = $start_time - $last_row;
-        for ( $u = $diff_start_time ; $u > 0 ; $u-- ) 
+        for ( $u = $diff_start_time ; $u > 0 ; $u-- ) {
           $hour_arr[$last_row] .= "<br />\n"; 
+        }
         $hour_arr[$last_row] .= $hour_arr[$i];
         $hour_arr[$i] = "";
         $rowspan_arr[$i] = 0;
@@ -259,7 +278,7 @@ for ( $d = $start_ind; $d < $end_ind; $d++ ) {
 
   // now save the output...
   if ( ! empty ( $hour_arr[9999] ) && strlen ( $hour_arr[9999] ) ) {
-		$untimed[$d] = $hour_arr[9999];
+    $untimed[$d] = $hour_arr[9999];
     $untimed_found = true;
   }
   $save_hour_arr[$d] = $hour_arr;
@@ -271,45 +290,41 @@ if ( $untimed_found ) {
   echo "<tr>\n<th class=\"empty\">&nbsp;</th>\n";
   for ( $d = $start_ind; $d < $end_ind; $d++ ) {
     $thiswday = date ( "w", $days[$d] );
-
     $is_weekend = ( $thiswday == 0 || $thiswday == 6 );
-	if ( $is_weekend ) {
-		$class = "weekend";
-	} else {
-		$class = "";
-	}
+    $class = ( $is_weekend ? "weekend" : "" );
 
-	if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
-		if ($class != "") {
-			$class .= " ";
-		}
-		$class .= "today";
-	}
-	echo "<td";
+   if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
+     if ($class != "") {
+       $class .= " ";
+     }
+     $class .= "today";
+   }
+   echo "<td";
 
-	// Use the class 'hasevents' for any hour block that has events
-	// in it.
-	if ( ! empty ( $untimed[$d] ) && strlen ( $untimed[$d] ) ) {
-		$class = 'hasevents';
-	}
+   // Use the class 'hasevents' for any hour block that has events
+   // in it.
+   if ( ! empty ( $untimed[$d] ) && strlen ( $untimed[$d] ) ) {
+    $class = 'hasevents';
+   }
 
-	if ( $class != "" ) {
-		echo " class=\"$class\"";
-	}
-	echo ">";
+   if ( $class != "" ) {
+     echo " class=\"$class\"";
+   }
+   echo ">";
 
-	if ( ! empty ( $untimed[$d] ) && strlen ( $untimed[$d] ) ) {
-		echo $untimed[$d];
-	} else {
-		echo "&nbsp;";
-	}
-	echo "</td>\n";
+   if ( ! empty ( $untimed[$d] ) && strlen ( $untimed[$d] ) ) {
+     echo $untimed[$d];
+   } else {
+     echo "&nbsp;";
+   }
+   echo "</td>\n";
   }
   echo "</tr>\n";
 }
 
-for ( $d = $start_ind; $d < $end_ind; $d++ )
+for ( $d = $start_ind; $d < $end_ind; $d++ ) {
   $rowspan_day[$d] = 0;
+}
 
 for ( $i = $first_slot; $i <= $last_slot; $i++ ) {
   $time_h = (int) ( ( $i * $interval ) / 60 );
@@ -318,89 +333,86 @@ for ( $i = $first_slot; $i <= $last_slot; $i++ ) {
   echo "<tr>\n<th class=\"row\">" .  $time . "</th>\n";
   for ( $d = $start_ind; $d < $end_ind; $d++ ) {
     $thiswday = date ( "w", $days[$d] );
-
     $is_weekend = ( $thiswday == 0 || $thiswday == 6 );
-	if ( $is_weekend ) {
-		$class = "weekend";
-	} else {
-		$class = "";
-	}
-	if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
-		if ( $class != "" ) {
-			$class .= " ";
-		}
-		$class .= "today";
-	}
-
-	// Use the class 'hasevents' for any hour block that has events
-	// in it.
-	if ( ! empty ( $save_hour_arr[$d][$i] ) &&
-		strlen ( $save_hour_arr[$d][$i] ) ) {
-		$class = 'hasevents';
-	}
-
-    if ( $rowspan_day[$d] > 1 ) {
-      // this might mean there's an overlap, or it could mean one event
-      // ends at 11:15 and another starts at 11:30.
-	if ( ! empty ( $save_hour_arr[$d][$i] ) ) {
-		echo "<td";
-		if ( $class != '' ) {
-			echo " class=\"$class\"";
-		}
-		echo ">" . $save_hour_arr[$d][$i] . "</td>\n";
-	}
-      $rowspan_day[$d]--;
-    } else {
-      if ( empty ( $save_hour_arr[$d][$i] ) ) {
-	echo "<td";
-	if ( $class != "" ) {
-		echo " class=\"$class\"";
-	}
-	echo ">";
-	if ( $can_add ) { //if user can add events...
-          echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, $user ); //..then echo the add event icon
-	}
-       echo "&nbsp;</td>\n";
-      } else {
-        $rowspan_day[$d] = $save_rowspan_arr[$d][$i];
-        if ( $rowspan_day[$d] > 1 ) {
-		echo "<td";
-		if ( $class != '' ) {
-			echo " class=\"$class\"";
-		}
-		echo " rowspan=\"$rowspan_day[$d]\">";
-		if ( $can_add ) {
-			echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, $user );
-		}
-		echo $save_hour_arr[$d][$i] . "</td>\n";
-        } else {
-		echo "<td";
-		if ( $class != '' ) {
-			echo " class=\"$class\"";
-		}
-		echo ">";
-		if ( $can_add ) {
-			echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, $user );
-		}
-          echo $save_hour_arr[$d][$i] . "</td>\n";
-        }
+    $class = ( $is_weekend ? "weekend" : "" );
+    if ( date ( 'Ymd', $days[$d] ) == date ( 'Ymd', $today ) ) {
+      if ( $class != "" ) {
+        $class .= " ";
       }
-    }
+        $class .= "today";
+      }
+
+   // Use the class 'hasevents' for any hour block that has events
+   // in it.
+   if ( ! empty ( $save_hour_arr[$d][$i] ) &&
+     strlen ( $save_hour_arr[$d][$i] ) ) {
+     $class = 'hasevents';
+   }
+
+   if ( $rowspan_day[$d] > 1 ) {
+     // this might mean there's an overlap, or it could mean one event
+     // ends at 11:15 and another starts at 11:30.
+     if ( ! empty ( $save_hour_arr[$d][$i] ) ) {
+       echo "<td";
+       if ( $class != '' ) {
+         echo " class=\"$class\"";
+       }
+       echo ">" . $save_hour_arr[$d][$i] . "</td>\n";
+     }
+     $rowspan_day[$d]--;
+   } else if ( empty ( $save_hour_arr[$d][$i] ) ) {
+     echo "<td";
+     if ( $class != "" ) {
+       echo " class=\"$class\"";
+     }
+     echo ">";
+     if ( $can_add ) { //if user can add events...
+       echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, 
+         $user ); //..then echo the add event icon
+     }
+     echo "&nbsp;</td>\n";
+   } else {
+     $rowspan_day[$d] = $save_rowspan_arr[$d][$i];
+     if ( $rowspan_day[$d] > 1 ) {
+       echo "<td";
+       if ( $class != '' ) {
+         echo " class=\"$class\"";
+       }
+       echo " rowspan=\"$rowspan_day[$d]\">";
+       if ( $can_add ) {
+         echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, $user );
+       }
+       echo $save_hour_arr[$d][$i] . "</td>\n";
+     } else {
+       echo "<td";
+       if ( $class != '' ) {
+         echo " class=\"$class\"";
+       }
+       echo ">";
+       if ( $can_add ) {
+         echo html_for_add_icon (  date ( "Ymd", $days[$d] ), $time_h, $time_m, $user );
+       }
+       echo $save_hour_arr[$d][$i] . "</td>\n";
+     }
+   }
   }
   echo "</tr>\n";
 }
-
 ?>
 </table>
 <br />
 
 <?php
-if ( ! empty ( $eventinfo ) ) echo $eventinfo;
-display_unapproved_events ( ( $is_assistant || $is_nonuser_admin ? $user : $login ) );
+if ( ! empty ( $eventinfo ) ) {
+  echo $eventinfo;
+}
+display_unapproved_events ( ( $is_assistant || 
+  $is_nonuser_admin ? $user : $login ) );
 ?>
 
 <br />
-<a title="<?php etranslate("Generate printer-friendly version")?>" class="printer" href="week.php?<?php
+<a title="<?php etranslate("Generate printer-friendly version")?>" 
+class="printer" href="week.php?<?php
   echo $u_url;
   if ( $thisyear ) {
     echo "year=$thisyear&amp;month=$thismonth&amp;day=$thisday";
