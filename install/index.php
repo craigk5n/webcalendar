7a04efc0282667c1357835359c83d25df68e263f
cknudsen <cknudsen>
2005-03-27 02:19:01 +0000

    Fixed syntax error if ASP-style '<?' is allowed.

--- install/index.php
+++ install/index.php
@@ -329,8 +329,8 @@ if ( ! empty ( $fd ) ) {
   fclose ( $fd );
 }
 
+echo '<?xml version="1.0" encoding="UTF-8"?>' . "\n";
 ?>
-<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head><title>WebCalendar Database Setup</title>
