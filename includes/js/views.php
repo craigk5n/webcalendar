02dd7f40fe806ced1c0a9039af2a804b45cea129
musus <musus>
2005-07-25 08:59:17 +0000

    adding new views.php js

---
+++ includes/js/views.php
@@ -0,0 +1,15 @@
+<script type="text/javascript">
+<!-- <![CDATA[
+function show(foo,f,section) {
+	document.getElementById(foo).style.display = "block";
+	if (f) { setCookie(foo, "o", section); }
+}
+
+function hide(foo,f,section) {
+	if (document.getElementById(foo)) {
+		document.getElementById(foo).style.display = "none";
+		if (f) { deleteCookie(foo, section); }
+	}
+}
+//]]> -->
+</script>
