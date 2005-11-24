8f38ca6c5d3e903ace06357901650087c992dd0c
umcesrjones <umcesrjones>
2005-11-24 13:23:56 +0000

    Fixed bug 1365452: 'Category' - pop-up cannot be translated completely

--- catsel.php
+++ catsel.php
@@ -25,7 +25,7 @@ print_header($INC,'','',true, false, true);
   echo "<tr><td valign=\"top\">\n";
   if ( ! empty ( $categories ) ) {
    echo "<select name=\"cats[]\" size=\"10\">\n" . 
-    "<option disabled>AVAILABLE CATEGORIES</option>\n";
+    "<option disabled>" . translate("AVAILABLE CATEGORIES") . "</option>\n";
     foreach ( $categories as $K => $V ) {
  //     if ( $category_owners[$K] == $login || $is_admin ) {
         if ( empty ( $category_owners[$K] ) ) {
@@ -40,7 +40,7 @@ print_header($INC,'','',true, false, true);
   }
  echo "<td valign=\"center\"><input type=\"button\" value=\"  >  \" onclick=\"selAdd()\" /></td>";
   echo "<td align=\"center\" valign=\"top\">\n<select name=\"eventcats[]\" size=\"9\" multiple>\n" . 
-    "<option disabled>EVENT CATEGORIES</option>\n";
+    "<option disabled>" . translate("EVENT CATEGORIES") . "</option>\n";
   if ( ! empty ( $cats ) ) {
   foreach ( $eventcats as $K) {  
    //disable if not creator and category is Global
@@ -55,14 +55,15 @@ print_header($INC,'','',true, false, true);
   }
  }
  echo "</select>\n"; 
- echo "<input type=\"button\" value=\"Remove\" onclick=\"selRemove()\" />";
+ echo "<input type=\"button\" value=\"" . translate("Remove") . 
+   "\" onclick=\"selRemove()\" />";
 
   echo "</td></tr>\n"; 
 ?>
-  <tr><td valign="top" align="right">*Global Category
- &nbsp;&nbsp;&nbsp;<input type="button" value="Ok" onclick="sendCats()" /></td>
+  <tr><td valign="top" align="right">*<?php etranslate("Global Category") ?>
+ &nbsp;&nbsp;&nbsp;<input type="button" value="<?php etranslate("Ok") ?>" onclick="sendCats()" /></td>
  <td colspan="2" align="left">&nbsp;&nbsp;  
-   <input type="button" value="Cancel" onclick="window.close()" />
+   <input type="button" value="<?php etranslate("Cancel") ?>" onclick="window.close()" />
  </td></tr>
  </form>
 </table>
