88bf58a1c5d179d8cf456b510c11c9d14b198359
umcesrjones <umcesrjones>
2005-12-19 13:12:50 +0000

    New theme definitions

---
+++ themes/autumn_pref.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Custom theme for use with WebCalendar
+ *
+ * Autumn - modify colors for autumn
+ *
+ * @author Ray Jones <rjones@umces.edu>
+ * @copyright Craig Knudsen, <cknudsen@cknudsen.com>, http://www.k5n.us/cknudsen
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPL
+ * @version $Id: 
+ * @package WebCalendar
+ */
+
+// Define your stuff here...
+// Any option in webcal_user_pref can be configured here
+//
+
+//This theme changes the color scheme only to the hues of autumn.
+//This theme will be available to both normal users and System Settings.
+$webcal_theme = array (
+"BGCOLOR"=>"#F4DD65",
+"CELLBG"=>"#F4DD65",
+"HASEVENTSBG"=>"#CC9900",
+"H2COLOR"=>"#000000",
+"OTHERMONTHBG"=>"#CC9900",
+"POPUP_BG"=>"#F4DD65",
+"POPUP_FG"=>"#000000",
+"TABLEBG"=>"#000000",
+"TEXTCOLOR"=>"#000000",
+"THBG"=>"#F4DD65",
+"THFG"=>"#000000",
+"TODAYCELLBG"=>"#E7D03A",
+"WEEKENDBG"=>"#FF6633"
+ );
+?>
---
+++ themes/spring_pref.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Custom theme for use with WebCalendar
+ *
+ * Spring - modify colors for spring
+ *
+ * @author Ray Jones <rjones@umces.edu>
+ * @copyright Craig Knudsen, <cknudsen@cknudsen.com>, http://www.k5n.us/cknudsen
+ * @license http://www.gnu.org/licenses/gpl.html GNU GPL
+ * @version $Id: 
+ * @package WebCalendar
+ */
+
+// Define your stuff here...
+// Any option in webcal_user_pref can be configured here
+//
+
+//This theme changes the color scheme only to the hues of spring.
+//This theme will be available to both normal users and System Settings.
+$webcal_theme = array (
+"BGCOLOR"=>"#CCFFCC",
+"CELLBG"=>"#99FF99",
+"HASEVENTSBG"=>"#66FF66",
+"H2COLOR"=>"#006600",
+"OTHERMONTHBG"=>"#999933",
+"POPUP_BG"=>"#6699CC",
+"POPUP_FG"=>"#000000",
+"TABLEBG"=>"#000000",
+"TEXTCOLOR"=>"#000000",
+"THBG"=>"#669900",
+"THFG"=>"#000000",
+"TODAYCELLBG"=>"#FFFF66",
+"WEEKENDBG"=>"#00CC99"
+ );
+?>
