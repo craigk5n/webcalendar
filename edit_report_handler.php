5bf6f15cdf3e34c9de3c7592a0f93ec6135e55b0
umcesrjones <umcesrjones>
2006-01-05 14:15:30 +0000

    Fixed bug 1397766: Tomorrow Report fails to This Week

--- edit_report_handler.php
+++ edit_report_handler.php
@@ -156,7 +156,7 @@ if ( empty ( $error ) ) {
   }
 
   $names[] = "cal_time_range";
-  $values[] = ( empty ( $time_range ) ? 11 : $time_range );
+  $values[] = ( ! isset ( $time_range ) ? 11 : $time_range );
 
   $names[] = "cal_cat_id";
   $values[] = ( empty ( $cat_id ) ? "NULL" : $cat_id );
