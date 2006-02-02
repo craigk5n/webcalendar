a65bb8ba93658c762ac352d3eee657ef8c65c7c7
jhoov <jhoov>
2006-02-02 22:24:05 +0000

    Fixed bug 1413327: repeated events appear out of order in various pages

--- includes/functions.php
+++ includes/functions.php
@@ -5963,7 +5963,7 @@ function error_check ( $nextURL ) {
  * @params passed automatically by usort, don't pass them in your call
  */
 function sort_events ( $a, $b ) { 
-  $retval = strnatcmp( $a->getDateTimeTS(), $b->getDateTimeTS() ); 
+  $retval = strnatcmp( $a->getTime(), $b->getTime() ); 
   if( ! $retval ) return strnatcmp( $a->getName(), $b->getName() );
   return $retval; 
 } 
@@ -5976,7 +5976,7 @@ function sort_events ( $a, $b ) {
  * @params passed automatically by usort, don't pass them in your call
  */
 function sort_events_insensitive ( $a, $b ) { 
-  $retval = strnatcmp( $a->getDateTimeTS(), $b->getDateTimeTS() ); 
+  $retval = strnatcmp( $a->getTime(), $b->getTime() ); 
   if( ! $retval ) return strnatcmp( strtolower($a->getName()), strtolower($b->getName()) ); 
   return $retval; 
 } 
