3a18e37a46bbe098583fccc84891e2a157db8635
umcesrjones <umcesrjones>
2006-05-26 17:23:25 +0000

    Fixed bug 1493472: entered 'task' will not be displayed on day.php

--- day.php
+++ day.php
@@ -26,6 +26,9 @@ $prevyear = date ( 'Y', $prev );
 $prevmonth = date ( 'm', $prev );
 $prevday = date ( 'd', $prev );
 
+if ( empty ( $TIME_SLOTS ) )
+  $TIME_SLOTS = 24;
+
 if ( ! empty ( $BOLD_DAYS_IN_YEAR ) && $BOLD_DAYS_IN_YEAR == 'Y' ) {
  $boldDays = true;
 } else {
@@ -35,14 +38,7 @@ if ( ! empty ( $BOLD_DAYS_IN_YEAR ) && $BOLD_DAYS_IN_YEAR == 'Y' ) {
 $startdate = mktime ( 0, 0, 0, $thismonth, 1, $thisyear );
 $enddate = mktime ( 0, 0, 0, $thismonth +1 , 0, $thisyear );
 
-$HeadX = '';
-if ( $AUTO_REFRESH == 'Y' && ! empty ( $AUTO_REFRESH_TIME ) ) {
-  $refresh = $AUTO_REFRESH_TIME * 60; // convert to seconds
-  $HeadX = "<meta http-equiv=\"refresh\" content=\"$refresh; url=day.php?$u_url" .
-    "date=$nowYmd$caturl" . ( ! empty ( $friendly ) ? '&amp;friendly=1' : '') . "\" />\n";
-}
-$INC = array('js/popups.php');
-print_header($INC,$HeadX);
+$smallTasks = $unapprovedStr = $printFriendlyStr = '';
 
 /* Pre-Load the repeated events for quckier access */
 $repeated_events = read_repeated_events ( empty ( $user ) ? $login : $user,
@@ -55,46 +51,59 @@ $events = read_events ( empty ( $user ) ? $login : $user, $startdate, $enddate,
 if ( empty ( $DISPLAY_TASKS_IN_GRID ) ||  $DISPLAY_TASKS_IN_GRID == 'Y' ) {
   /* Pre-load tasks for quicker access */
   $tasks = read_tasks ( ( ! empty ( $user ) && strlen ( $user ) )
-    ? $user : $login, $startdate, $enddate, $cat_id );
+    ? $user : $login, $now, $cat_id );
 }
-?>
-
-<table  width="100%" cellpadding="1">
-<tr><td style="vertical-align:top; width:80%;" >
-<?php
-  echo display_navigation( 'day' );
-?>
-</td><td>&nbsp;</td></tr><tr><td>
-<table class="glance" cellspacing="0" cellpadding="0">
-<?php
-if ( empty ( $TIME_SLOTS ) )
-  $TIME_SLOTS = 24;
-
-echo print_day_at_a_glance ( date ( 'Ymd', $now ),
-  empty ( $user ) ? $login : $user, $can_add );
-?>
-</table>
-</td>
-<td style="vertical-align:top;" rowspan="2">
-<!-- START MINICAL -->
-<div class="minicalcontainer" style="text-align:center">
-<?php echo display_small_month ( $thismonth, $thisyear, true ); ?>
-</div>
-<br />
-<?php 
 if ( ! empty ( $DISPLAY_TASKS ) && $DISPLAY_TASKS == 'Y' ) {
-  echo display_small_tasks ( $cat_id );
+  $smallTasks = display_small_tasks ( $cat_id );
 }
-?> 
-</td></tr></table>
-<br />
-<?php
- if ( ! empty ( $eventinfo ) ) echo $eventinfo;
 
-echo display_unapproved_events ( ( $is_assistant || $is_nonuser_admin ? $user : $login ) );
+$navStr = display_navigation( 'day' );
+$dayStr =  print_day_at_a_glance ( date ( 'Ymd', $now ),
+  empty ( $user ) ? $login : $user, $can_add );
+$smallMonthStr = display_small_month ( $thismonth, $thisyear, true );
+if ( empty ( $friendly ) ) {
+  $unapprovedStr = display_unapproved_events ( ( $is_assistant || 
+    $is_nonuser_admin ? $user : $login ) );
+  $printFriendlyStr = generate_printer_friendly ( 'day.php' );
+}
 
-echo  '<br />';
-echo generate_printer_friendly ( 'day.php' );
-echo print_trailer (); ?>
-</body>
-</html>
+$HeadX = '';
+if ( $AUTO_REFRESH == 'Y' && ! empty ( $AUTO_REFRESH_TIME ) ) {
+  $refresh = $AUTO_REFRESH_TIME * 60; // convert to seconds
+  $HeadX = "<meta http-equiv=\"refresh\" content=\"$refresh; url=day.php?$u_url" .
+    "date=$nowYmd$caturl" . ( ! empty ( $friendly ) ? '&amp;friendly=1' : '') . "\" />\n";
+}
+$INC = array('js/popups.php');
+print_header($INC,$HeadX);
+$trailerStr = print_trailer ();
+
+echo <<<EOT
+  <table width="100%" cellpadding="1">
+    <tr>
+      <td style="vertical-align:top; width:80%;" >
+      {$navStr}
+      </td>
+      <td>&nbsp;</td>
+   </tr>
+   <tr>
+     <td>
+      {$dayStr}
+     </td>
+     <td style="vertical-align:top;" rowspan="2">
+       <!-- START MINICAL -->
+       <div class="minicalcontainer" style="text-align:center">
+        {$smallMonthStr}
+       </div>
+       <br />
+        {$smallTasks}
+      </td>
+    </tr>
+  </table>
+  <br />
+  {$unapprovedStr}
+  <br />
+  {$printFriendlyStr}
+  {$trailerStr}
+  </body>
+  </html>
+EOT;
