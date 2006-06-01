81e1492711917bf415319606183629f575678540
umcesrjones <umcesrjones>
2006-06-01 19:37:58 +0000

    added tests for SimpleTest

--- tests/all_tests.php
+++ tests/all_tests.php
@@ -2,7 +2,7 @@
   if ( ! defined ( 'SIMPLETEST_ROOT' ) ) {
     define ( 'SIMPLETEST_ROOT', '../../simpletest/' );
   }
-
+  file_exists ( SIMPLETEST_ROOT . 'unit_tester.php' ) or die ( 'Simple Test not configured' );
   require_once(SIMPLETEST_ROOT . 'unit_tester.php');
   require_once(SIMPLETEST_ROOT . 'reporter.php');
 
