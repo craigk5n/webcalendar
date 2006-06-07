09e78dc78bdd06e3e5332a539900f1369e5827fc
Craig Knudsen <craig@k5n.us>
2006-06-07 18:23:02 +0000

    Added code documentation. Reformatting changes. Cleaned up imports (thanks eclipse)

--- java/us/k5n/webcalendar/ui/ControlPanel/UserDialog.java
+++ java/us/k5n/webcalendar/ui/ControlPanel/UserDialog.java
@@ -1,43 +1,29 @@
-/*
- * $Id$
- *
- * Description:
- *	WebCalendar Admin ControlPanel
- *	Dialog window to edit or add a user
- *
- * History:
- *	$Log$
- *	Revision 1.3  2005/09/20 02:31:41  cknudsen
- *	Added support for user update.
- *	
- *	Revision 1.2  2005/09/17 12:46:46  cknudsen
- *	Added support for deleting users.
- *	
- *	Revision 1.1  2005/09/16 13:27:03  cknudsen
- *	Moved ControlPanel to its own directory:
- *	  us/k5n/webcalendar/ui/ControlPanel
- *	Updated ControlPanel to support adding users.
- *	(Admin flag is not recognized yet though.)
- *	
- *
- * TODO:
- *	Replace the layout with GridBagLayout.
- */
-
 package us.k5n.webcalendar.ui.ControlPanel;
 
-import java.util.Calendar;
-import java.util.Vector;
-import java.net.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;        
-import us.k5n.webcalendar.*;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import us.k5n.webcalendar.User;
+import us.k5n.webcalendar.WebCalendarClient;
 
 /**
-  * User edit dialog window
-  */
+ * The UserDialog class provides a dialog window for editing or adding a user.
+ * 
+ * @author Craig Knudsen
+ * @version $Id$
+ */
 public class UserDialog extends JDialog {
   WebCalendarClient client = null;
   JFrame parent = null;
@@ -50,81 +36,74 @@ public class UserDialog extends JDialog {
   UserListener userListener = null;
 
   public UserDialog ( WebCalendarClient clientIn, JFrame parent, int mode,
-    UserListener userListenerIn )
-  {
+      UserListener userListenerIn ) {
     this ( clientIn, parent, mode, userListenerIn, null );
   }
 
   public UserDialog ( WebCalendarClient clientIn, JFrame parent, int modeIn,
-    UserListener userListenerIn, User user )
-  {
-    super ( parent, modeIn == EDIT_MODE ? "Edit User" : "Add User",
-      true );
+      UserListener userListenerIn, User user ) {
+    super ( parent, modeIn == EDIT_MODE ? "Edit User" : "Add User", true );
     this.client = clientIn;
     this.userListener = userListenerIn;
     mode = modeIn;
-    //setSize ( 400, 400 );
+    // setSize ( 400, 400 );
     JPanel buttonPanel, topPanel;
 
-    getContentPane().setLayout ( new BorderLayout () );
+    // TODO: Replace the layout with GridBagLayout.
+
+    getContentPane ().setLayout ( new BorderLayout () );
 
     buttonPanel = new JPanel ();
     JButton okButton = new JButton ( "Ok" );
     buttonPanel.add ( okButton );
-    getContentPane().add ( buttonPanel, BorderLayout.SOUTH );
+    getContentPane ().add ( buttonPanel, BorderLayout.SOUTH );
     okButton.addActionListener ( // Anonymous class as a listener.
-      new ActionListener () {
-        public void actionPerformed ( ActionEvent e ) {
-          if ( mode == ADD_MODE &&
-            ! password1.getText().equals ( password2.getText() ) ) {
-            client.showError ( "Passwords do not match" );
-          }
-          else if ( username.getText().length() == 0 ) {
-            client.showError ( "You must enter a username" );
-          }
-          else if ( mode == ADD_MODE && password1.getText().length() == 0 ) {
-            client.showError ( "You must enter a password" );
-          }
-          else {
-            if ( mode == ADD_MODE && client.addUser ( getUser() ) ) {
-              // success...
-              userListener.updateUserList ();
-              hide ();
-              dispose ();
-            } else if ( mode == EDIT_MODE && client.updateUser ( getUser() ) ) {
-              // success...
-              userListener.updateUserList ();
-              hide ();
-              dispose ();
+        new ActionListener () {
+          public void actionPerformed ( ActionEvent e ) {
+            if (mode == ADD_MODE
+                && !password1.getText ().equals ( password2.getText () )) {
+              client.showError ( "Passwords do not match" );
+            } else if (username.getText ().length () == 0) {
+              client.showError ( "You must enter a username" );
+            } else if (mode == ADD_MODE && password1.getText ().length () == 0) {
+              client.showError ( "You must enter a password" );
+            } else {
+              if (mode == ADD_MODE && client.addUser ( getUser () )) {
+                // success...
+                userListener.updateUserList ();
+                hide ();
+                dispose ();
+              } else if (mode == EDIT_MODE && client.updateUser ( getUser () )) {
+                // success...
+                userListener.updateUserList ();
+                hide ();
+                dispose ();
+              }
             }
           }
-        }
-      }
-    );
+        } );
 
     JButton cancelButton = new JButton ( "Cancel" );
     buttonPanel.add ( cancelButton );
-    getContentPane().add ( buttonPanel, BorderLayout.SOUTH );
+    getContentPane ().add ( buttonPanel, BorderLayout.SOUTH );
     cancelButton.addActionListener ( // Anonymous class as a listener.
-      new ActionListener () {
-        public void actionPerformed ( ActionEvent e ) {
-          hide ();
-          dispose ();
-        }
-      }
-    );
-
+        new ActionListener () {
+          public void actionPerformed ( ActionEvent e ) {
+            hide ();
+            dispose ();
+          }
+        } );
 
     topPanel = new JPanel ();
     topPanel.setLayout ( new BoxLayout ( topPanel, BoxLayout.Y_AXIS ) );
 
     JPanel subP = new JPanel ();
     subP.setLayout ( new FlowLayout () );
-    subP.add ( new JLabel ( "User login: " )  );
+    subP.add ( new JLabel ( "User login: " ) );
     username = new JTextField ( 25 );
     username.setName ( "username" );
-    if ( mode == EDIT_MODE ) {
-      if ( user != null && user.login != null )
+    if (mode == EDIT_MODE) {
+      if (user != null && user.login != null)
         username.setText ( user.login );
       username.setEditable ( false );
     }
@@ -136,7 +115,7 @@ public class UserDialog extends JDialog {
     subP.add ( new JLabel ( "First name: " ) );
     firstname = new JTextField ( 15 );
     firstname.setName ( "firstname" );
-    if ( mode == EDIT_MODE && user.firstName != null )
+    if (mode == EDIT_MODE && user.firstName != null)
       firstname.setText ( user.firstName );
     subP.add ( firstname );
     topPanel.add ( subP );
@@ -146,7 +125,7 @@ public class UserDialog extends JDialog {
     subP.add ( new JLabel ( "Last name: " ) );
     lastname = new JTextField ( 15 );
     lastname.setName ( "lastname" );
-    if ( mode == EDIT_MODE && user.lastName != null )
+    if (mode == EDIT_MODE && user.lastName != null)
       lastname.setText ( user.lastName );
     subP.add ( lastname );
     topPanel.add ( subP );
@@ -154,25 +133,22 @@ public class UserDialog extends JDialog {
     // WebCalendar uses firstname & lastname to derive
     // the display/full name
     /*
-    subP = new JPanel ();
-    subP.setLayout ( new FlowLayout () );
-    subP.add ( new JLabel ( "Full name: " ) );
-    fullname = new JTextField ( 25 );
-    fullname.setName ( "fullname" );
-    subP.add ( fullname );
-    topPanel.add ( subP );
-    */
+     * subP = new JPanel (); subP.setLayout ( new FlowLayout () ); subP.add (
+     * new JLabel ( "Full name: " ) ); fullname = new JTextField ( 25 );
+     * fullname.setName ( "fullname" ); subP.add ( fullname ); topPanel.add (
+     * subP );
+     */
 
     subP = new JPanel ();
     subP.setLayout ( new FlowLayout () );
     subP.add ( new JLabel ( "Email: " ) );
     email = new JTextField ( 25 );
-    if ( mode == EDIT_MODE && user.email != null )
+    if (mode == EDIT_MODE && user.email != null)
       email.setText ( user.email );
     subP.add ( email );
     topPanel.add ( subP );
 
-    if ( mode == ADD_MODE ) {
+    if (mode == ADD_MODE) {
       subP = new JPanel ();
       subP.setLayout ( new FlowLayout () );
       subP.add ( new JLabel ( "Password: " ) );
@@ -201,28 +177,26 @@ public class UserDialog extends JDialog {
     subP.add ( userType );
     topPanel.add ( subP );
 
-    getContentPane().add ( topPanel, BorderLayout.CENTER );
+    getContentPane ().add ( topPanel, BorderLayout.CENTER );
 
     pack ();
     show ();
   }
 
-  public User getUser ()
-  {
+  public User getUser () {
     User u = new User ( username.getText () );
     u.firstName = firstname.getText ();
     u.lastName = lastname.getText ();
-    //u.fullName = fullname.getText ();
-    if ( password1 != null )
+    // u.fullName = fullname.getText ();
+    if (password1 != null)
       u.password = password1.getText ();
     else
       u.password = null;
     u.email = email.getText ();
     Object o = userType.getSelectedItem ();
-    u.isAdmin = ( o != null &&
-      o.toString().toLowerCase().startsWith ( "admin" ) );
+    u.isAdmin = ( o != null && o.toString ().toLowerCase ().startsWith (
+        "admin" ) );
 
     return u;
   }
 }
-
--- java/us/k5n/webcalendar/ui/ControlPanel/UserListCellRenderer.java
+++ java/us/k5n/webcalendar/ui/ControlPanel/UserListCellRenderer.java
@@ -1,62 +1,44 @@
-/*
- * $Id$
- *
- * Description:
- *	Override the default JList cell renderer so we can use different
- *	colors in the JList to identify admin users.
- *	We could also add icons here if we wanted to...
- *
- * To-Do:
- *	Instead of using different colors, use different icons for admins
- *	and normal users.
- * History:
- *	$Log$
- *	Revision 1.1  2005/09/17 12:46:46  cknudsen
- *	Added support for deleting users.
- *	
- *
- */
-
 package us.k5n.webcalendar.ui.ControlPanel;
 
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;        
-import us.k5n.webcalendar.*;
+import java.awt.Color;
+import java.awt.Component;
 
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import us.k5n.webcalendar.User;
 
 /**
-  * User edit dialog window
-  */
-public class UserListCellRenderer extends JLabel
-  implements ListCellRenderer {
+ * This ListCellRenderer colors admin users as red and all other users as white
+ * in the JList. We could also add icons here if we wanted to...
+ * 
+ * @author Craig Knudsen
+ * @version $Id$
+ */
+public class UserListCellRenderer extends JLabel implements ListCellRenderer {
 
-  // This is the only method defined by ListCellRenderer.
-  // We just reconfigure the JLabel each time we're called.
-
-  public Component getListCellRendererComponent (
-    JList list,
-    Object value,            // value to display
-    int index,               // cell index
-    boolean isSelected,      // is the cell selected
-    boolean cellHasFocus )   // the list and the cell have the focus
-  {
+  /**
+   * This is the only method defined by ListCellRenderer. We just reconfigure
+   * the JLabel each time we're called.
+   */
+  public Component getListCellRendererComponent ( JList list, Object value,
+      int index, boolean isSelected, boolean cellHasFocus ) {
     Color bg, fg;
-    User user = (User) value;
-    String s = value.toString();
+    User user = (User)value;
+    String s = value.toString ();
     setText ( s );
-    if ( isSelected ) {
+    if (isSelected) {
       bg = list.getSelectionBackground ();
       fg = list.getSelectionForeground ();
-      if ( user.isAdmin ) {
+      if (user.isAdmin) {
         fg = Color.WHITE;
         bg = Color.RED;
       }
-    }
-    else {
-      bg = list.getBackground();
-      fg = list.getForeground();
-      if ( user.isAdmin ) {
+    } else {
+      bg = list.getBackground ();
+      fg = list.getForeground ();
+      if (user.isAdmin) {
         fg = Color.RED;
         bg = Color.WHITE;
       }
@@ -65,12 +47,10 @@ public class UserListCellRenderer extends JLabel
     setBackground ( bg );
     setForeground ( fg );
 
-    setEnabled ( list.isEnabled() );
-    setFont ( list.getFont() );
+    setEnabled ( list.isEnabled () );
+    setFont ( list.getFont () );
     setOpaque ( true );
     return this;
   }
 
 }
-
-
