579dde38489059f443df586e9717e43af6d68b49
Craig Knudsen <craig@k5n.us>
2006-06-07 18:17:22 +0000

    Added comments. Cleaned up imports.

--- java/us/k5n/webcalendar/ui/ControlPanel/ReadOnlyTable.java
+++ java/us/k5n/webcalendar/ui/ControlPanel/ReadOnlyTable.java
@@ -1,43 +1,38 @@
-/*
- * $Id$
- */
-
 package us.k5n.webcalendar.ui.ControlPanel;
 
-import java.awt.*;
-import javax.swing.*;
-import javax.swing.table.*;
+import java.awt.Color;
+import java.awt.Component;
 import java.util.Vector;
 
-
-
+import javax.swing.JTable;
+import javax.swing.table.TableCellRenderer;
 
 /**
-  * Overide methods of JTable to prevent cell editing.
-  */
-public class ReadOnlyTable extends JTable
-{
+ * Overide methods of JTable to prevent cell editing.
+ * 
+ * @author Craig Knudsen
+ * @version $Id$
+ */
+public class ReadOnlyTable extends JTable {
   int highlightedRow = -1;
   Color lightGray;
 
-  public Component prepareRenderer ( TableCellRenderer renderer,
-    int rowIndex, int vColIndex ) {
+  public Component prepareRenderer ( TableCellRenderer renderer, int rowIndex,
+      int vColIndex ) {
     Component c = super.prepareRenderer ( renderer, rowIndex, vColIndex );
-    if ( rowIndex == highlightedRow ) {
+    if (rowIndex == highlightedRow) {
       c.setBackground ( Color.blue );
       c.setForeground ( Color.white );
-    } else if ( rowIndex % 2 == 0 ) {
+    } else if (rowIndex % 2 == 0) {
       c.setBackground ( lightGray );
       c.setForeground ( Color.black );
     } else {
       // If not shaded, match the table's background
-      c.setBackground ( getBackground() );
+      c.setBackground ( getBackground () );
       c.setForeground ( Color.black );
     }
     return c;
   }
-
-
 
   public ReadOnlyTable ( int rows, int cols ) {
     super ( rows, cols );
