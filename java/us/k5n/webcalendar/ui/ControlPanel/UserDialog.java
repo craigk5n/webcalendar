9b77189494f35a8c950a8395307b30f1c5ae93ba
Craig Knudsen <craig@k5n.us>
2006-06-07 18:46:46 +0000

    Cleaned up imports.

--- java/us/k5n/webcalendar/ui/ControlPanel/UserDialog.java
+++ java/us/k5n/webcalendar/ui/ControlPanel/UserDialog.java
@@ -1,13 +1,11 @@
 package us.k5n.webcalendar.ui.ControlPanel;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
-import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JDialog;
