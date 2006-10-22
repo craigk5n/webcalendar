1d768fe7748b3dba5eff69481d4bb69c21026c95
bbannon <bbannon>
2006-10-22 15:38:38 +0000

    Update some tranlate phrases. The translations/*.txt files may not be completely accuarate. But, they are still close enough till the translators get around to updating them.

--- help_uac.php
+++ help_uac.php
@@ -1,34 +1,30 @@
 <?php
 /* $Id$ */
 include_once 'includes/init.php';
-include_once 'includes/help_list.php'; 
+include_once 'includes/help_list.php';
 
-$helpStr = translate('Help');
+$helpStr = translate( 'Help' );
 $titleStr = translate ( 'User Access Control' );
-$descStr = translate('User Access Control allows for finer control of user access and permissions than possible before. Users can also grant default and per individual permission if authorized by the administrator.');
+$descStr = translate( 'Allows for fine control of user access and permissions. Users can also grant default and per individual permission if authorized by the administrator.' );
 $inviteStr = translate ( 'Can Invite' );
-$inviteTStr = translate('If disabled, this user will not see you in the participants list.');
-$emailStr = translate('Can Email');
-$emailTStr = translate('If disabled, this user will not be able to send you emails.');
+$inviteTStr = translate( 'If disabled, this user cannot see you in the participants list.' );
+$emailStr = translate( 'Can Email' );
+$emailTStr = translate( 'If disabled, this user cannot send you emails.' );
 $timeStr = translate ( 'Can See Time Only' );
-$timeTStr = translate('If enabled, this user will not be able to view the details of any of your entries.');
- 
-print_header('', '', '', true, false, true);
-echo $helpListStr;
+$timeTStr = translate( 'If enabled, this user cannot view the details of any of your entries.' );
+
+print_header( '', '', '', true, false, true );
 echo <<<EOT
+    {$helpListStr}
 
-<div class="helpbody">
-  <h2>{$helpStr}:{$titleStr}</h2>
-  <p>{$descStr}</p>
-  <p><label>{$inviteStr}:</label>
-   {$inviteTStr}</p>
-  <p><label>{$emailStr}:</label>
-   {$emailTStr}</p>
-  <p><label>{$timeStr}:</label>
-   {$timeTStr}</p>
-</div>
-
+    <div class="helpbody">
+      <h2>{$helpStr}:{$titleStr}</h2>
+      <p>{$descStr}</p>
+      <p><label>{$inviteStr}:</label>{$inviteTStr}</p>
+      <p><label>{$emailStr}:</label>{$emailTStr}</p>
+      <p><label>{$timeStr}:</label>{$timeTStr}</p>
+    </div>
 EOT;
+echo print_trailer( false, true, true );
 
-echo print_trailer( false, true, true ); ?>
-
+?>
