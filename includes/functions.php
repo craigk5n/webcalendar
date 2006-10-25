bd3ec7ca31c1bcf8726bd8cc44b0e1209d56ce3d
bbannon <bbannon>
2006-10-25 12:55:23 +0000

    Cut some dupe code.

--- includes/functions.php
+++ includes/functions.php
@@ -51,8 +51,15 @@ function getPostValue ( $name ) {
   return ( $HTTP_POST_VARS[$name] );
 }
 
-/**
- * Gets the value resulting from an HTTP GET method.
+include_once 'includes/getGetValue.php';
+
+/* The 'include_once' line above should work. Both here and includes/gradient.php.
+But, just in case, I'm only commenting out this function instead of removing it.
+
+/* Gets the value resulting from an HTTP GET method.
+ *
+ * Since this function is used in more than one place, with different names,
+ *  let's make it a seperate 'include' file on it's own.
  *
  * <b>Note:</b> The return value will be affected by the value of
  * <var>magic_quotes_gpc</var> in the php.ini file.
@@ -60,26 +67,28 @@ function getPostValue ( $name ) {
  * If you need to enforce a specific input format (such as numeric input), then
  * use the {@link getValue()} function.
  *
- * @param string $name Name used in the HTML form or found in the URL
+ * @param string  $name  Name used in the HTML form or found in the URL
  *
- * @return string The value used in the HTML form (or URL)
+ * @return string        The value used in the HTML form (or URL)
  *
  * @see getPostValue
- */
+*/
+/*
 function getGetValue ( $name ) {
   global $HTTP_GET_VARS;
 
   if ( isset ( $_GET ) && is_array ( $_GET ) && ! empty ( $_GET[$name] ) ) {
-  $_GET[$name] = ( get_magic_quotes_gpc () != 0? $_GET[$name]: addslashes ( $_GET[$name]) );
+    $_GET[$name] = ( get_magic_quotes_gpc () != 0
+      ? $_GET[$name] : addslashes ( $_GET[$name] ) );
     $HTTP_GET_VARS[$name] = $_GET[$name];
-  return $_GET[$name];
-  } else if ( ! isset ( $HTTP_GET_VARS ) ) {
+    return $_GET[$name];
+  } else
+  if ( ! isset ( $HTTP_GET_VARS ) || ! isset ( $HTTP_GET_VARS[$name] ) )
     return null;
-  } else if ( ! isset ( $HTTP_GET_VARS[$name] ) ){
-    return null;
- }
+
   return ( $HTTP_GET_VARS[$name] );
 }
+*/
 
 /**
  * Gets the value resulting from either HTTP GET method or HTTP POST method.
