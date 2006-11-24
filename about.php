6a4b88e21dacd38f16c000566b3331cd9660422d
bbannon <bbannon>
2006-11-24 21:35:34 +0000

    Add a translate phrase. And formatting for more readable.

--- about.php
+++ about.php
@@ -8,80 +8,91 @@ $credits = getPostValue ( 'Credits' );
 $data = '';
 
 if ( ! empty ( $credits ) ) {
-  //Get Names from AUTHORS file
+  // Get Names from AUTHORS file
   if ( $fd = @fopen( 'AUTHORS', 'r' ) ) {
-		// Read in contents of entire file first
-		while ( !feof( $fd ) && empty( $error ) ) {
-			$data .= fgets( $fd, 4096 );
-		}
-		fclose( $fd );
+    // Read in contents of entire file first
+    while ( !feof( $fd ) && empty( $error ) ) {
+      $data .= fgets( $fd, 4096 );
+    }
+    fclose( $fd );
   }
-  //$data = unhtmlentities ( $data );
+  // $data = unhtmlentities ( $data );
   $data = preg_replace ( "/<.+>+/", '', $data );
   $data = preg_replace ( "/\n\s/", '<br />&nbsp;', $data );
   $data = preg_replace ( '/\s\s+/', '&nbsp;&nbsp;', $data );
   $data = preg_replace ( '/\n/', '<br />', $data );
 }
-$version = '<a title="' . $GLOBALS['PROGRAM_NAME'] . '" ' .
-  'id="programname" href="' . $GLOBALS['PROGRAM_URL'] . '" target="_blank">' .
-  $GLOBALS['PROGRAM_NAME'] . "</a>\n";
+$version = '<a title="' . $GLOBALS['PROGRAM_NAME'] . '" '
+ . 'id="programname" href="' . $GLOBALS['PROGRAM_URL'] . '" target="_blank">'
+ . $GLOBALS['PROGRAM_NAME'] . "</a>\n";
 
 print_header( '', '', '', true, false, true );
-?>
-<div align="left" style="margin-left:4px;position:absolute; bottom:0px" >
-<?php if ( empty ( $credits ) ) { ?>
-<h2 style="margin:0px"><?php etranslate ( 'Title' ); ?></h2>
-<p><?php echo translate ( 'version' ) 
-  . ' ' . $PROGRAM_VERSION ?></p>
-<p><?php echo $PROGRAM_DATE ?></p>
-<p>&nbsp;</p>
-<p>WebCalendar is a PHP application 
-used to maintain a calendar for a single user or an intranet group 
-of users. It can also be configured as an event calendar.</p>
-<?php } else {?>
-<script language="javascript1.2" type="text/javascript"> 
-var scrollW="235px", scrollH="250px" ;
-var copyS=scrollS=1, pauseS=0;
-var scrollcontent='<?php echo $data ?>'; 
-var actualH='', cross_scroll;
+echo '
+    <div align="left" style="margin-left:4px; position:absolute; bottom:0" >';
+if ( empty ( $credits ) )
+  echo '
+      <h2 style="margin:0">' . translate ( 'Title' ) . '</h2>
+      <p>' . translate ( 'version' ) . ' ' . $PROGRAM_VERSION . '</p>
+      <p>' . $PROGRAM_DATE . '</p>
+      <p>&nbsp;</p>
+      <p>'
+   . translate ( 'Webcalendar is a PHP application used to maintain a calendar for a single user or an internet / intranet group of users. It can also be configured as an event calendar.' )
+   . '</p>';
+else {
 
-function populate(){ 
-  cross_scroll=document.getElementById("scroller"); 
-  cross_scroll.innerHTML=scrollcontent;
-  actualH=cross_scroll.offsetHeight; 
-  lefttime=setInterval("scrollMe()",30);
+  ?>
+      <script language="javascript1.2" type="text/javascript">
+        var
+          scrollW="235px",
+          scrollH="250px",
+          copyS=scrollS=1,
+          pauseS=0,
+          scrollcontent='<?php echo $data ?>',
+          actualH='',
+          cross_scroll;
+
+        function populate(){
+          cross_scroll=document.getElementById("scroller");
+          cross_scroll.innerHTML=scrollcontent;
+          actualH=cross_scroll.offsetHeight;
+          lefttime=setInterval("scrollMe()",30);
+        }
+
+        window.onload=populate;
+
+        function scrollMe(){
+          if (parseInt(cross_scroll.style.top)>(actualH*(-1)+8))
+            cross_scroll.style.top=parseInt(cross_scroll.style.top)-copyS+"px";
+          else
+            cross_scroll.style.top=parseInt(scrollH)+8+"px";
+        }
+
+        with (document){
+          write('
+    <div style="position: relative;
+        width: '+scrollW+';
+        height: '+scrollH+';
+        overflow: hidden;"
+      onMouseover="copyS=pauseS" onMouseout="copyS=scrollS">
+      <div id="scroller"></div>
+    </div>');
+        }
+      </script>
+<?php
 }
 
-window.onload=populate; 
+echo '
+      <hr />
+      <div align="center" style="margin: 10px; ">
+        <form action="about.php" name="aboutform" method="post">
+          <input type="submit" name=' . ( empty ( $credits )
+  ? '"Credits" value="' . $creditStr : '"About" value="<< ' . $fullname )
+ . '" />' . '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input type="button" name="ok" value="' . translate ( 'Ok' )
+ . '" onclick="window.close()" />
+        </form>
+      </div>
+    </div>
+    ' . print_trailer ( false, true, true );
 
-function scrollMe(){ 
-  if (parseInt(cross_scroll.style.top)>(actualH*(-1)+8)) 
-    cross_scroll.style.top=parseInt(cross_scroll.style.top)-copyS+"px";
-  else 
-    cross_scroll.style.top=parseInt(scrollH)+8+"px"; 
-} 
- 
-with (document){ 
- write('<div style="position:relative;width:'+scrollW+';height:'+scrollH+';overflow:hidden" onMouseover="copyS=pauseS" onMouseout="copyS=scrollS">');
- write('<div id="scroller"></div></div>');
-} 
-</script> 
-<?php } ?>
-<hr />
-<div align="center" style="margin:10px; ">
-<form action="about.php" name="aboutform"  method="post">
-<?php if ( empty ( $credits ) ) { ?>
- <input type="submit" name="Credits" value="<?php echo $creditStr ?>" />
-<?php } else {?>
- <input type="submit" name="About" value="<< <?php echo $fullname ?>" />
-<?php } ?>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
- <input type="button" name="ok" value="<?php 
-   etranslate ( 'Ok' ); ?>" onclick="window.close()" />
-</form>
-</div>
-</div>
-
-<?php echo print_trailer ( false, true, true );
 ?>
-
