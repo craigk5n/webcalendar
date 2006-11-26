e8d50baae1ce346a44ec1a85ab12e87e3eb6db7b
bbannon <bbannon>
2006-11-26 20:28:45 +0000

    Cut dupe code. Lines < 80.

--- del_layer.php
+++ del_layer.php
@@ -4,10 +4,9 @@ include_once 'includes/init.php';
 
 $id = getGetValue ( 'id' );
 
-if ( $ALLOW_VIEW_OTHER != 'Y' || empty ( $id )) {
+if ( $ALLOW_VIEW_OTHER != 'Y' || empty ( $id ) ) {
   print_header ();
-  echo print_not_auth ();
-  echo print_trailer ();
+  echo print_not_auth () . print_trailer ();
   exit;
 }
 $id = getGetValue ( 'id' );
@@ -15,21 +14,19 @@ $updating_public = false;
 if ( $is_admin && ! empty ( $public ) && $PUBLIC_ACCESS == 'Y' ) {
   $updating_public = true;
   $layer_user = '__public__';
-} else {
+} else
   $layer_user = $login;
-}
 
 load_user_layers ( $layer_user, 1 );
 
 if ( strlen ( $layers[$id]['cal_layeruser'] ) > 0 &&
-  ( $is_admin || $readonly == 'N' ) ) {
+    ( $is_admin || $readonly == 'N' ) ) {
   $layeruser = $layers[$id]['cal_layeruser'];
 
-  dbi_execute ( 'DELETE FROM webcal_user_layers WHERE cal_login = ? AND cal_layeruser = ?', array( $layer_user, $layeruser ) );
+  dbi_execute ( 'DELETE FROM webcal_user_layers WHERE cal_login = ?
+    AND cal_layeruser = ?', array ( $layer_user, $layeruser ) );
 }
 
-if ( $updating_public )
-  do_redirect ( 'layers.php?public=1' );
-else
-  do_redirect ( 'layers.php' );
+  do_redirect ( 'layers.php' . ( $updating_public ? '?public=1' : '' ) );
+
 ?>
