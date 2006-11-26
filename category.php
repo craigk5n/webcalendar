76649f43c8903315b1ca059302ebff4329ef2839
bbannon <bbannon>
2006-11-26 18:13:30 +0000

    Cut dupe code. Stay in PHP. Lines about 80 chars. Utlize $translations[] array. Modify a translate phrase.

--- category.php
+++ category.php
@@ -1,144 +1,154 @@
 <?php
 /* $Id$ */
-include_once 'includes/init.php';
 
+include_once 'includes/init.php';
 // load user and global cats
 load_user_categories ();
-
-$error = '';
-
-$icon_path = 'icons/';
 
 if ( $CATEGORIES_ENABLED == 'N' ) {
   send_to_preferred_view ();
   exit;
 }
 
+$catname = $error = $idStr = '';
 $catIconStr = translate ( 'Category Icon' );
 $globalStr = translate ( 'Global' );
-
-// If editing, make sure they are editing their own
-// (or they are an admin user).
+$icon_path = 'icons/';
+// If editing, make sure they are editing their own (or they are an admin user).
 if ( ! empty ( $id ) ) {
   if ( empty ( $categories[$id] ) )
-    $error = translate( 'Invalid entry id' ) . ': ' . $id;
+    $error = $translations['Invalid entry id'] . ': ' . $id;
 
-    $idStr = "<input name=\"id\" type=\"hidden\" value=\"$id\" />";
-    $button = translate( 'Save' );
-    $catname = $categories[$id]['cat_name'];
-    $catowner = $categories[$id]['cat_owner'];
-    $catcolor = $categories[$id]['cat_color'];
-    $catIcon = $icon_path . 'cat-' . $id . '.gif';
-} else {
-  $idStr = '';
-  $catname = '';
+  $button = $translations['Save'];
+  $catcolor = $categories[$id]['cat_color'];
+  $catname = $categories[$id]['cat_name'];
+  $catowner = $categories[$id]['cat_owner'];
+  $catIcon = $icon_path . 'cat-' . $id . '.gif';
+  $idStr = '<input name="id" type="hidden" value="' . $id . '" />';
+} else
   $catcolor = '#000000';
-}
 
-$showIcon = ( ! empty ( $catIcon ) && file_exists ( $catIcon ) ? 'visible' : 'hidden' );
+$showIcon = ( ! empty ( $catIcon ) && file_exists ( $catIcon )
+  ? 'visible' : 'hidden' );
 
-$INC = array('js/visible.php');
-print_header($INC);
-?>
-<h2><?php etranslate( 'Categories' )?></h2>
-<?php
-echo display_admin_link();
+print_header ( array ( 'js/visible.php' ) );
+ob_start ();
+echo '
+    <h2>' . $translations['Categories'] . '</h2>
+    ' . display_admin_link ();
 
 if ( empty ( $add ) )
   $add = 0;
-
 // Adding/Editing category
-$button = translate( $add == '1' ? 'Add' : 'Save' );
+$button = ( $add == '1' ? translate ( 'Add' ) : $translations['Save'] );
 
-if ( ( ( $add == '1' ) || ( ! empty ( $id ) ) ) && empty ( $error ) ) { ?>
-  <form action="category_handler.php" method="post" name="catform" enctype="multipart/form-data">
-  <?php echo $idStr ."\n" ?>
-  <table cellspacing="2" cellpadding="3">
-    <tr><td width="25%">
-      <label for="catname">
-  <?php
-  etranslate( 'Category Name' )?>:</label></td>
-  <td colspan="3"><input type="text" name="catname" size="20" value="<?php   echo htmlspecialchars ( $catname ); ?>" />
-  </td></tr>
-<?php  if ( $is_admin && empty ( $id ) ) { ?>
-   <tr><td><label for="isglobal"><?php echo $globalStr?>:</label></td>
-      <td colspan="3">
-      <label><input type="radio" name="isglobal" value="N" <?php if ( ! empty ( $catowner ) || empty ( $id ) ) echo ' checked="checked"';?> />&nbsp;<?php etranslate ( 'No')?></label>&nbsp;&nbsp;
-      <label><input type="radio" name="isglobal" value="Y" <?php if ( empty ( $catowner ) && ! empty ( $id ) ) echo ' checked="checked"';?> />&nbsp;<?php etranslate ( 'Yes' )?></label>
-  </td></tr>
-  <?php } ?>
-  <tr><td>
-<?php echo print_color_input_html ( 'catcolor', 
-   translate( 'Color' ), $catcolor ) ?>
-  </td></tr>
-  <tr id="cat_icon" style="visibility:<?php echo $showIcon ?>"><td>
-    <label><?php echo $catIconStr?>:</label></td>
-     <td colspan="3"><img src="<?php echo $catIcon?>" name="urlpic" 
-      id="urlpic" alt="<?php echo $catIconStr?>" />
-     </td></tr>
-     <tr id="remove_icon" style="visibility:<?php echo $showIcon ?>">
-     <td><label for="delIcon">
-      <?php echo translate( 'Remove Icon' )?>:</label></td>
-      <td colspan="3"><input type="checkbox" name="delIcon" value="Y" />
-      </td></tr>
-  <tr><td colspan="4">
-  <label for="FileName">
-<?php if ( is_dir($icon_path) && ( ! empty ( $ENABLE_ICON_UPLOADS ) &&
-  $ENABLE_ICON_UPLOADS == 'Y' || $is_admin )) { 
-  echo translate ( 'Add Icon to Category' ) . '</label><br />&nbsp;&nbsp;&nbsp;'.
-   translate ( 'Upload' ) . '&nbsp;<span style="font-size:small;">(' . 
-   translate ('gif 3kb max') . ')</span> :'; ?>
- <input type="file" name="FileName" id="fileupload" size="45" maxlength="50" /> 
-  </td></tr>
-  </tr><td colspan="4">
-  <input type="hidden" name="urlname" size="50"   />
-  &nbsp;&nbsp;&nbsp;<input type="button" value="<?php 
-    etranslate ( 'Search for existing icons', true);?>" onclick="window.open('icons.php', 'icons','dependent,menubar=no,scrollbars=n0,height=300,width=400, outerHeight=320,outerWidth=420');" />
-  </td></tr>
-  </tr><td colspan="4">
-<?php } //end test of ENABLE_ICON_UPLOADS ?>
-  <input type="submit" name="action" value="<?php echo $button;?>" />
-  <?php if ( ! empty ( $id ) ) {  ?>
- <input type="submit" name="delete" value="<?php etranslate( 'Delete' );?>" onclick="return confirm('<?php etranslate( 'Are you sure you want to delete this entry?', true); ?>')" />
-  <?php }  ?>
-  </td></tr>
-  </table>
-  </form>
-  <?php
-} else if ( empty ( $error ) ) {
+if ( ( ( $add == '1' ) || ( ! empty ( $id ) ) ) && empty ( $error ) ) {
+  echo '
+    <form action="category_handler.php" method="post" name="catform" '
+   . 'enctype="multipart/form-data">' . $idStr . '
+      <table cellspacing="2" cellpadding="3">
+        <tr>
+          <td width="25%"><label for="catname">' . translate ( 'Category Name' )
+   . ':</label></td>
+          <td colspan="3"><input type="text" name="catname" size="20" value="'
+   . htmlspecialchars ( $catname ) . '" /></td>
+        </tr>' . ( $is_admin && empty ( $id ) ? '
+        <tr>
+          <td><label for="isglobal">' . $globalStr . ':</label></td>
+          <td colspan="3">
+            <label><input type="radio" name="isglobal" value="N" '
+     . ( ! empty ( $catowner ) || empty ( $id ) ? ' checked = "checked"' : '' )
+     . ' />&nbsp;' . $translations['No'] . '</label>&nbsp;&nbsp;
+            <label><input type="radio" name="isglobal" value="Y" '
+     . ( empty ( $catowner ) && ! empty ( $id ) ? ' checked = "checked"' : '' )
+     . ' />&nbsp;' . $translations['Yes'] . '</label>
+          </td>
+        </tr>' : '' ) . '
+        <tr>
+          <td>'
+   . print_color_input_html ( 'catcolor', translate ( 'Color' ), $catcolor )
+   . '</td>
+        </tr>
+        <tr id="cat_icon" style="visibility: ' . $showIcon . '">
+          <td><label>' . $catIconStr . ':</label></td>
+          <td colspan="3"><img src="' . $catIcon
+   . '" name="urlpic" id="urlpic" alt="' . $catIconStr . '" /></td>
+        </tr>
+        <tr id="remove_icon" style="visibility: ' . $showIcon . '">
+          <td><label for="delIcon">' . translate ( 'Remove Icon' )
+   . ':</label></td>
+          <td colspan="3"><input type="checkbox" name="delIcon" value="Y" /></td>
+        </tr>
+        <tr>
+          <td colspan="4">
+            <label for="FileName">' . ( is_dir ( $icon_path ) &&
+    ( $ENABLE_ICON_UPLOADS == 'Y' || $is_admin )
+    ? translate ( 'Add Icon to Category' ) . '</label><br/>&nbsp;&nbsp;&nbsp;'
+     . translate ( 'Upload' ) . '&nbsp;<span style="font-size:small;">('
+     . translate ( 'gif 3kb max' ) . ')</span>:
+            <input type="file" name="FileName" id="fileupload" size="45" '
+     . 'maxlength="50" />
+          </td>
+        </tr>
+        </tr>
+          <td colspan="4">
+            <input type="hidden" name="urlname" size="50" />&nbsp;&nbsp;&nbsp;
+            <input type="button" value="'
+     . translate ( 'Search for existing icons' )
+     . '" onclick="window.open (\'icons.php\', \'icons\',\''
+     . 'dependent,menubar=no,scrollbars=n0,height=300,width=400,outerHeight=320'
+     . ',outerWidth=420\');" />
+          </td>
+        </tr>
+        </tr>
+          <td colspan="4">' : '' ) // end test of ENABLE_ICON_UPLOADS
+  . '
+            <input type="submit" name="action" value="' . $button . '" />'
+   . ( ! empty ( $id ) ? '
+            <input type="submit" name="delete" value="'
+     . translate ( 'Delete' ) . '" onclick="return confirm ('
+     . str_replace ( 'XXX', translate ( 'entry' ),
+      translate ( 'Are you sure you want to delete this XXX?' ) )
+     . '\')" />' : '' ) . '
+          </td>
+        </tr>
+      </table>
+    </form>';
+} else
+if ( empty ( $error ) ) {
   // Displaying Categories
   $global_found = false;
   if ( ! empty ( $categories ) ) {
-    echo '<ul>';
+    echo '
+    <ul>';
     foreach ( $categories as $K => $V ) {
-      if ( $K <1 ) continue;
-      $catIcon = $icon_path. 'cat-' . $K . '.gif';
-      $color = ( ! empty ( $V['cat_color'] ) ? $V['cat_color'] : '#000000' );
-      $catStr = "<span style=\"color:{$color};\">{$V['cat_name']}</span>"; 
-      echo '<li>';
-      if ( $V['cat_owner'] == $login || $is_admin )
-        echo "<a href=\"category.php?id=$K\">{$catStr}</a>";
-      else
-        echo $catStr;
+      if ( $K < 1 )
+        continue;
+      $catIcon = $icon_path . 'cat-' . $K . '.gif';
+      $catStr = '<span style="color: '
+       . ( ! empty ( $V['cat_color'] ) ? $V['cat_color'] : '#000000' )
+       . ';">' . $V['cat_name'] . '</span>';
+      echo '
+      <li>' . ( $V['cat_owner'] == $login || $is_admin
+        ? '<a href="category.php?id=' . $K . '">' . $catStr . '</a>' : $catStr );
+
       if ( empty ( $V['cat_owner'] ) ) {
         echo '<sup>*</sup>';
         $global_found = true;
       }
-      if ( file_exists ( $catIcon ) ){
-        echo "\n&nbsp;&nbsp;<img src=\"$catIcon\" alt=\"" 
-          . $catIconStr. '" title="' . $catIconStr . "\" />";
-      }
-      echo "</li>\n";
-    }
-    echo '</ul>';
-  }
-  if ( $global_found )
-    echo "<br /><br />\n<sup>*</sup> " . $globalStr;
-  echo '<p><a href="category.php?add=1">' . translate( 'Add New Category' ) 
-    . "</a></p><br />\n";
-}
 
-if ( ! empty ( $error ) ) {
-  echo print_error ( $error );
+      echo ( file_exists ( $catIcon ) ? '<img src="' . $catIcon . '" alt="'
+         . $catIconStr . '" title="' . $catIconStr . '" />' : '' ) . '</li>';
+    }
+    echo '
+    </ul>';
+  }
+  echo ( $global_found ? '<br /><br />
+    <sup>*</sup> ' . $globalStr : '' ) . '
+    <p><a href="category.php?add=1">' . translate ( 'Add New Category' )
+   . '</a></p><br />';
 }
-echo print_trailer(); ?>
+ob_end_flush ();
+echo ( ! empty ( $error ) ? print_error ( $error ) : '' ) . print_trailer ();
+
+?>
