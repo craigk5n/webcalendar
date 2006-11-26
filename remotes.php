02b2f423e1956e93013bd69e595baee205934b80
bbannon <bbannon>
2006-11-26 21:57:18 +0000

    Cut dupe code. Lines < 80. Stay in PHP.

--- remotes.php
+++ remotes.php
@@ -1,39 +1,44 @@
 <?php
 /* $Id$ */
-defined( '_ISVALID' ) or die( 'You cannot access this file directly!' );
+defined ( '_ISVALID' ) or die ( 'You cannot access this file directly!' );
 
-$newRemoteStr = translate( 'Add New Remote Calendar' );
-$targetStr = 'target="remotesiframe" onclick="javascript:show(\'remotesiframe\');">';
+$newRemoteStr = translate ( 'Add New Remote Calendar' );
+$targetStr =
+'target="remotesiframe" onclick="javascript:show (\'remotesiframe\');">';
 
 if ( ! $NONUSER_PREFIX ) {
-  echo print_error_header () . 
-      translate( 'NONUSER_PREFIX not set' ) . ".\n";
-  echo "</body>\n</html>";
+  echo print_error_header () . translate ( 'NONUSER_PREFIX not set' ) . '
+  </body>
+</html>';
   exit;
 }
 $add = getValue ( 'add' );
-?>
-  <a name="tabnonusers"></a>
-  <div id="tabscontent_remotes">
-<?php
-if ( empty ($error) ) {
-  echo '<a title="' . 
-  $newRemoteStr . '" href="edit_remotes.php?add=1"' . $targetStr . 
-  $newRemoteStr . "</a><br />\n";
+echo '
+    <a name="tabnonusers"></a>
+    <div id="tabscontent_remotes">';
+
+if ( empty ( $error ) ) {
+  echo '
+      <a title="' . $newRemoteStr . '" href="edit_remotes.php?add=1"'
+   . $targetStr . $newRemoteStr . '</a><br />';
   // Displaying Remote Calendars
-  $userlist = get_nonuser_cals ( $login, true);
+  $userlist = get_nonuser_cals ( $login, true );
   if ( ! empty ( $userlist ) ) {
-    echo '<ul>';
+    echo '
+      <ul>';
     for ( $i = 0, $cnt = count ( $userlist ); $i < $cnt; $i++ ) {
-      echo '<li><a title="' . 
-        $userlist[$i]['cal_fullname'] . '" href="edit_remotes.php?nid=' . 
-  $userlist[$i]['cal_login'] . '"' . $targetStr . 
-  $userlist[$i]['cal_fullname'] . "</a></li>\n";
+      echo '
+        <li><a title="' . $userlist[$i]['cal_fullname']
+       . '" href="edit_remotes.php?nid=' . $userlist[$i]['cal_login'] . '"'
+       . $targetStr . $userlist[$i]['cal_fullname'] . '</a></li>';
     }
-    echo "</ul>";
+    echo '
+      </ul>';
   }
 }
 
-echo '<iframe name="remotesiframe" id="remotesiframe" style="width:90%;border-width:0px; height:250px;"></iframe>';
+echo '
+      <iframe name="remotesiframe" id="remotesiframe" style="width: 90%; '
+ . 'border: 0; height: 250px;"></iframe>
+    </div>';
 ?>
-</div>
