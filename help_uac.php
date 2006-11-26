0144d7f9d57ae9c205d6d1a445ad060a83dc5607
bbannon <bbannon>
2006-11-26 20:59:17 +0000

    Utilize translations array.

--- help_uac.php
+++ help_uac.php
@@ -3,22 +3,21 @@
 include_once 'includes/init.php';
 include_once 'includes/help_list.php';
 
-$helpStr = translate ( 'Help' );
-$titleStr = translate ( 'User Access Control' );
 $descStr =
 translate ( 'Allows for fine control of user access and permissions. Users can also grant default and per individual permission if authorized by the administrator.' );
 
 print_header ( '', '', '', true, false, true );
 echo $helpListStr . '
     <div class="helpbody">
-      <h2>' . $helpStr . ': ' . $titleStr . '</h2>
+      <h2>' . $translations['Help'] . ': '
+       . $translations['User Access Control'] . '</h2>
       <p>' . $descStr . '</p>';
 $tmp_arr = array (
-  translate ( 'Can Email' ) =>
+  $translations['Can Email'] =>
   translate ( 'If disabled, this user cannot send you emails.' ),
-  translate ( 'Can Invite' ) =>
+  $translations['Can Invite'] =>
   translate ( 'If disabled, this user cannot see you in the participants list.' ),
-  translate ( 'Can See Time Only' ) =>
+  $translations['Can See Time Only'] =>
   translate ( 'If enabled, this user cannot view the details of any of your entries.' ),
   );
 list_help ( $tmp_arr );
