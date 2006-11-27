23f25be831c1863f94b825ae43c49127d474a6c8
bbannon <bbannon>
2006-11-27 07:16:33 +0000

    Cut dupe code. Lines < 80. Stay in PHP. Utilize translations array.

--- assistant_edit.php
+++ assistant_edit.php
@@ -2,78 +2,76 @@
 /* $Id$ */
 include_once 'includes/init.php';
 
-if ( empty ( $login) || $login == '__public__' ) {
+if ( empty ( $login ) || $login == '__public__' ) {
   // do not allow public access
   do_redirect ( empty ( $STARTVIEW ) ? 'month.php' : "$STARTVIEW" );
   exit;
 }
 
-if ($user != $login)
-  $user = (($is_admin || $is_nonuser_admin) && $user) ? $user : $login;
+if ( $user != $login )
+  $user = ( ( $is_admin || $is_nonuser_admin ) && $user ) ? $user : $login;
 
-if ( $GROUPS_ENABLED == 'Y' ) {
-  $INC = array('js/assistant_edit.php/true');
-} else {
-  $INC = '';
+print_header ( ( $GROUPS_ENABLED == 'Y'
+    ? array ( 'js/assistant_edit.php/true' ) : '' ) );
+
+echo '
+    <form action="assistant_edit_handler.php" method="post" '
+ . 'name="assistanteditform">' . ( $user ? '
+      <input type="hidden" name="user" value="' . $user . '" />' : '' ) . '
+      <h2>';
+
+if ( $is_nonuser_admin ) {
+  nonuser_load_variables ( $user, 'nonuser' );
+  echo $nonuserfullname . ' ' . $translations['Assistants'] . '<br />
+      -- ' . translate ( 'Admin mode' ) . ' --';
+} else
+  echo translate ( 'Your assistants' );
+
+echo '</h2>
+      ' . display_admin_link () . '
+      <table>
+        <tr>
+          <td class="aligntop"><label for="users">'
+ . $translations['Assistants'] . ':</label></td>
+          <td>
+            <select name="users[]" id="users" size="10" multiple="multiple">';
+
+// get list of all users
+$users = get_my_users ();
+// get list of users for this view
+$res = dbi_execute ( 'SELECT cal_boss, cal_assistant FROM webcal_asst
+   WHERE cal_boss = ?', array ( $user ) );
+
+if ( $res ) {
+  while ( $row = dbi_fetch_row ( $res ) ) {
+    $assistantuser[$row[1]] = 1;
+  }
+  dbi_free_result ( $res );
 }
-print_header($INC);
-?>
 
-<form action="assistant_edit_handler.php" method="post" name="assistanteditform">
-<?php
-  if ($user) echo "<input type=\"hidden\" name=\"user\" value=\"$user\" />\n";
-  if ( $is_nonuser_admin ) {
-    nonuser_load_variables ( $user, 'nonuser' );
-    echo '<h2>' . $nonuserfullname . " " . translate( 'Assistants' )
-        ."<br />\n-- " . translate( 'Admin mode' ) . " --</h2>\n";
-  } else {
-    echo '<h2>' . translate( 'Your assistants' ) . "</h2>\n";
-  }
-
-echo display_admin_link();
-?>
-
-<table>
-<tr><td class="aligntop">
- <label for="users"><?php etranslate( 'Assistants' ); ?>:</label></td><td>
- <select name="users[]" id="users" size="10" multiple="multiple">
-<?php
-  // get list of all users
-  $users = get_my_users ();
-  // get list of users for this view
-  $sql = 'SELECT cal_boss, cal_assistant FROM webcal_asst WHERE cal_boss = ?';
-  $res = dbi_execute ( $sql, array( $user ) );
-  if ( $res ) {
-    while ( $row = dbi_fetch_row ( $res ) ) {
-      $assistantuser[$row[1]] = 1;
-    }
-    dbi_free_result ( $res );
-  }
-  for ( $i = 0, $cnt = count ( $users ); $i < $cnt; $i++ ) {
-    $u = $users[$i]['cal_login'];
-    if ($u == $login ) continue;
-    if ($u == '__public__' ) continue;
-    echo "<option value=\"$u\"";
-    if ( ! empty ( $assistantuser[$u] ) ) {
-      echo ' selected="selected"';
-    }
-    echo '>' . $users[$i]['cal_fullname'] . "</option>\n";
-  }
-?>
-</select>
-<?php
-if ( $GROUPS_ENABLED == 'Y' ) {
-  echo '<input type="button" onclick="selectUsers()" value="' .
-    translate( 'Select' ) . "...\" />\n";
+for ( $i = 0, $cnt = count ( $users ); $i < $cnt; $i++ ) {
+  $u = $users[$i]['cal_login'];
+  if ( $u == $login || $u == '__public__' )
+    continue;
+  echo '
+              <option value="' . $u . '"'
+   . ( ! empty ( $assistantuser[$u] ) ? ' selected="selected"' : '' ) . '>'
+   . $users[$i]['cal_fullname'] . '</option>';
 }
+
+echo '
+            </select>' . ( $GROUPS_ENABLED == 'Y' ? '
+            <input type="button" onclick="selectUsers ()" value="'
+   . $translations['Select'] . '..." />' : '' ) . '
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" class="aligncenter""><br /><input type="submit" '
+ . 'name="action" value="' . $translations['Save'] . '" />
+          </td>
+        </tr>
+      </table>
+    </form>
+    ' . print_trailer ();
+
 ?>
-</td></tr>
-<tr><td colspan="2" class="aligncenter"">
-<br />
-<input type="submit" name="action" value="<?php etranslate( 'Save' ); ?>" />
-</td></tr>
-</table>
-</form>
-
-<?php echo print_trailer(); ?>
-
