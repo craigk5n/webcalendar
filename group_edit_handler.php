3cff894e2c5fb578289cac83541157dbef174604
bbannon <bbannon>
2006-11-27 05:32:26 +0000

    Lines < 80.

--- group_edit_handler.php
+++ group_edit_handler.php
@@ -2,54 +2,54 @@
 /* $Id$ */
 include_once 'includes/init.php';
 
-if ( ! $is_admin ) {
+if ( ! $is_admin )
   $error = print_not_auth ();
-} else  {
+else {
   $delete = getPostValue ( 'delete' );
   if ( ! empty ( $delete ) ) {
     // delete this group
-    dbi_execute ( 'DELETE FROM webcal_group WHERE cal_group_id = ? ', array( $id ) );
-    dbi_execute ( 'DELETE FROM webcal_group_user WHERE cal_group_id = ? ', array( $id ) );
+    dbi_execute ( 'DELETE FROM webcal_group WHERE cal_group_id = ? ',
+      array ( $id ) );
+    dbi_execute ( 'DELETE FROM webcal_group_user WHERE cal_group_id = ? ',
+      array ( $id ) );
   } else {
     $date = date ( 'Ymd' );
-    if ( empty ( $groupname ) ) {
-      $error = translate( 'You must specify a group name' );
-    }
-    else if ( ! empty ( $id ) ) {
+    if ( empty ( $groupname ) )
+      $error = translate ( 'You must specify a group name' );
+    else
+    if ( ! empty ( $id ) ) {
       # update
-      if ( ! dbi_execute ( 'UPDATE webcal_group SET cal_name = ' .
-        '?, cal_last_update = ? ' .
-        'WHERE cal_group_id = ?', array( $groupname, $date, $id ) ) ) {
+      if ( ! dbi_execute ( 'UPDATE webcal_group SET cal_name = ?,
+        cal_last_update = ? WHERE cal_group_id = ?',
+          array ( $groupname, $date, $id ) ) )
         $error = db_error ();
-      }
     } else {
       # new... get new id first
-      $res = dbi_execute ( 'SELECT MAX(cal_group_id) FROM webcal_group' );
+      $res = dbi_execute ( 'SELECT MAX( cal_group_id ) FROM webcal_group' );
       if ( $res ) {
         $row = dbi_fetch_row ( $res );
         $id = $row[0];
         $id++;
         dbi_free_result ( $res );
-        $sql = 'INSERT INTO webcal_group ' .
-          '( cal_group_id, cal_owner, cal_name, cal_last_update ) VALUES ( ?, ?, ?, ? )';
-        if ( ! dbi_execute ( $sql, array( $id, $login, $groupname, $date ) ) ) {
+        if ( ! dbi_execute ( 'INSERT INTO webcal_group ( cal_group_id, cal_owner,
+          cal_name, cal_last_update ) VALUES ( ?, ?, ?, ? )',
+            array ( $id, $login, $groupname, $date ) ) )
           $error = db_error ();
-        }
-      } else {
+      } else
         $error = db_error ();
-      }
     }
-  
     # update user list
-    if ( empty ( $error ) &&  ! empty ( $users ) ) {
-      dbi_execute ( 'DELETE FROM webcal_group_user WHERE cal_group_id = ?', array( $id ) );
-      for ( $i = 0, $cnt = count ( $users ); $i < $cnt ; $i++ ) {
-        dbi_execute ( 'INSERT INTO webcal_group_user ( cal_group_id, cal_login ) ' .
-          'VALUES ( ?, ? )', array( $id, $users[$i] ) );
+    if ( empty ( $error ) && ! empty ( $users ) ) {
+      dbi_execute ( 'DELETE FROM webcal_group_user WHERE cal_group_id = ?',
+        array ( $id ) );
+      for ( $i = 0, $cnt = count ( $users ); $i < $cnt; $i++ ) {
+        dbi_execute ( 'INSERT INTO webcal_group_user ( cal_group_id, cal_login )
+          VALUES ( ?, ? )', array ( $id, $users[$i] ) );
       }
     }
   }
 }
 
-echo error_check('users.php', false); 
+echo error_check ( 'users.php', false );
+
 ?>
