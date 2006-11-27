c849f64e07f9990ee1a70958252ff716e376941a
bbannon <bbannon>
2006-11-27 04:59:13 +0000

    Stay in PHP. Cut dupe code. Lines < 80. Utilize translations array.

--- js_cacher.php
+++ js_cacher.php
@@ -1,43 +1,43 @@
 <?php
 /* $Id$ */
- //if the javascript doesn't need any input from php, then we can cache it
- // and not run init.php
+// If the javascript doesn't need any input from php,
+// then we can cache it and not run init.php.
 $caching = false;
 if ( empty ( $inc ) )
   $inc = $_GET['inc'];
+
 $arinc = explode ( '/', $inc );
 
-if ( $arinc[0] != 'js')
- return false;
- 
-if ( ! empty ( $arinc[2] ) &&  preg_match ( "/true/", $arinc[2] ) ) { 
-  $caching = true;
-}
+if ( $arinc[0] != 'js' )
+  return false;
 
-header( 'Content-type: text/javascript' ); 
+if ( ! empty ( $arinc[2] ) && stristr ( $arinc[2], 'true' ) )
+  $caching = true;
+
+header ( 'Content-type: text/javascript' );
 if ( $caching == true ) {
-  header('Last-Modified: '.date('r'));
-  header('Expires: ' . date('D, j M Y H:i:s', time() + 86400) . ' UTC');
-  header('Cache-Control: Public');
-  header('Pragma: Public');
-} else{
+  header ( 'Last-Modified: ' . date ( 'r' ) );
+  header ( 'Expires: ' . date ( 'D, j M Y H:i:s', time () + 86400 ) . ' UTC' );
+  header ( 'Cache-Control: Public' );
+  header ( 'Pragma: Public' );
+} else {
   require_once 'includes/classes/WebCalendar.class';
-     
-  $WebCalendar =& new WebCalendar ( __FILE__ );    
-     
-  include 'includes/config.php';    
-  include 'includes/dbi4php.php';    
-  include 'includes/functions.php';    
-     
-  $WebCalendar->initializeFirstPhase();    
-     
-  include "includes/$user_inc";
-  include_once 'includes/access.php';    
-  include_once 'includes/validate.php';    
-  include 'includes/translate.php';  
+
+  $WebCalendar =& new WebCalendar ( __FILE__ );
+
+  include 'includes/config.php';
+  include 'includes/dbi4php.php';
+  include 'includes/functions.php';
+
+  $WebCalendar->initializeFirstPhase ();
+
+  include 'includes/' . $user_inc;
+  include_once 'includes/access.php';
+  include_once 'includes/validate.php';
+  include 'includes/translate.php';
   include_once 'includes/gradient.php';
 
-  $WebCalendar->initializeSecondPhase();
+  $WebCalendar->initializeSecondPhase ();
 
   load_global_settings ();
   load_user_preferences ();
@@ -45,12 +45,12 @@ if ( $caching == true ) {
   send_no_cache_header ();
 }
 
-// we don't want to compress for IE6 because of 'object expected' errors
-$isIE6 = ereg ( "MSIE 6", $_SERVER['HTTP_USER_AGENT'] );
-if ( ini_get ( 'zlib.output_compression' ) != 1  && ! $isIE6 ) 
-  ob_start( 'ob_gzhandler' );
+// We don't want to compress for IE6 because of 'object expected' errors.
+if ( ini_get ( 'zlib.output_compression' ) != 1 && !
+ stristr ( $_SERVER['HTTP_USER_AGENT'], 'MSIE 6' ) )
+  ob_start ( 'ob_gzhandler' );
 
-$newinc = $arinc[0] . '/' . $arinc[1]; 
-include_once ( "includes/$newinc" );
+$newinc = $arinc[0] . '/' . $arinc[1];
+include_once ( 'includes/' . $newinc );
 
 ?>
