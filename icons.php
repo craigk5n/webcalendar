14fe41cedf81cf64b5644712a2b839f2bf47ea8c
bbannon <bbannon>
2006-11-27 07:21:49 +0000

    Cut dupe code. Lines < 80.

--- icons.php
+++ icons.php
@@ -3,68 +3,83 @@
 include_once 'includes/init.php';
 $icon_path = 'icons/';
 
-$can_edit = ( is_dir($icon_path) && ( ! empty ( $ENABLE_ICON_UPLOADS ) &&
-  $ENABLE_ICON_UPLOADS == 'Y' || $is_admin ));
-  
-if ( ! $can_edit ) do_redirect ( 'category.php' );
+$can_edit = ( is_dir ( $icon_path ) &&
+  ( $ENABLE_ICON_UPLOADS == 'Y' || $is_admin ) );
 
-$INC = array('js/visible.php');
-print_header($INC,'','',true);
+if ( ! $can_edit )
+  do_redirect ( 'category.php' );
 
-$icons = array();
+print_header ( array ( 'js/visible.php' ), '', '', true );
 
-if($d = dir($icon_path)) {
-  while (false !== ($entry = $d->read())) {
-    if(substr($entry,-3,3) == 'gif' ) {
+$icons = array ();
+
+if ( $d = dir ( $icon_path ) ) {
+  while ( false !== ( $entry = $d->read () ) ) {
+    if ( substr ( $entry, -3, 3 ) == 'gif' ) {
       $data = '';
-      //we''ll compare the files to eliminate duplicates
+      // We''ll compare the files to eliminate duplicates.
       $fd = @fopen ( $icon_path . $entry, 'rb' );
       if ( $fd ) {
-        //we only need to compare the first 1kb
+        // We only need to compare the first 1kb.
         $data .= fgets ( $fd, 1024 );
-        $icons[md5( $data )] = $entry;
+        $icons[md5 ( $data )] = $entry;
       }
-      fclose ( $fd );        
+      fclose ( $fd );
     }
   }
-  $d->close();
-  //remove duplicates are replace keys with 0...n
+  $d->close ();
+  // Remove duplicates and replace keys with 0...n.
   $icons = array_unique ( $icons );
   sort ( $icons );
   $title_str = translate ( 'Click to Select' );
-?>       
+
+  ?>
   <script language="JavaScript" type="text/javascript">
   <!-- <![CDATA[
   function sendURL ( url ) {
-    var thisInput = window.opener.document.catform.urlname;
-    var thisPic = window.opener.document.images.urlpic;
-    var thistr1 = window.opener.document.getElementById('cat_icon');
-    var thistr2 = window.opener.document.getElementById('remove_icon');
+    var
+      thisInput = window.opener.document.catform.urlname,
+      thisPic = window.opener.document.images.urlpic,
+      thistr1 = window.opener.document.getElementById ('cat_icon'),
+      thistr2 = window.opener.document.getElementById ('remove_icon');
     thisInput.value = url.substring (6);
     thisPic.src = url;
-    thistr1.style.visibility = "visible";
+    thistr1.style.visibility =
     thistr2.style.visibility = "visible";
     window.close ();
-  }     
-  //]]> -->
-  </script> 
-  
-  <table align="center" border="0"><tr>
-    <td colspan="8" align="center">
-<?php
-  echo '<h2>' .  translate ( 'Current Icons on Server' )  . '</h2>';   
-  echo "</td></tr>\n<tr>";
-  for ( $i = 0, $cnt = count ( $icons ); $i < $cnt; $i++ ) {  
-    echo "<td><a href=\"#\" onclick=\"sendURL('".$icon_path. $icons[$i]."')\" >" .
-      '<img src="' .$icon_path . $icons[$i]. '" border="0" title="' . 
-      $title_str . '" alt="' . $title_str . "\" /></a></td>\n";
-    if ( $i > 0 && $i%8 == 0 ) 
-      echo "</tr><tr>\n";
   }
-  echo '</tr><tr><td colspan="8" align="center">';
-  echo $title_str . "</td>\n";
-  echo "</tr>\n</body>\n</html>\n";    
+  //]]> -->
+  </script>
 
-}  
-    
+<?php
+  ob_start ();
+
+  echo '
+    <table align="center" border="0">
+      <tr>
+        <td colspan="8" align="center"><h2>'
+   . translate ( 'Current Icons on Server' ) . '</h2></td>
+      </tr>
+      <tr>';
+  for ( $i = 0, $cnt = count ( $icons ); $i < $cnt; $i++ ) {
+    echo '
+        <td><a href="#" onclick="sendURL (\'' . $icon_path . $icons[$i]
+     . '\')" ><img src="' . $icon_path . $icons[$i] . '" border="0" title="'
+     . $title_str . '" alt="' . $title_str . '" /></a></td>'
+     . ( $i > 0 && $i % 8 == 0 ? '
+      </tr>
+      <tr>' : '' );
+  }
+  echo '
+      </tr>
+      <tr>
+        <td colspan="8" align="center">' . $title_str . '</td>
+      </tr>
+    </table>
+  </body>
+</html>';
+
+  ob_end_flush ();
+}
+
 ?>
