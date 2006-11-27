efe9429ca59cbeee1f5a4e8e59c1d37fe57d0c0a
bbannon <bbannon>
2006-11-27 04:59:13 +0000

    Stay in PHP. Cut dupe code. Lines < 80. Utilize translations array.

--- views.php
+++ views.php
@@ -5,55 +5,52 @@ include_once 'includes/init.php';
 if ( ! $is_admin )
   $user = $login;
 
-$INC = array('js/views.php/true','js/visible.php');
-print_header($INC);
+print_header ( array ( 'js/views.php/true', 'js/visible.php' ) );
 
-echo display_admin_link();
-?>
+ob_start ();
+
+echo display_admin_link () . '
 <!-- TABS -->
-<div id="tabs">
-  <span class="tabfor" id="tab_views"><a href="#tabviews" onclick="return showTab('views')"><?php 
-    echo translate( 'Views' );
-  ?></a></span>
-</div>
+    <div id="tabs">
+      <span class="tabfor" id="tab_views"><a href="#tabviews" onclick="return '
+ . 'showTab (\'views\')>' . $translations['Views'] . '</a></span>
+    </div>
 
 <!-- TABS BODY -->
-<div id="tabscontent">
-  <!-- VIEWS -->
-  <a name="tabviews"></a>
-  <div id="tabscontent_views">
-<?php
-  echo '<a title="' . 
-  translate( 'Add New View' ) . '" href="views_edit.php" target="viewiframe" onclick="javascript:show(\'viewiframe\');">' . 
-  translate( 'Add New View' ) . "</a>\n";
-?>
-  <ul>
-<?php
+    <div id="tabscontent">
+<!-- VIEWS -->
+      <a name="tabviews"></a>
+      <div id="tabscontent_views">
+        <a title="' . translate ( 'Add New View' )
+ . '" href="views_edit.php" target="viewiframe" onclick="javascript:show '
+ . '(\'viewiframe\');">' . $translations['Add New View'] . '</a>
+        <ul>';
+
 $global_found = false;
 for ( $i = 0, $cnt = count ( $views ); $i < $cnt; $i++ ) {
   if ( $views[$i]['cal_is_global'] != 'Y' || $is_admin ) {
-    echo '<li><a title="' . 
-      htmlspecialchars ( $views[$i]['cal_name'] ) .
-      '" href="views_edit.php?id=' . 
-      $views[$i]['cal_view_id'] .
-      '" target="viewiframe" onclick="javascript:show(\'viewiframe\');">' . 
-      htmlspecialchars ( $views[$i]['cal_name'] ) . '</a>';
+    echo '
+          <li><a title="' . htmlspecialchars ( $views[$i]['cal_name'] )
+     . '" href="views_edit.php?id=' . $views[$i]['cal_view_id']
+     . '" target="viewiframe" onclick="javascript:show (\'viewiframe\');">'
+     . htmlspecialchars ( $views[$i]['cal_name'] ) . '</a>';
     if ( $views[$i]['cal_is_global'] == 'Y' ) {
-      echo '&nbsp;<abbr title="' . translate( 'Global' ) . '">*</abbr>';
+      echo '&nbsp;<abbr title="' . $translations['Global'] . '">*</abbr>';
       $global_found = true;
     }
-    echo "</li>\n";
+    echo '</li>';
   }
 }
-?>
-</ul>
-<?php
-  if ( $global_found )
-    echo "<br />\n*&nbsp;" . translate ( 'Global' );
-?>
-<br />
-<iframe name="viewiframe" id="viewiframe" style="width:90%;border-width:0px; height:343px;"></iframe>
-</div>
-</div>
-<?php echo print_trailer(); ?>
 
+ob_end_flush ();
+
+echo '
+        </ul>' . ( $global_found ? '<br />
+        *&nbsp;' . $translations['Global'] : '' ) . '<br />
+        <iframe name="viewiframe" id="viewiframe" style="width: 90%; border: 0;'
+ . ' height: 343px;"></iframe>
+      </div>
+    </div>
+    ' . print_trailer ();
+
+?>
