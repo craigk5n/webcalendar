69e1f915a38e4dba415a045b51f855cd8fabc690
bbannon <bbannon>
2006-11-27 05:58:05 +0000

    Better formatting.

--- ajax.php
+++ ajax.php
@@ -2,7 +2,7 @@
 /* $Id$
  *
  * Description
- * This is the handler for Ajax httpXmlRequests
+ * This is the handler for Ajax httpXmlRequests.
  */
 require_once 'includes/classes/WebCalendar.class';
 
@@ -25,38 +25,36 @@ load_global_settings ();
 load_user_preferences ();
 $WebCalendar->setLanguage();
 
-$page = getPostValue ( 'page' );
 $name = getPostValue ( 'name' );
+$page = getPostValue ( 'page' );
 // we're processing edit_remotes Calendar ID field
 if ( $page == 'edit_remotes' || $page == 'edit_nonuser' ) {
   $res = dbi_execute ( 'SELECT cal_login FROM webcal_nonuser_cals
     WHERE cal_login = ?', array ( $NONUSER_PREFIX . $name ) );
   if ( $res ) {
     $row = dbi_fetch_row ( $res );
-    // assuming we are using '_NUC_' as $NONUSER_PREFIX
+    // Presuming we are using '_NUC_' as $NONUSER_PREFIX.
     if ( $name == substr ( $row[0], strlen ( $NONUSER_PREFIX ) ) )
       echo translate ( 'Duplicate Name', true ) . ": $name";
   }
-} else
-  // we're processing username field
-  if ( $page == 'register' || $page == 'edit_user' ) {
-    $res = dbi_execute ( 'SELECT cal_login FROM webcal_user WHERE cal_login = ?',
-      array ( $name ) );
-    if ( $res ) {
-      $row = dbi_fetch_row ( $res );
-      if ( $row[0] == $name )
-        echo translate ( 'Username already exists', true ) . ": $name";
-    }
-  } else
-    // we're processing email field from any page field
-    if ( $page == 'email' ) {
-      $res = dbi_execute ( 'SELECT cal_email FROM webcal_user
-        WHERE cal_email = ?', array ( $name ) );
-      if ( $res ) {
-        $row = dbi_fetch_row ( $res );
-        if ( $row[0] == $name )
-          echo translate ( 'Email address already exists', true ) . ": $name";
-      }
-    }
+} elseif ( $page == 'register' || $page == 'edit_user' ) {
+  // We're processing username field.
+  $res = dbi_execute ( 'SELECT cal_login FROM webcal_user WHERE cal_login = ?',
+    array ( $name ) );
+  if ( $res ) {
+    $row = dbi_fetch_row ( $res );
+    if ( $row[0] == $name )
+      echo translate ( 'Username already exists', true ) . ": $name";
+  }
+} elseif ( $page == 'email' ) {
+  // We're processing email field from any page field.
+  $res = dbi_execute ( 'SELECT cal_email FROM webcal_user
+    WHERE cal_email = ?', array ( $name ) );
+  if ( $res ) {
+    $row = dbi_fetch_row ( $res );
+    if ( $row[0] == $name )
+      echo translate ( 'Email address already exists', true ) . ": $name";
+  }
+}
 
-    ?>
+?>
