565461e4fda3690520c76a14c1b3f09b39656cb1
bbannon <bbannon>
2006-11-27 06:06:14 +0000

    Utilize translations array.

--- help_import.php
+++ help_import.php
@@ -2,10 +2,13 @@
 /* $Id$ */
 include_once 'includes/init.php';
 include_once 'includes/help_list.php';
+
 print_header ( '', '', '', true );
+
 ob_start ();
+
 echo $helpListStr . '
-    <h2>' . translate ( 'Help' ) . ': ' . translate ( 'Import' ) . '</h2>
+    <h2>' . $translations['Help'] . ': ' . translate ( 'Import' ) . '</h2>
     <h3>' . translate ( 'Palm Desktop' ) . '</h3>
     <p>' .
 translate ( 'This form will allow you to import entries from the Palm Desktop Datebook.' )
@@ -38,7 +41,9 @@ translate ( 'Anything imported from Palm will be overwritten during the next imp
  . '</b>, ' .
 translate ( 'will cause events imported previously, that used the same UID as an event from the new import file, to be marked as deleted . This should allow an updated iCalendar file to be imported without creating duplicates.' )
  . '</p>';
+
 ob_end_flush ();
+
 echo print_trailer ( false, true, true );
 
 ?>
