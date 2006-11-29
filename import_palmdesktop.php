21d7a0dd66fa6d070b9d4aa9a65a4d47674f2506
bbannon <bbannon>
2006-11-29 16:16:47 +0000

    Cut dupe code. Lines < 80.

--- import_palmdesktop.php
+++ import_palmdesktop.php
@@ -1,43 +1,51 @@
 <?php
 /* $Id$ */
 // Parse the datebook file and return the data hash.
-//
-function parse_palmdesktop ($file, $exc_private = 1) {
-  $file = EscapeShellArg($file);
-  $exc_private = EscapeShellArg($exc_private);
-  exec ("perl tools/palm_datebook.pl $file $exc_private", $Entries);
+function parse_palmdesktop ( $file, $exc_private = 1 ) {
+  $file = EscapeShellArg ( $file );
+  $exc_private = EscapeShellArg ( $exc_private );
+  exec ( 'perl tools/palm_datebook.pl' . " $file $exc_private", $Entries );
   $data = array ();
-  while ( list( $line_num, $line ) = each( $Entries ) ) {
-    $data[] = ParseLine($line);
+  while ( list ( $line_num, $line ) = each ( $Entries ) ) {
+    $data[] = ParseLine ( $line );
   }
   return $data;
 }
 
-// Delete all Palm Events for $login to clear any events deleted in the palm
-// Return 1 if success
-function delete_palm_events($login) {
-  $res = dbi_execute ( 'SELECT cal_id FROM webcal_import_data ' .
-    'WHERE cal_login = ? AND cal_import_type = ?', array( $login, 'palm' ) );
+// Delete all Palm Events for $login to clear any events deleted in the palm.
+// Return 1 if successful.
+function delete_palm_events ( $login ) {
+  $res = dbi_execute ( 'SELECT cal_id FROM webcal_import_data
+    WHERE cal_login = ? AND cal_import_type = ?', array ( $login, 'palm' ) );
   if ( $res ) {
-     while ( $row = dbi_fetch_row ( $res ) ) {
-       dbi_execute ( 'DELETE FROM webcal_entry_user WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_entry_repeats WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_entry_repeats_not WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_entry_log WHERE cal_entry_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_site_extras WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_reminders WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_import_data WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_blob WHERE cal_id = ?', array( $row[0] ) );
-       dbi_execute ( 'DELETE FROM webcal_entry WHERE cal_id = ?', array( $row[0] ) );
-     }
+    while ( $row = dbi_fetch_row ( $res ) ) {
+      dbi_execute ( 'DELETE FROM webcal_entry_user WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_entry_repeats WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_entry_repeats_not WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_entry_log WHERE cal_entry_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_site_extras WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_reminders WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_import_data WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_blob WHERE cal_id = ?',
+        array ( $row[0] ) );
+      dbi_execute ( 'DELETE FROM webcal_entry WHERE cal_id = ?',
+        array ( $row[0] ) );
+    }
   }
   dbi_free_result ( $res );
   return 1;
 }
 
-function ParseLine($line){
+function ParseLine ( $line ) {
   global $calUser;
-  list(
+  list (
     $Entry['RecordID'],
     $Entry['StartTime'],
     $Entry['EndTime'],
@@ -56,19 +64,21 @@ function ParseLine($line){
     $Exceptions,
     $Entry['Repeat']['RepeatDays'],
     $WeekNum,
-      ) = explode('|', $line);
+    ) = explode ( '|', $line );
 
   // Adjust times to users Timezone if not Untimed
-  if ( isset ( $Entry['Untimed'] ) && $Entry['Untimed'] == 0) {
-
-    $tz_offset = date ('Z', $Entry['StartTime'] );
+  if ( isset ( $Entry['Untimed'] ) && $Entry['Untimed'] == 0 ) {
+    $tz_offset = date ( 'Z', $Entry['StartTime'] );
     $Entry['StartTime'] -= $tz_offset;
-    $tz_offset = date ('Z', $Entry['EndTime'] );		
+    $tz_offset = date ( 'Z', $Entry['EndTime'] );
     $Entry['EndTime'] -= $tz_offset;
   }
 
-  if ($Exceptions) $Entry['Repeat']['Exceptions'] = explode(': ',$Exceptions);
-  if (($WeekNum == '5') && ($Entry['Repeat']['Interval'] == '3')) $Entry['Repeat']['Interval'] = '6';
+  if ( $Exceptions )
+    $Entry['Repeat']['Exceptions'] = explode ( ': ', $Exceptions );
+  if ( ( $WeekNum == '5' ) && ( $Entry['Repeat']['Interval'] == '3' ) )
+    $Entry['Repeat']['Interval'] = '6';
   return $Entry;
 }
+
 ?>
