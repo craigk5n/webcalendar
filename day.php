f1e843b980aee93b07248fda7222569940945e66
bbannon <bbannon>
2006-11-29 17:07:13 +0000

    Cut dupe code. Lines < 80.

--- day.php
+++ day.php
@@ -2,101 +2,94 @@
 /* $Id$ */
 include_once 'includes/init.php';
 
-if (($user != $login) && $is_nonuser_admin)
-  load_user_layers ($user);
+if ( ( $user != $login ) && $is_nonuser_admin )
+  load_user_layers ( $user );
 else
   load_user_layers ();
 
 load_user_categories ();
 
-$wday = strftime ( "%w", mktime ( 0, 0, 0, $thismonth, $thisday, $thisyear ) );
+$wday = strftime ( '%w', mktime ( 0, 0, 0, $thismonth, $thisday, $thisyear ) );
 
 $now = mktime ( 0, 0, 0, $thismonth, $thisday, $thisyear );
-$nowYmd = date ( 'Ymd', $now );
 
 $next = mktime ( 0, 0, 0, $thismonth, $thisday + 1, $thisyear );
-$nextYmd = date ( 'Ymd', $next );
-$nextyear = date ( 'Y', $next );
-$nextmonth = date ( 'm', $next );
 $nextday = date ( 'd', $next );
+$nextmonth = date ( 'm', $next );
+$nextyear = date ( 'Y', $next );
+$nextYmd = date ( 'Ymd', $next );
 
 $prev = mktime ( 0, 0, 0, $thismonth, $thisday - 1, $thisyear );
-$prevYmd = date ( 'Ymd', $prev );
-$prevyear = date ( 'Y', $prev );
-$prevmonth = date ( 'm', $prev );
 $prevday = date ( 'd', $prev );
+$prevmonth = date ( 'm', $prev );
+$prevyear = date ( 'Y', $prev );
+$prevYmd = date ( 'Ymd', $prev );
 
 if ( empty ( $TIME_SLOTS ) )
   $TIME_SLOTS = 24;
 
-if ( ! empty ( $BOLD_DAYS_IN_YEAR ) && $BOLD_DAYS_IN_YEAR == 'Y' ) {
- $boldDays = true;
-} else {
- $boldDays = false;
-}
+$boldDays = ( $BOLD_DAYS_IN_YEAR == 'Y' );
 
 $startdate = mktime ( 0, 0, 0, $thismonth, 0, $thisyear );
-$enddate = mktime ( 23, 59, 59, $thismonth +1 , 0, $thisyear );
+$enddate = mktime ( 23, 59, 59, $thismonth + 1, 0, $thisyear );
 
-$smallTasks = $unapprovedStr = $printerStr = '';
+$printerStr = $smallTasks = $unapprovedStr = '';
 
 /* Pre-Load the repeated events for quckier access */
-$repeated_events = read_repeated_events ( empty ( $user ) ? $login : $user,
-  $cat_id, $startdate, $enddate  );
+$repeated_events = read_repeated_events ( empty ( $user )
+  ? $login : $user, $cat_id, $startdate, $enddate );
 
 /* Pre-load the non-repeating events for quicker access */
-$events = read_events ( empty ( $user ) ? $login : $user, $startdate, $enddate,
-  $cat_id  );
+$events = read_events ( empty ( $user )
+  ? $login : $user, $startdate, $enddate, $cat_id );
 
-if ( empty ( $DISPLAY_TASKS_IN_GRID ) ||  $DISPLAY_TASKS_IN_GRID == 'Y' ) {
+if ( empty ( $DISPLAY_TASKS_IN_GRID ) || $DISPLAY_TASKS_IN_GRID == 'Y' )
   /* Pre-load tasks for quicker access */
   $tasks = read_tasks ( ( ! empty ( $user ) && strlen ( $user ) )
     ? $user : $login, $now, $cat_id );
-}
-if ( ! empty ( $DISPLAY_TASKS ) && $DISPLAY_TASKS == 'Y' ) {
-  $smallTasks = display_small_tasks ( $cat_id );
-}
 
-$navStr = display_navigation( 'day' );
-$dayStr =  print_day_at_a_glance ( date ( 'Ymd', $now ),
-  empty ( $user ) ? $login : $user, $can_add );
+if ( $DISPLAY_TASKS == 'Y' )
+  $smallTasks = display_small_tasks ( $cat_id );
+
+$dayStr = print_day_at_a_glance ( date ( 'Ymd', $now ), empty ( $user )
+  ? $login : $user, $can_add );
+$navStr = display_navigation ( 'day' );
 $smallMonthStr = display_small_month ( $thismonth, $thisyear, true );
 if ( empty ( $friendly ) ) {
-  $unapprovedStr = display_unapproved_events ( ( $is_assistant || 
-    $is_nonuser_admin ? $user : $login ) );
+  $unapprovedStr = display_unapproved_events (
+    ( $is_assistant || $is_nonuser_admin ? $user : $login ) );
   $printerStr = generate_printer_friendly ( 'day.php' );
 }
-$eventinfo = ( ! empty ( $eventinfo )? $eventinfo : '' );
+$eventinfo = ( ! empty ( $eventinfo ) ? $eventinfo : '' );
 $trailerStr = print_trailer ();
 $HeadX = generate_refresh_meta ();
-$INC = array('js/popups.php/true');
-print_header($INC,$HeadX);
+print_header ( array ( 'js/popups.php/true' ), $HeadX );
 
 echo <<<EOT
-  <table width="100%" cellpadding="1">
-    <tr>
-      <td class="aligntop" width="80%" >
-      {$navStr}
-      </td>
-      <td>&nbsp;</td>
-   </tr>
-   <tr>
-     <td>
-      {$dayStr}
-     </td>
-     <td class="aligntop" rowspan="2">
-       <!-- START MINICAL -->
-       <div class="minicalcontainer aligncenter">
-        {$smallMonthStr}
-       </div>
-       <br />
-        {$smallTasks}
-      </td>
-    </tr>
-  </table>
-  {$eventinfo}
-  {$unapprovedStr}
-  {$printerStr}
-  {$trailerStr}
+    <table width="100%" cellpadding="1">
+      <tr>
+        <td class="aligntop" width="80%">
+          {$navStr}
+        </td>
+        <td>&nbsp;</td>
+      </tr>
+      <tr>
+        <td>
+          {$dayStr}
+        </td>
+        <td class="aligntop" rowspan="2">
+          <!-- START MINICAL -->
+          <div class="minicalcontainer aligncenter">
+            {$smallMonthStr}
+          </div><br />
+          {$smallTasks}
+        </td>
+      </tr>
+    </table>
+    {$eventinfo}
+    {$unapprovedStr}
+    {$printerStr}
+    {$trailerStr}
 EOT;
+
 ?>
