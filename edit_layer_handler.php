c7204fda368e1834887617082f0c07fdb71c3525
bbannon <bbannon>
2006-11-29 15:50:17 +0000

    Cut dupe code. Lines < 80.

--- edit_layer_handler.php
+++ edit_layer_handler.php
@@ -4,9 +4,8 @@ include_once 'includes/init.php';
 
 $error = '';
 
-if ( $ALLOW_VIEW_OTHER != 'Y' ) {
+if ( $ALLOW_VIEW_OTHER != 'Y' )
   $error = print_not_auth ();
-}
 
 if ( empty ( $dups ) )
   $dups = 'N';
@@ -15,74 +14,72 @@ $updating_public = false;
 if ( $is_admin && ! empty ( $public ) && $PUBLIC_ACCESS == 'Y' ) {
   $updating_public = true;
   save_layer ( '__public__', $layeruser, $layercolor, $dups, $id );
-} else if ( empty ( $cal_login ) ){
+} else
+if ( empty ( $cal_login ) )
   save_layer ( $login, $layeruser, $layercolor, $dups, $id );
-} else 
-//see if we are processing multiple layer_users as admin
-if ( $is_admin && ! empty ( $cal_login ) ) {
-  for ( $i=0, $cnt = count ( $cal_login ); $i < $cnt; $i++ ) {
-    save_layer ( $cal_login[$i], $layeruser, $layercolor, 'N', $id ); 
+else {
+  // See if we are processing multiple layer_users as admin.
+  if ( $is_admin && ! empty ( $cal_login ) ) {
+    for ( $i = 0, $cnt = count ( $cal_login ); $i < $cnt; $i++ ) {
+      save_layer ( $cal_login[$i], $layeruser, $layercolor, 'N', $id );
+    }
   }
 }
-
-
 
 function save_layer ( $layer_user, $layeruser, $layercolor, $dups, $id ) {
   global $error, $layers;
   if ( $layer_user == $layeruser )
     $error = translate ( 'You cannot create a layer for yourself' ) . '.';
-  
+
   load_user_layers ( $layer_user, 1 );
-  
+
   if ( ! empty ( $layeruser ) && $error == '' ) {
     // existing layer entry
     if ( ! empty ( $layers[$id]['cal_layeruser'] ) ) {
-      // update existing layer entry for this user
+      // Update existing layer entry for this user.
       $layerid = $layers[$id]['cal_layerid'];
-  
-      dbi_execute ( 'UPDATE webcal_user_layers SET cal_layeruser = ?, ' .
-        'cal_color = ?, cal_dups = ? WHERE cal_layerid = ?', 
-        array( $layeruser, $layercolor, $dups, $layerid ) );
-  
+
+      dbi_execute ( 'UPDATE webcal_user_layers SET cal_layeruser = ?,
+        cal_color = ?, cal_dups = ? WHERE cal_layerid = ?',
+        array ( $layeruser, $layercolor, $dups, $layerid ) );
     } else {
       // new layer entry
-      // check for existing layer for user.  can only have one layer per user
-      $res = dbi_execute ( 'SELECT COUNT(cal_layerid) FROM webcal_user_layers ' .
-        'WHERE cal_login = ? AND cal_layeruser = ?', array( $layer_user, $layeruser ) );
+      // Check for existing layer for user. Can only have one layer per user.
+      $res = dbi_execute ( 'SELECT COUNT (cal_layerid) FROM webcal_user_layers
+        WHERE cal_login = ? AND cal_layeruser = ?',
+        array ( $layer_user, $layeruser ) );
       if ( $res ) {
         $row = dbi_fetch_row ( $res );
-        if ( $row[0] > 0 ) {
-          $error = translate ( 'You can only create one layer for each user' ) . '.';
-        }
+        if ( $row[0] > 0 )
+          $error = translate ( 'You can only create one layer for each user' )
+           . '.';
+
         dbi_free_result ( $res );
       }
       if ( $error == '' ) {
-        $res = dbi_execute ( 'SELECT MAX(cal_layerid) FROM webcal_user_layers' );
+        $res =
+        dbi_execute ( 'SELECT MAX( cal_layerid ) FROM webcal_user_layers' );
         if ( $res ) {
           $row = dbi_fetch_row ( $res );
           $layerid = $row[0] + 1;
-        } else {
+        } else
           $layerid = 1;
-        }
-        dbi_execute ( 'INSERT INTO webcal_user_layers ( cal_layerid, cal_login, ' .
-          'cal_layeruser, cal_color, cal_dups ) VALUES ( ?, ?, ?, ?, ? )', 
-          array( $layerid, $layer_user, $layeruser, $layercolor, $dups ) );
+
+        dbi_execute ( 'INSERT INTO webcal_user_layers ( cal_layerid, cal_login,
+          cal_layeruser, cal_color, cal_dups ) VALUES ( ?, ?, ?, ?, ? )',
+          array ( $layerid, $layer_user, $layeruser, $layercolor, $dups ) );
       }
     }
   }
 }
-
-//We don't want to throw error if doing a multiple save
+// .
+// We don't want to throw error if doing a multiple save.
 if ( $error == '' || ! empty ( $cal_login ) ) {
-  if ( $updating_public )
-    do_redirect ( 'layers.php?public=1' );
-  else
-    do_redirect ( 'layers.php' );
+  do_redirect ( 'layers.php' . ( $updating_public ? '?public=1' : '' ) );
   exit;
 }
 
-print_header();
-echo print_error ( $error);
-echo print_trailer(); 
-?>
+print_header ();
+echo print_error ( $error ) . print_trailer ();
 
+?>
