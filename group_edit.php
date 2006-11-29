2907103ba9baa4b460a890af4690253bee8080f0
bbannon <bbannon>
2006-11-29 18:46:48 +0000

    Cut dupe code. Lines < 80. Utilize $translations[].

--- group_edit.php
+++ group_edit.php
@@ -1,23 +1,17 @@
 <?php
 /* $Id$ */
 include_once 'includes/init.php';
-print_header( '', '', '', true );
-?>
-<form action="group_edit_handler.php" method="post">
-<?php
+
+$groupname = $groupowner = $groupupdated = '';
 $newgroup = true;
-$groupname = '';
-$groupowner = '';
-$groupupdated = '';
 
-
-if ( empty ( $id ) ) {
-  $groupname = translate( 'Unnamed Group' );
-} else {
+if ( empty ( $id ) )
+  $groupname = translate ( 'Unnamed Group' );
+else {
   $newgroup = false;
-  // get group by id
-  $res = dbi_execute ( 'SELECT cal_owner, cal_name, cal_last_update, cal_owner ' .
-    'FROM webcal_group WHERE cal_group_id = ?', array( $id ) );
+  // Get group by id.
+  $res = dbi_execute ( 'SELECT cal_owner, cal_name, cal_last_update, cal_owner
+    FROM webcal_group WHERE cal_group_id = ?', array ( $id ) );
   if ( $res ) {
     if ( $row = dbi_fetch_row ( $res ) ) {
       $groupname = $row[1];
@@ -29,73 +23,93 @@ if ( empty ( $id ) ) {
   }
 }
 
+print_header ( '', '', '', true );
+
+ob_start ();
+
+echo '
+    <form action="group_edit_handler.php" method="post">
+      <h2>';
 
 if ( $newgroup ) {
   $v = array ();
-  echo '<h2>' . translate( 'Add Group' ) . "</h2>\n";
-  echo '<input type="hidden" name="add" value="1" />' . "\n";
-} else {
-  echo '<h2>' . translate( 'Edit Group' ) . "</h2>\n";
-  echo "<input type=\"hidden\" name=\"id\" value=\"$id\" />";
+  echo translate ( 'Add Group' ) . '</h2>
+      <input type="hidden" name="add" value="1';
+} else
+  echo translate ( 'Edit Group' ) . '</h2>
+      <input type="hidden" name="id" value="' . $id;
+
+echo '" />
+      <table>
+        <tr>
+          <td class="bold"><label for="groupname">'
+ . translate ( 'Group name' ) . ':</label></td>
+          <td><input type="text" name="groupname" id="groupname" size="20" '
+ . 'value="' . htmlspecialchars ( $groupname ) . '" /></td>
+        </tr>' . ( ! $newgroup ? '
+        <tr>
+          <td class="aligntop bold">' . translate ( 'Updated' ) . ':</td>
+          <td>' . date_to_str ( $groupupdated ) . '</td>
+        </tr>
+        <tr>
+          <td class="aligntop bold">' . translate ( 'Created by' ) . ':</td>
+          <td>' . $groupowner . '</td>
+        </tr>' : '' ) . '
+        <tr>
+          <td class="aligntop bold"><label for="users">'
+ . $translations['Users'] . ':</label></td>
+          <td>
+            <select name="users[]" id="users" size="10" multiple="multiple">';
+// .
+// Get list of all users.
+$users = user_get_users ();
+if ( $NONUSER_ENABLED == 'Y' ) {
+  $nonusers = get_nonuser_cals ();
+  $users = ( $NONUSER_AT_TOP == 'Y' )
+  ? array_merge ( $nonusers, $users ) : array_merge ( $users, $nonusers );
 }
-?>
-
-<table>
-<tr><td class="bold">
- <label for="groupname"><?php etranslate( 'Group name' )?>:</label></td><td>
- <input type="text" name="groupname" id="groupname" size="20" value="<?php echo htmlspecialchars ( $groupname );?>" />
-</td></tr>
-<?php if ( ! $newgroup ) { ?>
- <tr><td class="aligntop bold">
-  <?php etranslate( 'Updated' ); ?>:</td><td>
-  <?php echo date_to_str ( $groupupdated ); ?>
- </td></tr>
- <tr><td class="aligntop bold">
-  <?php etranslate( 'Created by' ); ?>:</td><td>
-  <?php echo $groupowner; ?>
- </td></tr>
-<?php } ?>
-<tr><td class="aligntop bold">
- <label for="users"><?php etranslate( 'Users' ); ?>:</label></td><td>
- <select name="users[]" id="users" size="10" multiple="multiple">
-<?php
-  // get list of all users
-  $users = user_get_users ();
-  if ($NONUSER_ENABLED == 'Y' ) {
-    $nonusers = get_nonuser_cals ();
-    $users = ($NONUSER_AT_TOP == 'Y') ? array_merge($nonusers, $users) : array_merge($users, $nonusers);
-  }
-
-  // get list of users for this group
-  if ( ! $newgroup ) {
-    $sql = 'SELECT cal_login FROM webcal_group_user WHERE cal_group_id = ?';
-    $res = dbi_execute ( $sql, array( $id ) );
-    if ( $res ) {
-      while ( $row = dbi_fetch_row ( $res ) ) {
-        $groupuser[$row[0]] = 1;
-      }
-      dbi_free_result ( $res );
+// .
+// Get list of users for this group.
+if ( ! $newgroup ) {
+  $res = dbi_execute ( 'SELECT cal_login FROM webcal_group_user
+    WHERE cal_group_id = ?', array ( $id ) );
+  if ( $res ) {
+    while ( $row = dbi_fetch_row ( $res ) ) {
+      $groupuser[$row[0]] = 1;
     }
+    dbi_free_result ( $res );
   }
-  for ( $i = 0, $cnt = count ( $users ); $i < $cnt; $i++ ) {
-    $u = $users[$i]['cal_login'];
-    echo "<option value=\"$u\" ";
-    if ( ! empty ( $groupuser[$u] ) ) {
-      echo ' selected="selected"';
-    }
-    echo '>' . $users[$i]['cal_fullname'] . "</option>\n";
-  }
-?>
- </select>
-</td></tr>
-<tr><td colspan="2" class="aligncenter">
- <br /><input type="submit" name="action" value="<?php 
-  if ( $newgroup ) etranslate( 'Add' ); else etranslate( 'Save' ); ?>" />
- <?php if ( ! $newgroup ) { ?>
-  <input type="submit" name="delete" value="<?php etranslate( 'Delete' )?>" onclick="return confirm('<?php str_replace ( 'XXX', $translations['entry'], $translations['Are you sure you want to delete this XXX?'] ); ?>')" />
- <?php } ?>
-</td></tr>
-</table>
-</form>
-<?php echo print_trailer ( false, true, true ); ?>
+}
+for ( $i = 0, $cnt = count ( $users ); $i < $cnt; $i++ ) {
+  $u = $users[$i]['cal_login'];
+  echo '
+              <option value="' . $u . '" '
+   . ( ! empty ( $groupuser[$u] ) ? ' selected="selected"' : '' )
+   . '>' . $users[$i]['cal_fullname'] . '</option>';
+}
 
+echo '
+            </select>
+          </td>
+        </tr>
+        <tr>
+          <td colspan="2" class="aligncenter"><br />
+            <input type="submit" name="action" value="'
+ . ( $newgroup ? $translations['Add'] : $translations['Save'] ) . '" />'
+ . ( ! $newgroup ? '
+            <input type="submit" name="delete" value="'
+   . $translations['Delete'] . '" onclick="return confirm (\''
+   . str_replace ( 'XXX', $translations['entry'],
+    $translations['Are you sure you want to delete this XXX?'] )
+   . '\')" />' : '' ) . '
+          </td>
+        </tr>
+      </table>
+    </form>
+    ';
+
+ob_end_flush ();
+
+echo print_trailer ( false, true, true );
+
+?>
