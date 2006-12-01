23c00781fddbee165ac2f4d07077aa9b1e7f590b
bbannon <bbannon>
2006-12-01 17:29:27 +0000

    Cut dupe code. Lines < 95. Utilize $translations[].

--- import_handler.php
+++ import_handler.php
@@ -1,9 +1,8 @@
 <?php
-/*
- * $Id$  
+/* $Id$
  *
  * Description:
- * Loads appropriate import file parser and processes the data returned
+ * Loads appropriate import file parser and processes the data returned.
  *    Currently supported:
  *      Palmdesktop (dba file)
  *      iCal (ics file)
@@ -11,108 +10,108 @@
  *
  *
  * Notes:
- * User defined inport routines may be used, see example
- *    in the SWITCH statement below
+ * User defined import routines may be used, see example
+ * in the SWITCH statement below.
  *
  * Input parameters:
- * FileName: File name specified by user on import.php
- *      user's calendar to import data into, unless
- *      single user = Y or Admin, caluser will equal logged
- *      in user.
- *    exc_private: exclude private records from Palmdesktop import
- *    overwrite: Overwrite previous import 
+ *   FileName:    File name specified by user on import.php user's calendar to
+ *                import data into, unless single user = Y or Admin,
+ *                caluser will equal logged in user.
+ *   exc_private: Exclude private records from Palmdesktop import.
+ *   overwrite:   Overwrite previous import.
  *
  * Security:
  * TBD
  */
 include_once 'includes/init.php';
 include_once 'includes/xcal.php';
-$error = '';
-print_header();
+$error = $sqlLog = '';
+print_header ();
 
-$overwrite = getValue( 'overwrite' );
+$overwrite = getValue ( 'overwrite' );
 $doOverwrite = ( empty ( $overwrite ) || $overwrite != 'Y' ) ? false : true;
 $numDeleted = 0;
 
-$sqlLog = '';
-
-if ( ! empty ( $_FILES['FileName'] ) ) {
+if ( ! empty ( $_FILES['FileName'] ) )
   $file = $_FILES['FileName'];
-} else if ( ! empty ( $HTTP_POST_FILES['FileName'] ) ) {
+else
+if ( ! empty ( $HTTP_POST_FILES['FileName'] ) )
   $file = $HTTP_POST_FILES['FileName'];
-}
 
-if ( empty ( $file ) ) {
-  echo 'No file!<br />';
-}
+if ( empty ( $file ) )
+  echo $translations['No file'] . '!<br />';
 
 // Handle user
 $calUser = getValue ( 'calUser' );
 if ( ! empty ( $calUser ) ) {
-  if ( $single_user == 'N' && ! $is_admin ) $calUser = $login;
-} else {
+  if ( $single_user == 'N' && ! $is_admin )
+    $calUser = $login;
+} else
   $calUser = $login;
-}
 
-if ($file['size'] > 0) {
-  switch ($ImportType) {
+if ( $file['size'] > 0 ) {
+  switch ( $ImportType ) {
 
-// ADD New modules here:
-
-//    case 'MODULE':
-//      include "import_module.php";
-//      $data = parse_module($HTTP_POST_FILES['FileName']['tmp_name']);
-//      break;
-//
+    // ADD New modules here:
+/*
+    case 'MODULE':
+      include "import_module.php";
+      $data = parse_module ( $HTTP_POST_FILES['FileName']['tmp_name'] );
+      break;
+*/
 
     case 'PALMDESKTOP':
       include 'import_palmdesktop.php';
-      if (delete_palm_events($login) != 1) $errormsg = 'Error deleting palm events from webcalendar.';
-      $data = parse_palmdesktop($file['tmp_name'], $exc_private);
+      if ( delete_palm_events ( $login ) != 1 )
+        $errormsg = translate ( 'Error deleting palm events from webcalendar.' );
+      $data = parse_palmdesktop ( $file['tmp_name'], $exc_private );
       $type = 'palm';
       break;
 
     case 'VCAL':
-      $data = parse_vcal($file['tmp_name']);
+      $data = parse_vcal ( $file['tmp_name'] );
       $type = 'vcal';
       break;
 
     case 'ICAL':
-      $data = parse_ical($file['tmp_name']);
+      $data = parse_ical ( $file['tmp_name'] );
       $type = 'ical';
       break;
 
     case 'OUTLOOKCSV':
       include 'import_outlookcsv.php';
-      $data = parse_outlookcsv($file['tmp_name']);
+      $data = parse_outlookcsv ( $file['tmp_name'] );
       $type = 'outlookcsv';
       break;
-
   }
-  $errorStr = translate( 'Error' );
   $count_con = $count_suc = $error_num = 0;
-  if (! empty ($data) && empty ($errormsg) ) {
+  if ( ! empty ( $data ) && empty ( $errormsg ) ) {
     import_data ( $data, $doOverwrite, $type );
-    echo '<p>' . translate( 'Import Results' ) . "</p>\n<br /><br />\n" .
-      translate( 'Events successfully imported' ) . ": $count_suc<br />\n";
-    echo translate( 'Events from prior import marked as deleted' ) . ": $numDeleted<br />\n";
-    if ( empty ( $ALLOW_CONFLICTS ) ) {
-      echo translate( 'Conflicting events' ) . ': ' . $count_con . "<br />\n";
-    }
-    echo translate ( 'Errors' ) . ": $error_num<br /><br />\n";
-  } elseif (! empty ( $errormsg ) ) {
-    echo "<br /><br />\n<b>" . $errorStr . ":</b> $errormsg<br />\n";
-  } else {
-    echo "<br /><br />\n<b>" . $errorStr . ':</b> ' .
-      translate( 'There was an error parsing the import file or no events were returned' ) .
-      ".<br />\n";
-  }
-} else {
- echo "<br /><br />\n<b>" . $errorStr . ":</b> " .
-    translate( 'The import file contained no data' ) . ".<br />\n";
-}
+    echo '
+    <p>' . $translations['Import Results'] . '</p><br /><br />
+    ' . $translations['Events successfully imported'] . ': ' . $count_suc
+     . '<br />
+    ' . translate ( 'Events from prior import marked as deleted' ) . ': '
+     . $numDeleted . '<br />
+    ' . ( empty ( $ALLOW_CONFLICTS )
+      ? translate ( 'Conflicting events' ) . ': ' . $count_con . '<br />
+    ' : '' ) . $translations['Errors'] . ': ' . $error_num . '<br /><br />';
+  } elseif ( ! empty ( $errormsg ) )
+    echo '
+    <br /><br />
+    <b>' . $translations['Error'] . ':</b> ' . $errormsg . '<br />';
+  else
+    echo '
+    <br /><br />
+    <b>' . $translations['Error'] . ':</b> '
+     . $translations['There was an error parsing the import file or no events were returned']
+     . '.<br />';
+} else
+  echo '
+    <br /><br />
+    <b>' . $translations['Error'] . ':</b> '
+   . translate ( 'The import file contained no data' ) . '.<br />';
+// echo "<hr />$sqlLog\n";
+echo print_trailer ();
 
-
-//echo "<hr />$sqlLog\n";
-
-echo print_trailer (); ?>
+?>
