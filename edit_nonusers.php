521dc985cf493cb8b019c1ed98efe9f78ca43ccc
bbannon <bbannon>
2006-12-11 08:15:56 +0000

    Stay in PHP. Cut dupe code. Lines < 80. Utilize $translations[].

--- edit_nonusers.php
+++ edit_nonusers.php
@@ -1,105 +1,133 @@
 <?php
 /* $Id$ */
 include_once 'includes/init.php';
-$INC = array('js/edit_nonuser.php/false');
-print_header( $INC, '', '', true, '', true, false );
+print_header ( array ( 'js/edit_nonuser.php/false' ),
+  '', '', true, '', true, false );
 
 if ( ! $is_admin ) {
-  echo print_not_auth ( true );
-  echo "</body>\n</html>";
+  echo print_not_auth ( true ) . '
+  </body>
+</html>';
   exit;
 }
 if ( ! $NONUSER_PREFIX ) {
-  echo print_error_header () . 
-    translate( 'NONUSER_PREFIX not set' ) . ".\n";
-  echo "</body>\n</html>";
+  echo print_error_header () . translate ( 'NONUSER_PREFIX not set' ) . '.
+  </body>
+</html>';
   exit;
 }
 $add = getValue ( 'add' );
 $nid = getValue ( 'nid' );
 
-// Adding/Editing nonuser calendar
-if (( ($add == '1') || (! empty ($nid)) ) && empty ($error)) {
+// Adding/Editing nonuser calendar.
+if ( ( ( $add == '1' ) || ( ! empty ( $nid ) ) ) && empty ( $error ) ) {
   $userlist = user_get_users ();
-  $button = translate( 'Add' );
-  $nid = clean_html($nid);
-?>
+  $button = $translations['Add'];
+  $nid = clean_html ( $nid );
 
-<form action="edit_nonusers_handler.php" name="editnonuser" method="post" onsubmit="return valid_form(this);">
-  <?php
   if ( ! empty ( $nid ) ) {
     nonuser_load_variables ( $nid, 'nonusertemp_' );
-    $id_display = "$nid <input type=\"hidden\" name=\"nid\" value=\"$nid\" />";
-    $button = translate( 'Save' );
-    $nonusertemp_login = substr($nonusertemp_login, strlen($NONUSER_PREFIX));
-  } else {
-    $id_display = '<input type="text" name="nid" id="calid" size="20" onchange="check_name();" maxlength="20" /> ' . translate ( 'word characters only' );
-  }
-  if (! empty ( $nonusertemp_admin ) ){
-    echo "<input type=\"hidden\" name=\"old_admin\" value=\"{$nonusertemp_admin}\" />";
-  }
-  ?>
-<h2><?php
-  if ( ! empty ( $nid ) ) {
- nonuser_load_variables ( $nid, 'nonusertemp_' );
- echo translate( 'Edit User' );
-  } else {
- echo translate( 'Add User' );
-  }
-?></h2>
-<table>
- <tr><td>
-  <label for="calid"><?php etranslate( 'Calendar ID' )?>:</label></td><td>
-  <?php echo $id_display ?>
- </td></tr>
- <tr><td>
-  <label for="nfirstname"><?php etranslate( 'First Name' )?>:</label></td><td>
-  <input type="text" name="nfirstname" id="nfirstname" size="20" maxlength="25" value="<?php echo empty ( $nonusertemp_firstname ) ? '' : htmlspecialchars ( $nonusertemp_firstname ); ?>" />
- </td></tr>
- <tr><td>
-  <label for="nlastname"><?php etranslate( 'Last Name' )?>:</label></td><td>
-  <input type="text" name="nlastname" id="nlastname" size="20" maxlength="25" value="<?php echo empty ( $nonusertemp_lastname ) ? '' : htmlspecialchars ( $nonusertemp_lastname ); ?>" />
- </td></tr>
- <tr><td>
-  <label for="nadmin"><?php etranslate( 'Admin' )?>:</label></td><td>
-  <select name="nadmin" id="nadmin">
-<?php
+    $id_display = $nid . '
+      <input type="hidden" name="nid" value="' . $nid . '" />';
+    $button = $translations['Save'];
+    $nonusertemp_login = substr ( $nonusertemp_login,
+      strlen ( $NONUSER_PREFIX ) );
+  } else
+    $id_display = '
+      <input type="text" name="nid" id="calid" size="20" '
+     . 'onchange="check_name ();" maxlength="20" /> '
+     . translate ( 'word characters only' );
+
+  ob_start ();
+
+  echo '
+    <form action="edit_nonusers_handler.php" name="editnonuser" method="post" '
+   . 'onsubmit="return valid_form (this);">'
+   . ( empty ( $nonusertemp_admin ) ? '' : '
+      <input type="hidden" name="old_admin" value="'
+     . $nonusertemp_admin . '" />' ) . '
+      <h2>' . ( empty ( $nid )
+    ? translate ( 'Add User' ) : translate ( 'Edit User' ) ) . '</h2>
+      <table>
+        <tr>
+          <td><label for="calid">' . translate ( 'Calendar ID' )
+   . ':</label></td>
+          <td>' . $id_display . '</td>
+        </tr>
+        <tr>
+          <td><label for="nfirstname">' . translate ( 'First Name' )
+   . ':</label></td>
+          <td><input type="text" name="nfirstname" id="nfirstname" size="20" '
+   . 'maxlength="25" value="'
+   . ( empty ( $nonusertemp_firstname )
+    ? '' : htmlspecialchars ( $nonusertemp_firstname ) ) . '" /></td>
+        </tr>
+        <tr>
+          <td><label for="nlastname">' . translate ( 'Last Name' )
+   . ':</label></td>
+          <td><input type="text" name="nlastname" id="nlastname" size="20" '
+   . 'maxlength="25" value="'
+   . ( empty ( $nonusertemp_lastname )
+    ? '' : htmlspecialchars ( $nonusertemp_lastname ) ) . '" /></td>
+        </tr>
+        <tr>
+          <td><label for="nadmin">' . $translations['Admin'] . ':</label></td>
+          <td>
+            <select name="nadmin" id="nadmin">';
+
   for ( $i = 0, $cnt = count ( $userlist ); $i < $cnt; $i++ ) {
- echo '<option value="' .$userlist[$i]['cal_login']. '"';
- if (! empty ( $nonusertemp_admin ) &&
-            $nonusertemp_admin == $userlist[$i]['cal_login'] ) 
-  echo ' selected="selected"';
- echo '>' . $userlist[$i]['cal_fullname']."</option>\n";
+    echo '
+              <option value="' . $userlist[$i]['cal_login'] . '"'
+     . ( ! empty ( $nonusertemp_admin ) &&
+      $nonusertemp_admin == $userlist[$i]['cal_login']
+      ? ' selected="selected"' : '' ) . '>' . $userlist[$i]['cal_fullname']
+     . '</option>';
   }
+
+  echo '
+            </select>
+          </td>
+        </tr>';
+
+  if ( ! $use_http_auth ) {
+    echo '
+        <tr>
+          <td valign="top"><label for="ispublic">'
+     . translate ( 'Is public calendar' ) . ':</td>
+          <td>
+            <input type="radio" name="ispublic" value="Y" '
+     . ( ! empty ( $nonusertemp_is_public ) && $nonusertemp_is_public == 'Y'
+      ? ' checked="checked"' : '' ) . ' /> ' . $translations['Yes']
+     . '&nbsp;&nbsp;<input type="radio" name="ispublic" value="N" '
+     . ( empty ( $nonusertemp_is_public ) || $nonusertemp_is_public != 'Y'
+      ? ' checked="checked"' : '' ) . ' /> ' . $translations['No'] . '<br />';
+
+    if ( ! empty ( $nonusertemp_login ) ) {
+      $nu_url = $SERVER_URL . 'nulogin.php?login=' . $nonusertemp_login;
+      echo '
+            <a href="' . "$nu_url\">$nu_url" . '</a>';
+    }
+
+    echo '
+          </td>
+        </tr>';
+  }
+
+  echo '
+      </table><br />
+      <input type="submit" name="action" value="' . $button . '" />'
+   . ( empty ( $nid ) ? '' : '
+      <input type="submit" name="delete" value="' . $translations['Delete']
+     . '" onclick="return confirm (\''
+     . str_replace ( 'XXX', $translations['entry'],
+      $translations['Are you sure you want to delete this XXX?'] )
+     . '\')" />' ) . '
+    </form>
+    ';
+}
+
+ob_end_flush ();
+
+echo print_trailer ( false, true, true );
+
 ?>
-  </select>
- </td></tr>
-
-<?php if ( ! $use_http_auth ) { ?>
- <tr><td valign="top"><label for="ispublic"><?php
-   etranslate( 'Is public calendar' );?>:</td>
- <td><input type="radio" name="ispublic" value="Y" <?php
-   if ( ! empty ( $nonusertemp_is_public ) &&
-     $nonusertemp_is_public == 'Y' ) echo ' checked="checked"';
-   echo "> " . translate ( 'Yes' ) . "&nbsp;&nbsp;\n";?>
- <input type="radio" name="ispublic" value="N" <?php
-   if ( empty ( $nonusertemp_is_public ) ||
-     $nonusertemp_is_public != 'Y' ) echo ' checked="checked"';
-   echo '> ' . translate ( 'No' );?><br />
- <?php if ( ! empty ( $nonusertemp_login ) ) {
-                $nu_url = $SERVER_URL . "nulogin.php?login=$nonusertemp_login";
-                echo "<a href=\"$nu_url\">$nu_url</a>\n";
-              }
-        ?>
- </td></tr>
-<?php } ?>
-</table>
-  <br />
-  <input type="submit" name="action" value="<?php echo $button;?>" />
-  <?php if ( ! empty ( $nid ) ) {  ?>
-    <input type="submit" name="delete" value="<?php etranslate( 'Delete' );?>" onclick="return confirm('<?php echo str_replace ( 'XXX', $translations['entry'], $translations['Are you sure you want to delete this XXX?'] ); ?>')" />
-  <?php }  ?>
-  </form>
-<?php }
-echo print_trailer ( false, true, true ); ?>
-
