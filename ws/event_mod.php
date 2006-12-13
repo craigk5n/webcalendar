bdbfeff293ebb104126b66e3fdabeaed2451193b
bbannon <bbannon>
2006-12-13 21:11:30 +0000

    Cut dupe code. Add some translate()s. Utilize $translations[].

--- ws/event_mod.php
+++ ws/event_mod.php
@@ -1,14 +1,13 @@
 <?php
-/*
- * $Id$
+/* $Id$
  *
  * Description:
  *	Web Service functionality to update, delete or add events.
  *
  * Input Parameters:
- *	id - event id
+ *	id       - event id
  *	username - user login of user to add/edit
- *	action - approve, reject, delete
+ *	action   - approve, reject, delete
  *
  * Result:
  *	On success:
@@ -25,80 +24,67 @@
  * Security:
  *	- The current user must have permission to modify the event
  *	  in the way specified.
- *
  */
 
 $WS_DEBUG = false;
 
 $error = '';
 
-require_once "ws.php";
+require_once 'ws.php';
 
 // Initialize...
 ws_init ();
 
-//Header ( "Content-type: text/xml" );
-Header ( "Content-type: text/plain" );
+// header ( 'Content-type: text/xml' );
+header ( 'Content-type: text/plain' );
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 $out = '<result>';
 
-$id = getIntValue ( 'id' );
+$id = getValue ( 'id', '-?[0-9]+' );
 $user = getGetValue ( 'username' );
 if ( empty ( $user ) )
   $user = $login;
 $action = getGetValue ( 'action' );
-if ( $action != 'approve' && $action != 'delete' && $action != 'reject' ) {
-  $error = "Unsupported action: " . ws_escape_xml ( $action );
-}
-if ( empty ( $error ) && empty ( $id ) ) {
-  $error = "No event id specified";
-}
+if ( $action != 'approve' && $action != 'delete' && $action != 'reject' )
+  $error = translate ( 'Unsupported action' ) . ': '
+   . ws_escape_xml ( $action );
+
+if ( empty ( $error ) && empty ( $id ) )
+  $error = translate ( 'No event id specified' );
 
 // If public user, they cannot do this...
-if ( empty ( $error ) && $login == '__public__' ) {
-  $error = translate( 'Not authorized' );
-}
+if ( empty ( $error ) && $login == '__public__' )
+  $error = $translations['Not authorized'];
 
 // Only admin users can modify events on the public calendar
-if ( empty ( $error ) && $PUBLIC_ACCESS == 'Y' && $user == '__public__' &&
-  ! $is_admin ) {
-  $error = translate( 'Not authorized' ) . ' ' . "(not admin)";
-}
+if ( empty ( $error ) && $PUBLIC_ACCESS == 'Y' && $user == '__public__' && !
+    $is_admin )
+  $error = $translations['Not authorized']
+   . ' (' . translate ( 'not admin' ) . ')';
 
 if ( empty ( $error ) && ! $is_admin && $user != $login ) {
   // User has request to modify event on someone else's calendar and
   // the user is not an admin user.
   if ( access_is_enabled () ) {
-    if ( ! access_user_calendar ( 'approve', $user ) ) {
-      $error = translate( 'Not authorized' );
-    }
-  } else {
-    // TODO: support boss/assistant when UAC is not enabled
-    $error = translate( 'Not authorized' );
-  }
+    if ( ! access_user_calendar ( 'approve', $user ) )
+      $error = $translations['Not authorized'];
+  } else
+    // TODO: support boss/assistant when UAC is not enabled.
+    $error = $translations['Not authorized'];
 }
 
-if ( $action == 'approve' ) {
-  update_status ( 'A', $user, $id );
-} else if ( $action == 'reject' ) {
-  update_status ( 'R', $user, $id );
-} else if ( $action == 'delete' ) {
-  update_status ( 'D', $user, $id );
-}
+if ( $action == 'approve' || $action == 'delete' || $action == 'reject' )
+  update_status ( ucase (substr ( $action, 0, 1) ), $user, $id );
 
-if ( empty ( $error ) ) {
-  $out .= '<success/>';
-} else {
-  $out .= '<error>' . ws_escape_xml ( $error ) . '</error>';
-}
-$out .= "</result>\n";
+$out .= ( empty ( $error )
+  ? '<success/>' : '<error>' . ws_escape_xml ( $error ) . '</error>' )
+ . "</result>\n";
 
 // If web servic debugging is on...
-if ( ! empty ( $WS_DEBUG ) && $WS_DEBUG ) {
+if ( ! empty ( $WS_DEBUG ) && $WS_DEBUG )
   ws_log_message ( $out );
-}
 
 // Send output now...
 echo $out;
--- ws/user_mod.php
+++ ws/user_mod.php
@@ -1,20 +1,19 @@
 <?php
-/*
- * $Id$
+/* $Id$
  *
  * Description:
  *	Web Service functionality to update, delete or add a user.
  *
  * Input Parameters:
- *	username - user login of user to add/edit
+ *	username   - user login of user to add/edit
  *	firstname* - user firstname
- *	lastname* - user lastname
- *	password* - user password
- *	admin* - is admin (1 or 0)
- *	email* - email address
- *	add* - 1=adding user
- *	del* - 1=deleting user
- *	(*) optional
+ *	lastname*  - user lastname
+ *	password*  - user password
+ *	admin*     - is admin (1 or 0)
+ *	email*     - email address
+ *	add*       - 1=adding user
+ *	del*       - 1=deleting user
+ *	 (*) optional
  *
  * Result:
  *	On success:
@@ -25,7 +24,7 @@
  * Notes:
  *	If updating a user, the omission of a parameter (email, for example)
  *	will result in the value being set to an empty string
- *	(the old value will be preserved)... except for password, which
+ * (the old value will be preserved)... except for password, which
  *	cannot be blank.
  *
  * Developer Notes:
@@ -38,150 +37,124 @@
  *	  $admin_can_add_user global variable set to add a user.
  *	- User include file (user.php, user-ldap.php, etc.) must have the
  *	  $admin_can_delete_user global variable set to delete a user.
- *
  */
 
 $WS_DEBUG = false;
 
 // Security precaution.  In case, register_globals is on, unset anything
 // a malicious user may set in the URL.
-$admin_can_add_user = false;
-$admin_can_delete_user = false;
+$admin_can_add_user = $admin_can_delete_user = false;
 $error = '';
 
-require_once "ws.php";
+require_once 'ws.php';
 
 // Initialize...
 ws_init ();
 
-//Header ( "Content-type: text/xml" );
-Header ( "Content-type: text/plain" );
+// header ( "Content-type: text/xml" );
+header ( 'Content-type: text/plain' );
 
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
 $out = '<result>';
 
 // If not an admin user, they cannot do this...
-if ( ! $is_admin ) {
-  $error = translate("Not authorized") . ' ' . "(not admin)";
-}
+if ( ! $is_admin )
+  $error = $translations['Not authorized'] . ' ('
+   . $transltions['not admin'] . ')';
 
 // Some installs do not allow
-if ( empty ( $error ) && ! $admin_can_add_user ) {
-  $error = translate("Not authorized");
-}
+if ( empty ( $error ) && ! $admin_can_add_user )
+  $error = $translations['Not authorized'];
 
-$user_login = getGetValue ( 'username' );
-$user_firstname = getGetValue ( 'firstname' );
-$user_lastname = getGetValue ( 'lastname' );
-$user_password = getGetValue ( 'password' );
-$user_admin = getGetValue ( 'admin' );
-$user_email = getGetValue ( 'email' );
 $addIn = getGetValue ( 'add' );
 $add = ( ! empty ( $addIn ) && $addIn == '1' );
+
 $deleteIn = getGetValue ( 'delete' );
 if ( empty ( $deleteIn ) )
   $deleteIn = getGetValue ( 'del' );
 $delete = ( ! empty ( $deleteIn ) && $deleteIn == '1' );
 
+$user_admin = getGetValue ( 'admin' );
+$user_email = getGetValue ( 'email' );
+$user_firstname = getGetValue ( 'firstname' );
+$user_lastname = getGetValue ( 'lastname' );
+$user_login = getGetValue ( 'username' );
+$user_password = getGetValue ( 'password' );
+
 // This error should not happen in a properly written client, so no need to
 // translate it.
-if ( empty ( $error ) && empty ( $user_login ) ) {
-  $error = translate ( 'Username can not be blank' );
-}
+if ( empty ( $error ) && empty ( $user_login ) )
+  $error = 'Username can not be blank';
 
 // Check for invalid characters in the login
-if ( empty ( $error ) && addslashes ( $user_login ) != $user_login ) {
-  $error = 'Invalid characters in login.';
-}
+if ( empty ( $error ) && addslashes ( $user_login ) != $user_login )
+  $error = translate ( 'Invalid characters in login' ) . '.';
 
 // Check to see if username exists
 if ( empty ( $error ) ) {
   if ( user_load_variables ( $user_login, 'old_' ) ) {
     // username does already exist
-    if ( $add ) {
-      $error = "User " . ws_escape_xml ( $user_login ) .
-        " already exists";
-    }
+    if ( $add )
+      $error = $translations['User'] . ' ' . ws_escape_xml ( $user_login )
+       . ' ' . translate ( 'already exists' );
   } else {
     // username does not already exist
-    if ( ! $add ) {
-      $error = "User " . ws_escape_xml ( $user_login ) .
-        " does not exist";
-    } else if ( $delete ) {
-      $error = "User " . ws_escape_xml ( $user_login ) .
-        " does not exist";
-    }
+    if ( ! $add || $delete )
+      $error = $translations['User'] . ' ' . ws_escape_xml ( $user_login ) . ' '
+       . $translations['does not exist'];
   }
 }
 
 // If adding a user, make sure a password was provided
-if ( empty ( $error ) && $add && empty ( $user_password ) ) {
-  $error = translate( 'You have not entered a password' );
-}
+if ( empty ( $error ) && $add && empty ( $user_password ) )
+  $error = $translations['You have not entered a password'];
 
-if ( empty ( $error ) && ! $add && ! $delete ) {
-  if ( empty ( $user_password ) )
-    $user_password = $old_password;
-}
+if ( empty ( $error ) && ! $add && ! $delete && empty ( $user_password ) )
+  $user_password = $old_password;
 
-// admin must be 'Y' or 'N' for call to user_add_user()
-if ( empty ( $user_admin ) || $user_admin != '1' )
-  $user_admin = 'N';
-else
-  $user_admin = 'Y';
+// admin must be 'Y' or 'N' for call to user_add_user ()
+$user_admin = ( empty ( $user_admin ) || $user_admin != '1' ? 'N' : 'Y' );
 
-// If user is editing themself, do not let them take away admin
-// setting.  We don't want them to accidentally have no admin
-// users left.
-if ( empty ( $error ) && $user_login == $login && $user_admin == 'N' ) {
-  $error = 'You cannot remove admin rights from yourself';
-}
+// If user is editing themself, do not let them take away admin setting.
+// We don't want them to accidentally have no admin users left.
+if ( empty ( $error ) && $user_login == $login && $user_admin == 'N' )
+  $error = translate ( 'You cannot remove admin rights from yourself!' );
 
-
-if ( empty ( $error ) && $delete ) {
+if ( empty ( $error ) && $delete )
   user_delete_user ( $user_login );
-  // we don't check return status... hope it worked
-} else if ( empty ( $error ) && $add ) {
+// we don't check return status... hope it worked
+else
+if ( empty ( $error ) && $add ) {
   if ( user_add_user ( $user_login, $user_password, $user_firstname,
-    $user_lastname, $user_email, $user_admin ) ) {
+      $user_lastname, $user_email, $user_admin ) ) {
     // success    :-)
-  } else {
+  } else
     // error
-    if ( empty ( $error ) ) {
-      $error = 'Unknown error saving user';
-    } else {
-      // In case there are any strange chars in a db error message
-      $error = ws_escape_xml ( $error );
-    }
-  }
-} else if ( empty ( $error ) ) {
-  // Update
-  if ( user_update_user ( $user_login, $user_firstname,
-    $user_lastname, $user_email, $user_admin ) ) {
-    // success    :-)
-  } else {
-    // error
-    if ( empty ( $error ) ) {
-      $error = 'Unknown error saving user';
-    } else {
-      // In case there are any strange chars in a db error message
-      $error = ws_escape_xml ( $error );
-    }
-  }
-}
-
+    $error = ( empty ( $error )
+      ? translate ( 'Unknown error saving user' )
+      :// In case there are any strange chars in a db error message.
+      ws_escape_xml ( $error ) );
+} else
 if ( empty ( $error ) ) {
-  $out .= '<success/>';
-} else {
-  $out .= '<error>' . $error . '</error>';
+  // update
+  if ( user_update_user ( $user_login, $user_firstname,
+      $user_lastname, $user_email, $user_admin ) ) {
+    // success    :-)
+  } else
+    // error
+    $error = ( empty ( $error )
+      ? $translations['Unknown error saving user']
+      :// In case there are any strange chars in a db error message.
+      ws_escape_xml ( $error ) );
 }
-$out .= "</result>\n";
 
-// If web servic debugging is on...
-if ( ! empty ( $WS_DEBUG ) && $WS_DEBUG ) {
+( empty ( $error ) ? '<success/>' : '<error>' . $error . '</error>' )
+ . "</result>\n";
+
+// If web service debugging is on...
+if ( ! empty ( $WS_DEBUG ) && $WS_DEBUG )
   ws_log_message ( $out );
-}
 
 // Send output now...
 echo $out;
