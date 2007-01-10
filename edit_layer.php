3720ac05921f81c546cd53bda47e966186e6e5e0
umcesrjones <umcesrjones>
2007-01-10 22:03:08 +0000

    Fixed bug [ 1631478 ] Edit/Add Layer has the same 'Headline' and no <br>

--- edit_layer.php
+++ edit_layer.php
@@ -21,23 +21,25 @@ load_user_layers ( $layer_user, 1 );
 $color = ( ! empty ( $layers[$id]['cal_color'] )
   ? $layers[$id]['cal_color'] : '#000' );
 
+$checked = 'checked="checked"';
+
 print_header ( array ( 'js/edit_layer.php', 'js/visible.php' ) );
 
 ob_start ();
 
-echo '
-    <h2>'
- . ( $updating_public ? translate ( $PUBLIC_ACCESS_FULLNAME ) . ' ' : '' )
- . empty ( $layers[$id]['cal_layeruser'] )
-  ? translate ( 'Add Layer' ) : translate ( 'Edit Layer' )
- . ' &nbsp;<img src="images/help.gif" alt="' . translate ( 'Help' )
- . '" class="help" onclick="window.open ( \'help_layers.php\', \'cal_help\', '
- . '\'dependent,menubar,scrollbars,height=400,width=400,innerHeight=420,'
- . 'outerWidth=420\' );" /></h2>
-    <form action="edit_layer_handler.php" method="post" onsubmit="return '
- . 'valid_form (this);" name="prefform">' . ( $updating_public ? '
-      <input type="hidden" name="public" value="1" />' : '' ) . '
-      <table cellspacing="2" cellpadding="3">';
+$publicStr = ( $updating_public ? translate ( $PUBLIC_ACCESS_FULLNAME ) . ' ' : '' );
+$titleStr = ( empty ( $layers[$id]['cal_layeruser']  )
+  ? translate ( 'Add Layer' ) : translate ( 'Edit Layer' ) );
+$helpStr = translate ( 'Help' );
+$hiddenStr = ( $updating_public ? '
+      <input type="hidden" name="public" value="1" />' : '' );
+echo  <<<EOT
+    <h2>{$publicStr}{$titleStr}&nbsp;
+      <img src="images/help.gif" alt="{$helpStr}" class="help" 
+onclick="window.open ( 'help_layers.php','cal_help','dependent,menubar,scrollbars,height=400,width=400,innerHeight=420,outerWidth=420' );" /></h2>
+    <form action="edit_layer_handler.php" method="post" onsubmit="return valid_form (this);" name="prefform">{$hiddenStr}
+    <table cellspacing="2" cellpadding="3">
+EOT;
 
 if ( $single_user == 'N' ) {
   $otherlist = $userlist = get_my_users ( '', 'view' );
@@ -99,23 +101,30 @@ if ( $single_user == 'N' ) {
             </select>
           </td>
         </tr>
+
 EOT;
   }
 }
+$colorHtmlStr = print_color_input_html ( 'layercolor', translate ( 'Color' ),
+  $color );
+$duplicatesStr = translate ( 'Duplicates' );
+$dupsChecked = ( ! empty ( $layers[$id]['cal_dups'] ) && $layers[$id]['cal_dups'] == 'Y'
+  ? $checked : '' );
+$showStr = translate ( 'Show layer events that are the same as your own' );
 
-echo '
+echo <<<EOT
         <tr>
-          <td>' . print_color_input_html ( 'layercolor', translate ( 'Color' ),
-  $color ) . '</td>
+          <td>{$colorHtmlStr}</td>
         </tr>
         <tr>
-          <td class="bold">' . translate ( 'Duplicates' ) . ':</td>
-          <td colspan="3"><label><input type="checkbox" name="dups" value="Y"'
- . ( ! empty ( $layers[$id]['cal_dups'] ) && $layers[$id]['cal_dups'] == 'Y'
-  ? ' checked="checked"' : '' )
- . ' />&nbsp;' . translate ( 'Show layer events that are the same as your own' )
- . '?</label></td>
-        </tr>';
+          <td class="bold">{$duplicatesStr}:</td>
+          <td colspan="3"><label>
+           <input type="checkbox" name="dups" value="Y" {$dupsChecked}/>&nbsp;
+           {$showStr}?</label>
+         </td>
+        </tr>
+
+EOT;
 // If admin and adding a new layer, add ability to select other users
 if ( $is_admin && empty ( $layers[$id]['cal_layeruser'] ) &&
     empty ( $public ) ) {
@@ -125,7 +134,7 @@ if ( $is_admin && empty ( $layers[$id]['cal_layeruser'] ) &&
         <tr>
           <td class="bold">{$addmyStr}:</td>
           <td colspan="3">
-            <input type="checkbox" name="is_mine" checked="checked"
+            <input type="checkbox" name="is_mine" {$checked}
               onclick="show_others ();" /></td>
         </tr>
         <tr id="others" style="visibility: hidden;">
@@ -136,26 +145,28 @@ if ( $is_admin && empty ( $layers[$id]['cal_layeruser'] ) &&
             </select>
           </td>
         </tr>
+
 EOT;
 }
-
-echo '
-        <tr>
-          <td colspan="4">
-            <input type="submit" value="' . translate ( 'Save' ) . '" />'
+$saveStr = translate ( 'Save' );
 // If a layer already exists put a 'Delete Layer' link.
- . ( ! empty ( $layers[$id]['cal_layeruser'] ) ? '&nbsp;&nbsp;&nbsp;
+$deleteStr = ( ! empty ( $layers[$id]['cal_layeruser'] ) ? '&nbsp;&nbsp;&nbsp;
             <input type="button" value="' . translate ( 'Delete layer' )
    . '" onclick="return deleteLayer ( \'del_layer.php?id=' . $id
-   . ( $updating_public ? '&amp;public=1' : '' ) . '\')" />' : '' )
-// End 'Delete Layer' link.
- . '
+   . ( $updating_public ? '&amp;public=1' : '' ) . '\')" />' : '' );
+$hiddenStr = ( ! empty ( $layers[$id]['cal_layeruser'] ) ? '
+      <input type="hidden" name="id" value="' . $id . '" />' : '' );
+
+echo  <<<EOT
+        <tr>
+          <td colspan="4">
+            <input type="submit" value="{$saveStr}" />{$deleteStr}
           </td>
         </tr>
-      </table>' . ( ! empty ( $layers[$id]['cal_layeruser'] ) ? '
-      <input type="hidden" name="id" value="' . $id . '" />' : '' ) . '
+      </table>
+      {$hiddenStr}
     </form>
-    ';
+EOT;
 
 ob_end_flush ();
 
