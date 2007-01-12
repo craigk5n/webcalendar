3b42b4fe40a257ae44da22581abd3ad5c616c283
umcesrjones <umcesrjones>
2007-01-12 16:40:48 +0000

    Use $_SESSION variables for css_cacher and js_cacher to improve performance. Moved  config logic from WebCalendar.class back into config.sys to allow it to be called  as a function.

--- js_cacher.php
+++ js_cacher.php
@@ -2,6 +2,7 @@
 /* $Id$ */
 // If the javascript doesn't need any input from php,
 // then we can cache it and not run init.php.
+define ( '_ISVALID', true );
 $caching = false;
 if ( empty ( $inc ) )
   $inc = $_GET['inc'];
@@ -10,38 +11,44 @@ if ( empty ( $inc ) && ! empty ( $_REQUEST['inc'] ) )
 
 $arinc = explode ( '/', $inc );
 
-if ( $arinc[0] != 'js' )
+if ( $arinc[0] != 'js' && $arinc[0] != 'htmlarea' )
   return false;
+
+// get list of files in the js directory
+$myDirectory = opendir( 'includes/' . $arinc[0] );
+while($fileName = readdir($myDirectory)) {
+	$fileList[] = $fileName;
+}
+closedir($myDirectory);
 
 if ( ! empty ( $arinc[2] ) && stristr ( $arinc[2], 'true' ) )
   $caching = true;
 
 header ( 'Content-type: text/javascript' );
 if ( $caching == true ) {
-  header ( 'Last-Modified: ' . date ( 'r' ) );
+  $cookie = ( isset ( $_COOKIE['webcalendar_csscache'] ) ?
+    $_COOKIE['webcalendar_csscache'] : 0 );
+
+  header('Last-Modified: '. date('r', mktime ( 0,0,0 ) + $cookie ) );
   header ( 'Expires: ' . date ( 'D, j M Y H:i:s', time () + 86400 ) . ' UTC' );
   header ( 'Cache-Control: Public' );
   header ( 'Pragma: Public' );
 } else {
-  require_once 'includes/classes/WebCalendar.class';
-
-  $WebCalendar =& new WebCalendar ( __FILE__ );
-
   include 'includes/translate.php';
   include 'includes/config.php';
   include 'includes/dbi4php.php';
   include 'includes/functions.php';
 
-  $WebCalendar->initializeFirstPhase ();
-
+  do_config ( 'includes/settings.php' );
   include 'includes/' . $user_inc;
   include_once 'includes/access.php';
   include_once 'includes/validate.php';
   include_once 'includes/gradient.php';
 
-  $WebCalendar->initializeSecondPhase ();
-
   load_global_settings ();
+	@session_start ();
+  $login = ( ! empty ( $_SESSION['webcal_login'] ) ? $_SESSION['webcal_login'] : '__public__' );
+
   load_user_preferences ();
 
   send_no_cache_header ();
@@ -52,7 +59,9 @@ if ( ini_get ( 'zlib.output_compression' ) != 1 && !
  stristr ( $_SERVER['HTTP_USER_AGENT'], 'MSIE 6' ) )
   ob_start ( 'ob_gzhandler' );
 
-$newinc = $arinc[0] . '/' . $arinc[1];
-include_once ( 'includes/' . $newinc );
+//we only allow includes if they exist in our includes/js directory, or htmlarea
+$newinc = 'includes/' . $arinc[0] . '/' . $arinc[1];
+if ( is_file ( $newinc ) && array_search ( $arinc[1], $fileList ) )
+  include_once ( $newinc );
 
 ?>
