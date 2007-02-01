a2ad669daa35b6ae8280a328964b48139206c7f7
bbannon <bbannon>
2007-02-01 02:23:11 +0000

    Formatted for consistancy.

--- themes/default_pref.php
+++ themes/default_pref.php
@@ -1,43 +1,42 @@
 <?php
-/**
- * Custom theme for use with WebCalendar
+/* Custom theme for use with WebCalendar.
  *
- * Default System Settings
+ * Default System Settings.
  *
  * @author Ray Jones <rjones@umces.edu>
  * @copyright Craig Knudsen, <cknudsen@cknudsen.com>, http://www.k5n.us/cknudsen
  * @license http://www.gnu.org/licenses/gpl.html GNU GPL
- * @version $Id: 
+ * @version $Id$:
  * @package WebCalendar
  */
 
 // Define your stuff here...
-// Any option in webcal_user_pref can be configured here
-//
+// Any option in webcal_user_pref can be configured here,
 
-//This theme will be available to both normal users and System Settings.
+// This theme will be available to both normal users and System Settings.
 $webcal_theme = array (
-'BGCOLOR'              =>'#FFFFFF',
-'CELLBG'               =>'#C0C0C0',
-'DISPLAY_SM_MONTH'     =>'Y',
-'DISPLAY_TASKS'        =>'N',
-'DISPLAY_TASKS_IN_GRID'=>'N',
-'DISPLAY_WEEKENDS'     =>'Y',
-'DISPLAY_WEEKNUMBER'   =>'Y',
-'FONTS'                =>'Arial, Helvetica, sans-serif',
-'HASEVENTSBG'          =>'#FFFF33',
-'H2COLOR'              =>'#000000',
-'MENU_THEME'           =>'default',
-'OTHERMONTHBG'         =>'#D0D0D0',
-'POPUP_BG'             =>'#FFFFFF',
-'POPUP_FG'             =>'#000000',
-'TABLEBG'              =>'#000000',
-'TEXTCOLOR'            =>'#000000',
-'THBG'                 =>'#FFFFFF',
-'THFG'                 =>'#000000',
-'TODAYCELLBG'          =>'#FFFF33',
-'WEEKENDBG'            =>'#D0D0D0'
- );
- 
+  'MENU_THEME'            => 'default',
+  'BGCOLOR'               => '#FFFFFF',
+  'CELLBG'                => '#C0C0C0',
+  'DISPLAY_SM_MONTH'      => 'Y',
+  'DISPLAY_TASKS'         => 'N',
+  'DISPLAY_TASKS_IN_GRID' => 'N',
+  'DISPLAY_WEEKENDS'      => 'Y',
+  'DISPLAY_WEEKNUMBER'    => 'Y',
+  'FONTS'                 => 'Arial, Helvetica, sans-serif',
+  'H2COLOR'               => '#000000',
+  'HASEVENTSBG'           => '#FFFF33',
+  'OTHERMONTHBG'          => '#D0D0D0',
+  'POPUP_BG'              => '#FFFFFF',
+  'POPUP_FG'              => '#000000',
+  'TABLEBG'               => '#000000',
+  'TEXTCOLOR'             => '#000000',
+  'THBG'                  => '#FFFFFF',
+  'THFG'                  => '#000000',
+  'TODAYCELLBG'           => '#FFFF33',
+  'WEEKENDBG'             => '#D0D0D0',
+  );
+
 include 'theme_inc.php';
+
 ?>
