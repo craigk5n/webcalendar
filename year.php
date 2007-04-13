fb08bfa37772f757a774385c6923f43ba5c696d4
bbannon <bbannon>
2007-04-13 12:46:22 +0000

    Some code cleanup. Make number of rows / columns more easily configurable.

--- year.php
+++ year.php
@@ -3,8 +3,8 @@
 include_once 'includes/init.php';
 send_no_cache_header ();
 
-if (($user != $login) && $is_nonuser_admin)
-  load_user_layers ($user);
+if ( ( $user != $login ) && $is_nonuser_admin )
+  load_user_layers ( $user );
 else
   load_user_layers ();
 
@@ -16,17 +16,15 @@ if ( empty ( $year ) )
 $thisyear = $year;
 if ( $year != date ( 'Y' ) )
   $thismonth = 1;
-//set up global $today value for highlighting current date
-set_today($date);
-if ( $year > '1903' )
-  $prevYear = $year - 1;
-else
-  $prevYear=$year;
 
-$nextYear= $year + 1;
+// Set up global $today value for highlighting current date.
+set_today ( $date );
 
-$startdate = mktime ( 0, 0, 0, 1, 1, $year);
-$enddate = mktime ( 23, 59, 59, 12, 31, $year);
+$nextYear = $year++;
+$prevYear = $year - ( $year > '1903' ? 1 : 0 );
+
+$startdate = mktime ( 0, 0, 0, 1, 1, $year );
+$enddate = mktime ( 23, 59, 59, 12, 31, $year );
 
 if ( $ALLOW_VIEW_OTHER != 'Y' && ! $is_admin )
   $user = '';
@@ -36,99 +34,85 @@ $catSelectStr = '';
 if ( ! empty ( $BOLD_DAYS_IN_YEAR ) && $BOLD_DAYS_IN_YEAR == 'Y' ) {
   /* Pre-Load the repeated events for quckier access */
   $repeated_events = read_repeated_events (
-    ( ! empty ( $user ) && strlen ( $user ) )
-     ? $user : $login, $startdate, $enddate, $cat_id );
+    ( ! empty ( $user ) && strlen ( $user ) ? $user : $login ),
+    $startdate, $enddate, $cat_id );
 
   /* Pre-load the non-repeating events for quicker access */
-  $events = read_events ( ( ! empty ( $user ) && strlen ( $user ) )
-    ? $user : $login, $startdate, $enddate, $cat_id );
+  $events = read_events (
+    ( ! empty ( $user ) && strlen ( $user ) ? $user : $login ),
+    $startdate, $enddate, $cat_id );
   $boldDays = true;
-  $catSelectStr = print_category_menu( 'year', $thisyear, $cat_id );
+  $catSelectStr = print_category_menu ( 'year', $thisyear, $cat_id );
 }
 
-//Disable $DISPLAY_ALL_DAYS_IN_MONTH
+// Disable $DISPLAY_ALL_DAYS_IN_MONTH.
 $DISPLAY_ALL_DAYS_IN_MONTH = 'N';
 // Include unapproved events?
 $get_unapproved = ( $DISPLAY_UNAPPROVED == 'Y' );
 
-$month1  = display_small_month(1,$year,False);
-$month2  = display_small_month(2,$year,False);
-$month3  = display_small_month(3,$year,False);
-$month4  = display_small_month(4,$year,False);
-$month5  = display_small_month(5,$year,False);
-$month6  = display_small_month(6,$year,False);
-$month7  = display_small_month(7,$year,False);
-$month8  = display_small_month(8,$year,False);
-$month9  = display_small_month(9,$year,False);
-$month10 = display_small_month(10,$year,False);
-$month11 = display_small_month(11,$year,False);
-$month12 = display_small_month(12,$year,False);
-
-$prevStr = translate ( 'Previous' );
 $nextStr = translate ( 'Next' );
-$userStr = ( ! empty ( $user ) ? "&amp;user=$user" : '' );
+$prevStr = translate ( 'Previous' );
+$userStr = ( empty ( $user ) ? '' : '&amp;user=' . $user );
+
 if ( $single_user == 'N' ) {
   if ( ! empty ( $user ) ) {
     user_load_variables ( $user, 'user_' );
-  $fullnameStr =  $user_fullname;
-  } else {
-  $fullnameStr = $fullname;
-  }
+    $fullnameStr = $user_fullname;
+  } else
+    $fullnameStr = $fullname;
 }
-$asstModeStr = ( $is_assistant ? '-- ' .
-  translate( 'Assistant mode' ) . ' --' : '' );
+$asstModeStr = ( $is_assistant
+  ? '      <span class="asstmode">-- '
+   . translate ( 'Assistant mode' ) . ' --</span>' : '' );
+
 if ( empty ( $friendly ) ) {
-  $unapprovedStr = display_unapproved_events ( ( $is_assistant ||
-    $is_nonuser_admin ? $user : $login ) );
+  $unapprovedStr = display_unapproved_events ( ( $is_assistant || $is_nonuser_admin
+      ? $user : $login ) );
   $printerStr = generate_printer_friendly ( 'year.php' );
-} else {
+} else
   $unapprovedStr = $printerStr = '';
+
+$yr_rows = 3;
+/* TODO: Move $yr_rows = 3 to webcal_config as default.
+ * Add to webcal_user_prefs for each user.
+ */
+$yr_cols = intval ( 12 / $yr_rows );
+$m = 1;
+
+$gridOmonths = '';
+
+for ( $r = 1; $r <= $yr_rows; $r++ ) {
+  $gridOmonths .= '        <tr>';
+
+  for( $c = 1; $c <= $yr_cols; $c++, $m++ ) {
+    $gridOmonths .= '
+          <td>' . display_small_month ( $m, $year, false ) . '</td>';
+  }
+  $gridOmonths .= '
+        </tr>';
 }
+
 $trailerStr = print_trailer ();
-print_header();
-
+print_header ();
 echo <<<EOT
-  <div class="title">
-    <a title="{$prevStr}" class="prev" href="year.php?year={$prevYear}{$userStr}">
-    <img src="images/leftarrow.gif" alt="{$prevStr}" /></a>
-    <a title="{$nextStr}" class="next" href="year.php?year={$nextYear}{$userStr}">
-    <img src="images/rightarrow.gif" alt="{$nextStr}" /></a>
-    <span class="date">{$thisyear}</span>
-    <br />
-    <span class="user">{$fullnameStr}</span>
-    <br />
-    <span class="asstmode">{$asstModeStr}</span>
-    {$catSelectStr}
-  </div>
-  <br />
-
-  <div align="center">
-  <table id="monthgrid">
-    <tr>
-      <td>{$month1}</td>
-      <td>{$month2}</td>
-      <td>{$month3}</td>
-      <td>{$month4}</td>
-    </tr>
-    <tr>
-      <td>{$month5}</td>
-      <td>{$month6}</td>
-      <td>{$month7}</td>
-      <td>{$month8}</td>
-    </tr>
-    <tr>
-      <td>{$month9}</td>
-      <td>{$month10}</td>
-      <td>{$month11}</td>
-      <td>{$month12}</td>
-    </tr>
-   </table>
-  </div>
-
-  <br />
-  {$unapprovedStr}
-  <br />
-  {$printerStr}
-  {$trailerStr}
+    <div class="title">
+      <a title="{$prevStr}" class="prev" href="year.php?year={$prevYear}{$userStr}">
+        <img src="images/leftarrow.gif" alt="{$prevStr}" /></a>
+      <a title="{$nextStr}" class="next" href="year.php?year={$nextYear}{$userStr}">
+        <img src="images/rightarrow.gif" alt="{$nextStr}" /></a>
+      <span class="date">{$thisyear}</span><br />
+      <span class="user">{$fullnameStr}</span><br />
+      {$asstModeStr}
+      {$catSelectStr}
+    </div><br />
+    <div align="center">
+      <table id="monthgrid">
+        {$gridOmonths}
+      </table>
+    </div><br />
+    {$unapprovedStr}<br />
+    {$printerStr}
+    {$trailerStr}
 EOT;
+
 ?>
