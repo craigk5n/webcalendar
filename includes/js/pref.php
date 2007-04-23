6decb1e2f7d3e97944978f6b3819b220462e3543
bbannon <bbannon>
2007-04-23 17:24:08 +0000

    A little cleanup.

--- includes/js/pref.php
+++ includes/js/pref.php
@@ -1,5 +1,5 @@
 <?php /* $Id$  */
-defined( '_ISVALID' ) or die( 'You cannot access this file directly!' );
+defined ( '_ISVALID' ) or die ( 'You cannot access this file directly!' );
 
   global $ALLOW_COLOR_CUSTOMIZATION;
 ?>
