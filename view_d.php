6c56d3b937b173f96825ea6e2af25e18187bbfd2
bbannon <bbannon>
2007-07-19 02:44:31 +0000

    Fix typos.

--- view_d.php
+++ view_d.php
@@ -65,11 +65,11 @@ echo <<<EOT
     <div class="viewnav">
       <a title="{$previousStr}" class="prev"
         href="view_d.php?id={$id}&amp;date={$prevdate}">
-        <img src="images/leftarrow.gif" class="prevnext"
+        <img src="images/leftarrow.gif" class="prev"
           alt="{$previousStr}" /></a>
       <a title="{$nextStr}" class="next"
         href="view_d.php?id={$id}&amp;date={$nextdate}">
-        <img src="images/rightarrow.gif" class="prevnext"
+        <img src="images/rightarrow.gif" class="next"
           alt="{$nextStr}" /></a>
       <div class="title">
         <span class="date">{$nowStr}</span><br />
