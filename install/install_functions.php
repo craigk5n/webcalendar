830cf23d0ad6a8aee135e5dab47f400bc27a8498
umcesrjones <umcesrjones>
2007-08-02 16:09:45 +0000

    Fixed bug [ 1754622 ] every 'normal' entry will be displayed as 'Priority: 2-High'

--- install/install_functions.php
+++ install/install_functions.php
@@ -132,6 +132,13 @@ function do_v11b_updates () {
     }
     dbi_free_result ( $res );
   }
+    // Update Priority to new values
+    //Old High=3, Low = 1....New Highest =1 Lowest =9
+    //We will leave 3 alone and change 1,2 to 7,5
+  dbi_execute ( 'UPDATE webcal_entry SET cal_priority = 7
+    WHERE cal_prioity = 1' );
+  dbi_execute ( 'UPDATE webcal_entry SET cal_priority = 5
+    WHERE cal_prioity = 2' );
 }
 
 /* Convert site_extra reminders to webcal_reminders. */
