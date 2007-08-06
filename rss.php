5a0d4ce67510c3d5790e787b6fe06dbf4d4340b5
umcesrjones <umcesrjones>
2007-08-06 14:10:30 +0000

    *** empty log message ***

--- rss.php
+++ rss.php
@@ -146,7 +146,7 @@ load_user_preferences ();
 // .
 // Determine what remote access has been set up by user.
 // This will only be used if $username is not __public__.
-if ( ! empty ( $USER_REMOTE_ACCESS ) && $username != '__public__' ) {
+if ( isset ( $USER_REMOTE_ACCESS ) && $username != '__public__' ) {
   // public entries only
   $allow_access = array ( 'P' );
   if ( $USER_REMOTE_ACCESS > 0 ) // plus confidential
@@ -258,7 +258,6 @@ for ( $i = $startTime; date ( 'Ymd', $i ) <= $endtimeYmd && $numEvents < $maxEve
   $d = date ( 'Ymd', $i );
   $eventIds = array ();
   $pubDate = gmdate ( 'D, d M Y', $i );
-
   $entries = get_entries ( $d, false );
   $rentries = get_repeating_entries ( $username, $d );
   $entrycnt = count ( $entries );
@@ -324,7 +323,7 @@ countentries==' . $entrycnt . ' ' . $rentrycnt . '
     <item>';
         $unixtime = date_to_epoch ( $rentries[$j]->getDateTime () );
         $dateinfo = ( $date_in_title == true
-          ? date ( $date_format, $unixtime )
+          ? date ( $date_format, $i )
            . ( $rentries[$j]->isAllDay () || $rentries[$j]->isUntimed ()
             ? $time_separator . date ( $time_format, $unixtime ) : '' ) . ' '
           : '' );
