322f0425e4791b2fa4d97786fe44dc0a605f9a1f
bbannon <bbannon>
2007-09-17 21:24:36 +0000

    Code cleanup.

--- includes/classes/hKit/hcal.profile.php
+++ includes/classes/hKit/hcal.profile.php
@@ -1,60 +1,59 @@
 <?php
-  // hcal profile for hkit
-  // hacked together by Ray Jones 06/26/2006
+// hcal profile for hkit
+// Hacked together by Ray Jones 06/26/2006
+// $Id$
 
-  $this->root_class = 'vevent';
+$this->root_class = 'vevent';
 
-  $this->classes = array (
-    'dtstart',
-    'dtend',
-    'duration',
-    'rrule', 'exdate', 'rdate',
-    'summary', 'description',
-    'location',
-    'url',
-    'class', 'categories', 'priority',
-    'transp', 'status',
-    'tz', 'uid'
+$this->classes = array (
+  'dtstart',
+  'dtend',
+  'duration',
+  'rrule',
+  'exdate',
+  'rdate',
+  'summary',
+  'description',
+  'location',
+  'url',
+  'class',
+  'categories',
+  'priority',
+  'transp',
+  'status',
+  'tz',
+  'uid'
   );
 
-  // classes that must only appear once per event
-  $this->singles = array (
-    'summary'
+// Classes that must only appear once per event.
+$this->singles = array (
+  'summary'
   );
 
-  // classes that are required (not strictly enforced - give at least one!)
-  $this->required = array (
-    'dtstart'
+// Classes that are required (not strictly enforced - give at least one!)
+$this->required = array (
+  'dtstart'
   );
 
-  $this->att_map = array (
-    'url'  => array ('A|href', 'IMG|src', 'AREA|href')
+$this->att_map = array (
+  'url' => array ( 'A|href', 'IMG|src', 'AREA|href' )
   );
 
-
-  $this->callbacks = array (
-    'url'  => array ($this, 'resolvePath'),
-    'email'  => array ($this, 'resolveEmail')
+$this->callbacks = array (
+  'url' => array ( $this, 'resolvePath' ),
+  'email' => array ( $this, 'resolveEmail' )
   );
 
-
-
-  function hKit_hcal_post($a)
-  {
-    foreach ($a as &$vevent){
-
-      hKit_labels_toUpper($vevent);
-
-    }
-
-    return $a;
-
+function hKit_hcal_post ( $a ) {
+  foreach ( $a as &$vevent ) {
+    hKit_labels_toUpper ( $vevent );
   }
 
+  return $a;
+}
 
-  function hKit_labels_toUpper(&$vevent)
-  {
-    $vevent = array_change_key_case($vevent, CASE_UPPER);
-  }
+function hKit_labels_toUpper ( &$vevent ) {
+  $vevent = array_change_key_case ( $vevent, CASE_UPPER );
+}
 
 ?>
