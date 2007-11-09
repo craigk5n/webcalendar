379dbe6648628844ad79df52105c3c31c1a7146b
umcesrjones <umcesrjones>
2007-11-09 21:32:54 +0000

    Fix bug [ 1829224 ] getPostVal fails on non-array

--- includes/formvars.php
+++ includes/formvars.php
@@ -26,7 +26,9 @@ function getPostValue ( $name ) {
   $postName = null;
   if ( isset ( $_POST ) && is_array ( $_POST ) && ! empty ( $_POST[$name] ) )
     $postName = ( get_magic_quotes_gpc () != 0
-      ? $_POST[$name] : addslashes ( $_POST[$name] ) );
+      ? $_POST[$name] : (is_array ( $_POST[$name] ) 
+			? array_map ( 'addslashes',  
+			$_POST[$name] ): addslashes ( $_POST[$name] ) ) );
   return $postName;
 }
 
