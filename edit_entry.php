aadd606bb94a5ac8a1fdf7ecc1dc6d69963e5c46
umcesrjones <umcesrjones>
2007-11-16 22:04:17 +0000

    Removed MANY tabs

--- edit_entry.php
+++ edit_entry.php
@@ -54,7 +54,7 @@ if ( _WC_READONLY ) {
 } else if ( ! empty ( $eid ) && $eid > 0 ) {
   $item = loadEvent ( $eid  );
  //  print_r ( $item);
-	if ( ! $item ) {
+  if ( ! $item ) {
     $error = str_replace ('XXX', $eid, translate ( 'Invalid entry id XXX' ) );
   } else {
     // first see who has access to edit this entry
@@ -93,48 +93,48 @@ if ( _WC_READONLY ) {
     //Don't adjust for All Day entries
     $cal_date = $item->getDate( 'Ymd' );
     $cal_time = $item->getDate( 'His' );
-		
-		$end_time = $item->getEndDate ( 'His' );
-	
+    
+    $end_time = $item->getEndDate ( 'His' );
+  
   
     $due_date = $item->getDueDate( 'Ymd' );
     $due_time = $item->getDueDate( 'His' );
 
     
     $can_edit_uac = access_user_calendar ( 'edit', 
-		  $create_by, $WC->loginId(), $item->getCalType(), $access );
-		if ( ! $can_edit_uac ) {
-		   $can_edit = false;
-			 $error = str_replace ('XXX', 'UAC', translate ( 'Restricted by XXX' ) );
-		}
-		  
+      $create_by, $WC->loginId(), $item->getCalType(), $access );
+    if ( ! $can_edit_uac ) {
+       $can_edit = false;
+       $error = str_replace ('XXX', 'UAC', translate ( 'Restricted by XXX' ) );
+    }
+      
     //if we allow NUCs to edit their own events, we can compare their IP
     // to the one saved with the event.
     if ( getPref ( 'RESTRICT_NUC_EDITS_BY_IP', 2 ) && $WC->isNonUser() && 
-	  ( $cal_creatorIp != ip2long ( $_SERVER['REMOTE_ADDR'] ) ) ) {
+    ( $cal_creatorIp != ip2long ( $_SERVER['REMOTE_ADDR'] ) ) ) {
       $can_edit = false;
-			$error = str_replace ('XXX', 'IP', translate ( 'Restricted by XXX' ) );
-		}
+      $error = str_replace ('XXX', 'IP', translate ( 'Restricted by XXX' ) );
+    }
     //what kind of entry are we dealing with?
     if ( $eType == 'task' ) {      
-			$others_complete = 'Y';
-			$pctnum = 0;
-			$sql = 'SELECT cal_login_id,  cal_percent, cal_status 
-			 FROM webcal_entry_user WHERE cal_id = ?';
+      $others_complete = 'Y';
+      $pctnum = 0;
+      $sql = 'SELECT cal_login_id,  cal_percent, cal_status 
+       FROM webcal_entry_user WHERE cal_id = ?';
       $res = dbi_execute ( $sql, array( $eid ) );
       if ( $res ) {
         while ( $row = dbi_fetch_row ( $res ) ) {
           $overall_percent[$pctnum]['percent'] = $row[1];
-		      $overall_percent[$pctnum]['fullname'] = $WC->User->getFullName( $row[0] ); 
-					if ( $row[1] < 100 ) $others_complete = 'N';
+          $overall_percent[$pctnum]['fullname'] = $WC->User->getFullName( $row[0] ); 
+          if ( $row[1] < 100 ) $others_complete = 'N';
           if ( $WC->isLogin( $row[0] ) || ( $WC->isAdmin() && 
-	          $WC->userId() == $row[0] ) ) {
+            $WC->userId() == $row[0] ) ) {
             $task_percent = $row[1];
             $task_status = $row[2];
           } 
-				$pctnum++;
+        $pctnum++;
         }
-				if ( $pctnum == 1 ) $others_complete = 'Y';  
+        if ( $pctnum == 1 ) $others_complete = 'Y';  
         dbi_free_result ( $res ); 
       } 
     }
@@ -154,37 +154,37 @@ if ( _WC_READONLY ) {
       $rpt_end_date = $cal_date;
       $rpt_freq = 1;
     } elseif ( $item->isRepeat() ) {
-			$rpt_type = $item->getRepeatType();
-			$rpt_end = $item->getRepeatType();
+      $rpt_type = $item->getRepeatType();
+      $rpt_end = $item->getRepeatType();
 
-			if ( $item->getRepeatEnd() ) {
-				 $rpt_end_date = date( 'Ymd', $item->getRepeatEnd() );
-				 $rpt_end_time = date( 'His', $item->getRepeatEnd() );
-			}  else {
-				$rpt_end_date = $cal_date;
-				$rpt_end_time = $cal_time;
-			}        
-			$rpt_freq = $item->getRepeatFrequency();
-			
-		  $byday = explode( ',', $item->getRepeatByDay() );
-			$smarty->assign ( 'bydayStr', $item->getRepeatByDay() );
-			
-			$bymonth = explode( ',', $item->getRepeatByMonth() );
-			$smarty->assign ( 'bymonth', $item->getRepeatByMonth() );
-			
-			$bymonthday = explode( ',', $item->getRepeatByMonthDay() );
-			$smarty->assign ( 'bymonthdayStr', $item->getRepeatByMonthDay() );
+      if ( $item->getRepeatEnd() ) {
+         $rpt_end_date = date( 'Ymd', $item->getRepeatEnd() );
+         $rpt_end_time = date( 'His', $item->getRepeatEnd() );
+      }  else {
+        $rpt_end_date = $cal_date;
+        $rpt_end_time = $cal_time;
+      }        
+      $rpt_freq = $item->getRepeatFrequency();
       
-			$bysetpos = explode( ',', $item->getRepeatBySetPos() );			
-			$smarty->assign ( 'bysetposStr', $item->getRepeatBySetPos() );
-			
-			$smarty->assign ( 'byweekno', $item->getRepeatByWeekNo() );
-			$smarty->assign ( 'byyearday', $item->getRepeatByYearDay() );
-			$smarty->assign ( 'wkst', $item->getRepeatWkst() );
-			$smarty->assign ( 'rpt_count', $item->getRepeatCount() );
-					 
-			//Check to see if Weekends Only is applicable
-			$smarty->assign ( 'weekdays_only', $item->isWeekdaysOnly() );
+      $byday = explode( ',', $item->getRepeatByDay() );
+      $smarty->assign ( 'bydayStr', $item->getRepeatByDay() );
+      
+      $bymonth = explode( ',', $item->getRepeatByMonth() );
+      $smarty->assign ( 'bymonth', $item->getRepeatByMonth() );
+      
+      $bymonthday = explode( ',', $item->getRepeatByMonthDay() );
+      $smarty->assign ( 'bymonthdayStr', $item->getRepeatByMonthDay() );
+      
+      $bysetpos = explode( ',', $item->getRepeatBySetPos() );      
+      $smarty->assign ( 'bysetposStr', $item->getRepeatBySetPos() );
+      
+      $smarty->assign ( 'byweekno', $item->getRepeatByWeekNo() );
+      $smarty->assign ( 'byyearday', $item->getRepeatByYearDay() );
+      $smarty->assign ( 'wkst', $item->getRepeatWkst() );
+      $smarty->assign ( 'rpt_count', $item->getRepeatCount() );
+           
+      //Check to see if Weekends Only is applicable
+      $smarty->assign ( 'weekdays_only', $item->isWeekdaysOnly() );
 
 
     //Get Repeat Exceptions/Inclusions
@@ -193,7 +193,7 @@ if ( _WC_READONLY ) {
 
       //determine if Expert mode needs to be set
       $smarty->assign ( 'expert_mode', $item->isExpertMode() );
-		}  
+    }  
 
   if ( getPref ( 'CATEGORIES_ENABLED' ) ) {
     $catById = get_categories_by_eid ( $eid, $real_user, true );
@@ -209,16 +209,16 @@ if ( _WC_READONLY ) {
   
   //get participants
   $sql = 'SELECT cal_login_id , cal_status 
-	  FROM webcal_entry_user 
-		WHERE cal_id = ? AND cal_status IN (\'A\', \'W\' )';
+    FROM webcal_entry_user 
+    WHERE cal_id = ? AND cal_status IN (\'A\', \'W\' )';
   $res = dbi_execute ( $sql, array( $eid ) );
   if ( $res ) {
-	  $partnum=0;
+    $partnum=0;
     while ( $row = dbi_fetch_row ( $res ) ) {
       $participants[$partnum]['cal_login_id'] = $row[0];
-			$participants[$partnum]['cal_fullname'] = $WC->getFullName ( $row[0] );
-			$participants[$partnum]['status'] = ( $row[1] == 'W' ? '(?)' : '' );
-		  $partnum++;
+      $participants[$partnum]['cal_fullname'] = $WC->getFullName ( $row[0] );
+      $participants[$partnum]['status'] = ( $row[1] == 'W' ? '(?)' : '' );
+      $partnum++;
     }
     dbi_free_result ( $res );    
   }
@@ -226,7 +226,7 @@ if ( _WC_READONLY ) {
   if (  $eType == 'event'  && getPref ( 'ALLOW_EXTERNAL_USERS' ) ) {
     $external_users = event_get_external_users ( $eid );
   }
-	}
+  }
 } else {
  // ##########   New entry.   ################
  // to avoid warnings below about use of undefined var
@@ -247,34 +247,38 @@ if ( _WC_READONLY ) {
  
  //Did we pass in a time?
  $cal_time = $WC->getValue ( 'cal_time' );
- $smarty->assign ( 'isTimed', ! empty ( $cal_time ) );	
+ $smarty->assign ( 'isTimed', ! empty ( $cal_time ) );  
  
   if ( ! empty ( $defusers ) ) {
-	  $smarty->assign ( 'isTimed', true );		
+    $smarty->assign ( 'isTimed', true );    
     $tmp_ar = explode ( ',', $defusers );
     for ( $i = 0, $cnt = count ( $tmp_ar ); $i < $cnt; $i++ ) {
       $participants[$tmp_ar[$i]] = 1;
     }
   }
-	
-	$smarty->assign ( 'allDay', $WC->getGet ( 'allday' ) );
-	
-	$cat_id = $WC->getGet ( 'cat_id' );
-	//if ( $cat_id && $cat_id > 0 ) {
-	  $categories = $WC->categories ();
-		if ( isset ( $categories[$cat_id] ) ) {
-	    $catNames = $categories[$cat_id]['cat_name'];
-	    $catList = $WC->getGet ( 'cat_id' );
-		}
-	//}
-	$overall_percent = 0;
-	$others_complete = 'Y';
-	
-	if ( $eType == 'task' ) {
-	  $overall_percent = array();
-	  $overall_percent[0]['percent'] = 0;
-		$overall_percent[0]['fullname'] = $WC->User->getFullName( $WC->loginId() );
-	}
+  if ( empty ( $participants ) ) {
+    $participants[0]['cal_login_id'] = $WC->loginId();
+    $participants[0]['cal_fullname'] = $WC->getFullName ( $WC->loginId() );
+  }
+  
+  $smarty->assign ( 'allDay', $WC->getGet ( 'allday' ) );
+  
+  $cat_id = $WC->getGet ( 'cat_id' );
+  //if ( $cat_id && $cat_id > 0 ) {
+    $categories = $WC->categories ();
+    if ( isset ( $categories[$cat_id] ) ) {
+      $catNames = $categories[$cat_id]['cat_name'];
+      $catList = $WC->getGet ( 'cat_id' );
+    }
+  //}
+  $overall_percent = 0;
+  $others_complete = 'Y';
+  
+  if ( $eType == 'task' ) {
+    $overall_percent = array();
+    $overall_percent[0]['percent'] = 0;
+    $overall_percent[0]['fullname'] = $WC->User->getFullName( $WC->loginId() );
+  }
 } //end new entry
 
 if ( ! empty ( $error ) || ! $can_edit ) {
@@ -389,42 +393,42 @@ if ( ( ! getPref ( 'DISABLE_PARTICIPANTS_FIELD' )
   $myuserlist = array ();
   $smarty->assign ( 'show_participants', true );
   
-	$userlist = get_my_users ( $real_user, 'invite', false );
+  $userlist = get_my_users ( $real_user, 'invite', false );
   $size = count ( $userlist );
-	 
-	$nonuserlist = get_nonuser_cals ();
+   
+  $nonuserlist = get_nonuser_cals ();
   $size = ( count ( $nonuserlist ) > $size ? count ( $nonuserlist ) : $size );
-	  
-  $grouplist = get_groups ( $real_user );	
+    
+  $grouplist = get_groups ( $real_user );  
   $size = ( count ( $grouplist ) > $size ? count ( $grouplist ) : $size );
-	 
+   
   for ( $i = 0, $usercnt = count ( $userlist ); $i < $usercnt; $i++ ) {
     $l = $myuserlist[$i]['cal_login_id'] = $userlist[$i]['cal_login_id'];
     $n = $myuserlist[$i]['cal_fullname'] = $userlist[$i]['cal_fullname'];
     if ( ! empty ( $eid ) ) {
       if ( ! empty ($participants[$l]) )
-        $userlist[$i]['selected'] = SELECTED;
+        $participants[$i]['selected'] = SELECTED;
     } else {
       if ( ! empty ($defusers) && ! empty ( $participants[$l] ) ) {
         // default selection of participants was in the URL
-        $userlist[$i]['selected'] = SELECTED;
+        $participants[$i]['selected'] = SELECTED;
       }
       if ( ( $WC->isLogin( $l ) &&  ! $WC->isNonuserAdmin() ) 
-			  || ( $WC->userId() == $l ) ) {
-        $userlist[$i]['selected'] = SELECTED;
+        || ( $WC->userId() == $l ) ) {
+        $participants[$i]['selected'] = SELECTED;
       }
     }
   }
   if ( $size > 50 )
     $size = 15;
   else if ( $size > 5 )
-    $size = 5;	
+    $size = 5;  
 
-	$smarty->assign ( 'size', $size );
-	$smarty->assign ( 'myuserlist', $myuserlist );
-	$smarty->assign ( 'nonuserlist', $nonuserlist );
-	$smarty->assign ( 'grouplist', $grouplist );
-	$smarty->assign ( 'participants', $participants );
+  $smarty->assign ( 'size', $size );
+  $smarty->assign ( 'myuserlist', $myuserlist );
+  $smarty->assign ( 'nonuserlist', $nonuserlist );
+  $smarty->assign ( 'grouplist', $grouplist );
+  $smarty->assign ( 'participants', $participants );
 }
 
 //Setup Repeat data
@@ -432,45 +436,45 @@ $smarty->assign ( 'rpt_type', $rpt_type );
 $smarty->assign ( 'rpt_freq', $rpt_freq );
 //set up bymonth
 for ( $by_month =1;$by_month <=12; $by_month++){
-	$smartybymon[$by_month]['checked']= in_array($by_month,$bymonth)? CHECKED :'';
-	$smartybymon[$by_month]['date']= translate ( date ( 'M', mktime ( 0,0,0, $by_month,1 ) ) );
+  $smartybymon[$by_month]['checked']= in_array($by_month,$bymonth)? CHECKED :'';
+  $smartybymon[$by_month]['date']= translate ( date ( 'M', mktime ( 0,0,0, $by_month,1 ) ) );
 }
 $smarty->assign ( 'bymonth', $smartybymon );
 //set up bydayAll
 for ( $byday_single =0;$byday_single <=6; $byday_single++){
     $bydayAlllbl = translate ( $WC->byday_names[$byday_single] );
-		$bydayAll[$bydayAlllbl] = in_array($WC->byday_names[$byday_single],$byday)? CHECKED :'';
+    $bydayAll[$bydayAlllbl] = in_array($WC->byday_names[$byday_single],$byday)? CHECKED :'';
 }
 $smarty->assign ( 'bydayAll', $bydayAll );
 
 //set up byday
 for ( $loop_ctr=1; $loop_ctr <= 6; $loop_ctr++) {
-	for ( $rpt_byday =0;$rpt_byday <=6; $rpt_byday++){
-	  $rptByday[$loop_ctr . $rpt_byday]['id'] = "b$loop_ctr$rpt_byday";
-		$rptByday[$loop_ctr . $rpt_byday]['value'] = 
-		  (in_array($loop_ctr . $WC->byday_names[$rpt_byday],$byday) 
-		?$loop_ctr . translate ( $WC->byday_names[$rpt_byday] )
-	 : (in_array(($loop_ctr -6) . $WC->byday_names[$rpt_byday],$byday)
-	 ?($loop_ctr -6) . translate ( $WC->byday_names[$rpt_byday] ):'        ')); 
-	}
+  for ( $rpt_byday =0;$rpt_byday <=6; $rpt_byday++){
+    $rptByday[$loop_ctr . $rpt_byday]['id'] = "b$loop_ctr$rpt_byday";
+    $rptByday[$loop_ctr . $rpt_byday]['value'] = 
+      (in_array($loop_ctr . $WC->byday_names[$rpt_byday],$byday) 
+    ?$loop_ctr . translate ( $WC->byday_names[$rpt_byday] )
+   : (in_array(($loop_ctr -6) . $WC->byday_names[$rpt_byday],$byday)
+   ?($loop_ctr -6) . translate ( $WC->byday_names[$rpt_byday] ):'        ')); 
+  }
 }
 $smarty->assign ( 'byday', $rptByday );
 
 //set up bymonthday
 for ( $loop_ctr=1; $loop_ctr <32; $loop_ctr++) {
   $rptByMonthDay[$loop_ctr]['id'] = 'bymonthday' . $loop_ctr;
-	$rptByMonthDay[$loop_ctr]['value'] =  (in_array($loop_ctr,$bymonthday) 
-		?($loop_ctr):(in_array(($loop_ctr -32),$bymonthday)
-	 ?($loop_ctr -32):'')); 
+  $rptByMonthDay[$loop_ctr]['value'] =  (in_array($loop_ctr,$bymonthday) 
+    ?($loop_ctr):(in_array(($loop_ctr -32),$bymonthday)
+   ?($loop_ctr -32):'')); 
 }
 $smarty->assign ( 'bymonthday', $rptByMonthDay );
 
 //set up bysetpos
 for ( $loop_ctr=1; $loop_ctr <32; $loop_ctr++) {
   $rptBySetPos[$loop_ctr]['id'] = 'p' . $loop_ctr;
-	$rptBySetPos[$loop_ctr]['value'] = (in_array($loop_ctr,$bysetpos) 
-		?($loop_ctr):(in_array(($loop_ctr -32),$bysetpos)
-	 ?($loop_ctr -32):'')); 
+  $rptBySetPos[$loop_ctr]['value'] = (in_array($loop_ctr,$bysetpos) 
+    ?($loop_ctr):(in_array(($loop_ctr -32),$bysetpos)
+   ?($loop_ctr -32):'')); 
 }
 $smarty->assign ( 'bysetpos', $rptBySetPos );
 
