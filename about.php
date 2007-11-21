c6f9dff78298e619bf546a8b07be540bc1348434
bbannon <bbannon>
2007-11-21 19:02:10 +0000

    Code cleanup.

--- about.php
+++ about.php
@@ -6,7 +6,7 @@ $credits = $WC->getPOST ( 'Credits' );
 $data = '';
 
 if ( ! empty ( $credits ) ) {
-  // Get Names from AUTHORS file
+  // Get Names from AUTHORS file.
   if ( $fd = @fopen ( 'AUTHORS', 'r' ) ) {
     // Read in contents of entire file first.
     while ( ! feof ( $fd ) && empty ( $error ) ) {
@@ -18,11 +18,12 @@ if ( ! empty ( $credits ) ) {
   $data = preg_replace ( '/<.+>+/', '', $data );
   $data = preg_replace ( "/\n\s/", '<br />&nbsp;', $data );
   $data = preg_replace ( '/\s\s+/', '&nbsp;&nbsp;', $data );
-  $data = preg_replace ( '/\n/', '<br />', $data );
+  $data = preg_replace ( "/\n/", '<br />', $data );
 }
 build_header ( '', '', '', 61 );
 $smarty->assign ( 'credits', $credits );
 $smarty->assign ( 'data', $data );
-$smarty->display ( 'about.tpl' );
-?>
 
+$smarty->display ( 'about.tpl' );
+
+?>
