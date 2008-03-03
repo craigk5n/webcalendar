7e80096392c648dc17ea7b0d546981bf49a5d51f
Craig Knudsen <craig@k5n.us>
2008-03-03 20:06:54 +0000

    Fixes for edit_entry_handler to call getPostValue

--- edit_entry_handler.php
+++ edit_entry_handler.php
@@ -47,6 +47,8 @@ function sort_byday ( $a, $b ) {
 
 $id = getPostValue ( 'cal_id' );
 
+$override = getPostValue ( 'override' );
+$override_date = getPostValue ( 'override_date' );
 if ( ! empty ( $override ) && ! empty ( $override_date ) ) {
   // Override date specified.
   // User is going to create an exception to a repeating event.
@@ -55,6 +57,7 @@ if ( ! empty ( $override ) && ! empty ( $override_date ) ) {
 }
 
 // Remember previous cal_goup_id if present.
+$parent = getPostValue ( 'parent' );
 $old_id = ( empty ( $parent ) ? $old_id : $parent );
 $old_status = array ();
 
@@ -69,10 +72,41 @@ $name = getPostValue ( 'name' );
 $priority = getPostValue ( 'priority' );
 $reminder = getPostValue ( 'reminder' );
 $rpt_type = getPostValue ( 'rpt_type' );
+$rptmode = getPostValue ( 'rptmode' );
+$rpt_end_use = getPostValue ( 'rpt_end_use' );
+$rpt_count = getPostValue ( 'rpt_count' );
+$rpt_freq = getPostValue ( 'rpt_freq' );
+$weekdays_only = getPostValue ( 'weekdays_only' );
+$wkst = getPostValue ( 'wkst' );
+$bydayList = getPostValue ( 'bydayList' );
+$bymonthdayList = getPostValue ( 'bymonthdayList' );
+$bysetposList = getPostValue ( 'bysetposList' );
+$bydayAll = getPostValue ( 'bydayAll' );
+$byday = getPostValue ( 'byday' );
+$bymonth = getPostValue ( 'bymonth' );
+$bysetpos = getPostValue ( 'bysetpos' );
+$bymonthday = getPostValue ( 'bymonthday' );
+$byweekno = getPostValue ( 'byweekno' );
+$byyearday = getPostValue ( 'byyearday' );
+$exceptions = getPostValue ( 'exceptions' );
 $timetype = getPostValue ( 'timetype' );
 $participants = getPostValue ( 'selectedPart' );
 // entry_changed is calculated client-side with javascript
 $entry_changed = getPostValue ( 'entry_changed' );
+$rem_action = getPostValue ( 'rem_action' );
+$rem_last_sent = getPostValue ( 'rem_last_sent' );
+$rem_times_sent = getPostValue ( 'rem_times_sent' );
+$reminder = getPostValue ( 'reminder' );
+$rem_when = getPostValue ( 'rem_when' );
+$rem_hours = getPostValue ( 'rem_hours' );
+$rem_minutes = getPostValue ( 'rem_minutes' );
+$rem_before = getPostValue ( 'rem_before' );
+$rem_related = getPostValue ( 'rem_related' );
+$rem_rep_count = getPostValue ( 'rem_rep_count' );
+$rem_rep_days = getPostValue ( 'rem_rep_days' );
+$rem_rep_hours = getPostValue ( 'rem_rep_hours' );
+$rem_rep_minutes = getPostValue ( 'rem_rep_minutes' );
+$rem_rep_minutes = getPostValue ( 'rem_rep_minutes' );
 
 $description =
 ( strlen ( $description ) == 0 || $description == '<br />'
@@ -103,6 +137,7 @@ $year = getPostValue ( 'year' );
 if ( empty ( $eType ) )
   $eType = 'event';
 
+$percent = getPostValue ( 'percent' );
 if ( empty ( $percent ) )
   $percent = 0;
 
@@ -228,6 +263,7 @@ if ( $eType != 'task' ) {
  */
 $can_doall = $can_edit = false;
 // Value may be needed later for recreating event.
+$user = getPostValue ( 'user' );
 $old_create_by = ( empty ( $user ) ? '' : $user );
 if ( empty ( $id ) )
   // New event...
@@ -618,7 +654,8 @@ if ( empty ( $error ) ) {
     if ( ! empty ( $site_extras[$i][5] ) )
       $extra_email = $site_extras[$i][5] & EXTRA_DISPLAY_EMAIL;
 
-    $value = $$extra_name;
+    //$value = $$extra_name;
+    $value = getPostValue ( $extra_name );
     $sql = '';
     $query_params = array ();
 
@@ -984,6 +1021,7 @@ if ( empty ( $error ) ) {
   // Add external participants.
   $ext_emails = $ext_names = $matches = array ();
   $ext_count = 0;
+  $externalparticipants = getPostValue ( 'externalparticipants' );
   if ( $single_user == 'N' && !
     empty ( $ALLOW_EXTERNAL_USERS ) && $ALLOW_EXTERNAL_USERS == 'Y' && !
       empty ( $externalparticipants ) ) {
