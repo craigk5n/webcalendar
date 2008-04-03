108e4ba236f83fd918b4906bca0ed977aa0b63fc
umcesrjones <umcesrjones>
2008-04-03 20:07:39 +0000

    Commented out do_debug settings

--- includes/functions.php
+++ includes/functions.php
@@ -30,8 +30,8 @@
  */
 function do_debug ( $msg ) {
   // log to /tmp/webcal-debug.log
-   error_log ( date ( 'Y-m-d H:i:s' ) . "> $msg\n<br />",
-   3, 'c:/wamp/logs/debug.txt' );
+   //error_log ( date ( 'Y-m-d H:i:s' ) . "> $msg\n<br />",
+   //3, 'c:/wamp/logs/debug.txt' );
   // fwrite ( $fd, date ( 'Y-m-d H:i:s' ) . "> $msg\n" );
   // fclose ( $fd );
   // 3, '/tmp/webcal-debug.log' );
