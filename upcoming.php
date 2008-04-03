468eebf1b2ff0c37f222cf1983fa2c11a51611fa
umcesrjones <umcesrjones>
2008-04-03 18:19:36 +0000

    Moved translate

--- upcoming.php
+++ upcoming.php
@@ -82,13 +82,13 @@ if ( empty ($upcoming_initialized)) {
 $save_current_working_dir= getcwd ();
 chdir(dirname(__FILE__));
 
+include_once 'includes/translate.php';
 require_once 'includes/classes/WebCalendar.class';
 require_once 'includes/classes/Event.class';
 require_once 'includes/classes/RptEvent.class';
 
 $WebCalendar =& new WebCalendar ( __FILE__ );
 
-include 'includes/translate.php';
 include 'includes/config.php';
 include 'includes/dbi4php.php';
 include 'includes/formvars.php';
