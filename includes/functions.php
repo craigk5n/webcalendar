fdfda53790beff8fd18bf24747f4f0ca58f1b2dd
umcesrjones <umcesrjones>
2008-04-21 19:14:49 +0000

    Fixed bug [ 1948086 ] File Attachments

--- includes/functions.php
+++ includes/functions.php
@@ -30,8 +30,8 @@
  */
 function do_debug ( $msg ) {
   // log to /tmp/webcal-debug.log
-   //error_log ( date ( 'Y-m-d H:i:s' ) . "> $msg\n<br />",
-   //3, 'c:/wamp/logs/debug.txt' );
+   error_log ( date ( 'Y-m-d H:i:s' ) . "> $msg\n<br />",
+   3, 'c:/wamp/logs/debug.txt' );
   // fwrite ( $fd, date ( 'Y-m-d H:i:s' ) . "> $msg\n" );
   // fclose ( $fd );
   // 3, '/tmp/webcal-debug.log' );
@@ -4292,7 +4292,7 @@ function print_checkbox ( $vals, $id = '', $onchange = '' ) {
 
   $setting = ( empty ( $vals[3] ) ? $vals[0] : $vals[3] );
   $variable = $vals[0];
-
+  $hidden = '';
   if ( ! empty ( $id ) && $id = 'dito' )
     $id = $vals[0];
 
@@ -4305,13 +4305,17 @@ function print_checkbox ( $vals, $id = '', $onchange = '' ) {
   if ( $SCRIPT == 'admin.php' ) {
     $setting = $s[$vals[0]];
     $variable = 'admin_' . $vals[0];
+	$hidden = '
+	  <input type="hidden" name="' . $variable . '" value="N" />';
   }
   if ( $SCRIPT == 'pref.php' ) {
     $setting = $prefarray[$vals[0]];
     $variable = 'pref_' . $vals[0];
+	$hidden = '
+	  <input type="hidden" name="' . $variable . '" value="N" />';
   }
-  return '
-      <label><input type="checkbox" name="' . $variable . '" value="' . $vals[1]
+  return $hidden . '
+    <label><input type="checkbox" name="' . $variable . '" value="' . $vals[1]
    . '" ' . ( empty ( $id ) ? '' : 'id="' . $id . '" ' )
    . ( $setting == $vals[1] ? $checked : '' )
    . ( empty ( $onchange ) ? '' : ' onchange="' . $onchange . '()"' )
