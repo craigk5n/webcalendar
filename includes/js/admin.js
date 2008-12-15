8ea4b852ed6d86de0bb3b12c3ffa750339318f20
bbannon <bbannon>
2008-12-15 09:45:38 +0000

    Starting to remove PHP from javascript files.

---
+++ includes/js/admin.js
@@ -0,0 +1,203 @@
+// $Id$
+
+function init_admin() {
+  attach_handler();
+  comment_handler();
+  email_handler();
+  eu_handler();
+  popup_handler();
+  public_handler();
+  sr_handler();
+  altps();
+
+  return true;
+}
+
+function valid_form( form ) {
+  var err = '';
+
+  if ( form.admin_SERVER_URL.value == '' ) {
+    err = xlate['reqServerURL'];
+    form.admin_SERVER_URL.select();
+    form.admin_SERVER_URL.focus();
+  }
+  else if ( form.admin_SERVER_URL.value.charAt(
+    form.admin_SERVER_URL.value.length - 1 ) != '/' ) {
+      err = xlate['endServerURL'];
+      form.admin_SERVER_URL.select();
+      form.admin_SERVER_URL.focus();
+  }
+
+  if ( parseInt( form.admin_WORK_DAY_START_HOUR.value ) >=
+    parseInt( form.admin_WORK_DAY_END_HOUR.value ) ) {
+      err += xlate['invalidHours'];
+      form.admin_WORK_DAY_START_HOUR.select();
+      form.admin_WORK_DAY_START_HOUR.focus();
+  }
+
+  if ( err != '' ) {
+    alert( xlate['error'] + err );
+    return false;
+  }
+
+  if ( !valid_color( form.admin_BGCOLOR.value ) ) {
+    err = xlate['invalidDocuBG'];
+    form.admin_BGCOLOR.select();
+    form.admin_BGCOLOR.focus();
+  }
+  else if ( !valid_color( form.admin_H2COLOR.value ) ) {
+    err = xlate['invalidTitleFG'];
+    form.admin_H2COLOR.select();
+    form.admin_H2COLOR.focus();
+  } 
+  else if ( !valid_color( form.admin_CELLBG.value ) ) {
+    err = xlate['invalidCellBG'];
+    form.admin_CELLBG.select();
+    form.admin_CELLBG.focus();
+  } 
+  else if ( !valid_color( form.admin_TABLEBG.value ) ) {
+    err = xlate['invalidGridFG'];
+    form.admin_TABLEBG.select();
+    form.admin_TABLEBG.focus();
+  } 
+  else if ( !valid_color( form.admin_THBG.value ) ) {
+    err = xlate['invalidTHBG'];
+    form.admin_THBG.select();
+    form.admin_THBG.focus();
+  } 
+  else if ( !valid_color( form.admin_THFG.value ) ) {
+    err = xlate['invalidTextFG'];
+    form.admin_THFG.select();
+    form.admin_THFG.focus();
+  } 
+  else if ( !valid_color( form.admin_POPUP_BG.value ) ) {
+    err = xlate['invalidPopupBG'];
+    form.admin_POPUP_BG.select();
+    form.admin_POPUP_BG.focus();
+  } 
+  else if ( !valid_color( form.admin_POPUP_FG.value ) ) {
+    err = xlate['invalidPopupFG'];
+    form.admin_POPUP_FG.select();
+    form.admin_POPUP_FG.focus();
+  } 
+  else if ( !valid_color( form.admin_TODAYCELLBG.value ) ) {
+    err = xlate['invalidTodayBG'];
+    form.admin_TODAYCELLBG.select();
+    form.admin_TODAYCELLBG.focus();
+  }
+
+  if ( err.length > 0 ) {
+    alert( xlate['error'] + err + "\n\n" + xlate['formatColorRGB'] );
+    return false;
+  }
+  return true;
+}
+
+// Gets called on page load and when user changes setting for
+// "Disable popup".
+function popup_handler() {
+  if ( document.prefform.admin_DISABLE_POPUPS[0].checked ) {
+    // Popups disabled
+    makeInvisible( 'pop' );
+  } else {
+    // Popups allowed
+    makeVisible( 'pop' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Allow public access".
+function public_handler() {
+  if ( document.prefform.admin_PUBLIC_ACCESS[0].checked ) {
+    // Public Access enabled
+    makeVisible( 'pa' );
+  } else {
+    // Public Access disabled
+    makeInvisible( 'pa' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Allow external users".
+function eu_handler() {
+  if ( document.prefform.admin_ALLOW_EXTERNAL_USERS[0].checked ) {
+    // External Users enabled
+    makeVisible( 'eu' );
+  } else {
+    makeInvisible( 'eu' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Allow self registration".
+function sr_handler() {
+  if ( document.prefform.admin_ALLOW_SELF_REGISTRATION[0].checked ) {
+    // Self Registration enabled
+    makeVisible( 'sr' );
+  } else {
+    makeInvisible( 'sr' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Allow attachments".
+function attach_handler() {
+  if ( document.prefform.admin_ALLOW_ATTACH[0].checked ) {
+    makeVisible( 'at1' );
+  } else {
+    makeInvisible( 'at1' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Allow comments".
+function comment_handler() {
+  if ( document.prefform.admin_ALLOW_COMMENTS[0].checked ) {
+    makeVisible( 'com1' );
+  } else {
+    makeInvisible( 'com1' );
+  }
+}
+
+// Gets called on page load and when user changes setting for
+// "Email enabled".
+function email_handler() {
+  if ( document.prefform.admin_SEND_EMAIL[0].checked ) {
+    // Email enabled
+    makeVisible( 'em' );
+    if ( document.prefform.admin_EMAIL_MAILER.selectedIndex == 0 ) {
+      makeVisible( 'em_smtp' );
+      if ( document.prefform.admin_SMTP_AUTH[0].checked ) {
+        makeVisible( 'em_auth' )
+      } else {
+        makeInvisible( 'em_auth' )
+      }
+    } else {
+      makeInvisible( 'em_smtp' );
+    }
+  } else {
+    makeInvisible( 'em' );
+  }
+}
+
+//See the showTab function in includes/js/visible.js
+//for common code shared by all pages using the tabbed GUI.
+var tabs = ['', 'settings', 'public', 'uac',
+  'groups', 'nonuser', 'other', 'email', 'colors']; 
+
+function showPreview() {
+  var theme = document.forms['prefform'].admin_THEME.value.toLowerCase();
+
+  if ( theme == 'none' ) {
+    return false;
+  }
+  var previewWindow =
+    window.open( 'themes/' + theme  + '.php',
+      'Preview','resizable=yes,scrollbars=yes' );
+}
+
+function setTab( tab ) {
+  document.forms['prefform'].currenttab.value = tab;
+  showTab( tab );
+  return false;
+}
---
+++ includes/js/visible.js
@@ -0,0 +1,196 @@
+// $Id$
+
+// detect browser
+NS4 = ( document.layers ? 1 : 0 );
+IE4 = ( document.all ? 1 : 0 );
+// W3C stands for the W3C standard, implemented in Mozilla (and Netscape 6) and IE5
+W3C = ( document.getElementById ? 1 : 0 );
+
+function makeVisible( name, hide ) {
+  var ele;
+
+  if ( W3C ) {
+    ele = document.getElementById( name );
+  }
+  else if ( NS4 ) {
+    ele = document.layers[name];
+  }
+  else { // IE4
+    ele = document.all[name];
+  }
+
+  if ( NS4 ) {
+    ele.visibility = 'show';
+  } 
+  else { // IE4 & W3C & Mozilla
+    ele.style.visibility = 'visible';
+    if ( hide ) {
+      ele.style.display = '';
+    }
+  }
+}
+
+function makeInvisible( name, hide ) {
+  var ele;
+
+  if ( W3C ) {
+    ele = document.getElementById( name ).style;
+    ele.visibility = 'hidden';
+    if ( hide ) {
+      ele.display = 'none';
+    }
+  }
+  else if ( NS4 ) {
+    document.layers[name].visibility = 'hide';
+  }
+  else {
+    ele = document.all[name].style;
+    ele.visibility = 'hidden';
+    if ( hide ) {
+      ele.display = 'none';
+    }
+  }
+}
+
+function showTab( name ) {
+  if ( !document.getElementById ) {
+    return true;
+  }
+
+  var div, i, tab, tname;
+
+  for ( i = 0; i < tabs.length; i++ ) {
+    tname = tabs[i];
+    tab = document.getElementById( 'tab_' + tname );
+    // We might call without parameter, if so display tabfor div.
+    if ( tab && !name ) {
+      if ( tab.className == 'tabfor' ) {
+        name = tname;
+      }
+    }
+    else if ( tab ) {
+      tab.className = ( tname == name ? 'tabfor' : 'tabbak' );
+    }
+    div = document.getElementById( 'tabscontent_' + tname );
+    if ( div ) {
+      div.style.display = ( tname == name ? 'block' : 'none' );
+    }
+  }
+
+  return false;
+}
+
+function visByClass( classname, state ) {
+  var
+    inc = 0,
+    alltags = ( document.all ? document.all : document.getElementsByTagName( '*' ) );
+
+  for ( i = 0; i < alltags.length; i++ ) {
+    var str = alltags[i].className;
+
+    if ( str && str.match( classname ) ) {
+      alltags[i].style.display = ( state == 'hide' ? 'none' : '' );
+    }
+  }
+}
+
+function getScrollingPosition() {
+  var position = [0, 0];
+
+  if ( typeof window.pageYOffset != 'undefined' ) {
+    position = [
+      window.pageXOffset,
+      window.pageYOffset
+    ];
+  }
+  else if ( typeof document.documentElement.scrollTop != 'undefined'
+    && document.documentElement.scrollTop > 0 ) {
+    position = [
+      document.documentElement.scrollLeft,
+      document.documentElement.scrollTop
+    ];
+  }
+  else if ( typeof document.body.scrollTop != 'undefined' ) {
+    position = [
+      document.body.scrollLeft,
+      document.body.scrollTop
+    ];
+  }
+
+  return position;
+}
+
+//These common function are here because all the files that use them
+//also use visibility functions.
+function selectDate( day, month, year, current, evt, form ) {
+  // Get currently selected day/month/year.
+  monthobj = eval( 'document.' + form.id + '.' + month );
+  curmonth = monthobj.options[monthobj.selectedIndex].value;
+  yearobj = eval( 'document.' + form.id + '.' + year );
+  evt = ( evt ? evt : window.event );
+  var scrollingPosition = getScrollingPosition();
+
+  if ( typeof evt.pageX != 'undefined' && typeof evt.x != 'undefined' ) {
+    mX = evt.pageX;
+    mY = self.screen.availHeight - evt.pageY;
+  }
+  else {
+    mX = evt.clientX + scrollingPosition[0];
+    mY = evt.clientY + scrollingPosition[1];
+  }
+
+  mX += 40;
+  var colorWindow =
+    window.open( 'datesel.php?form=' + form.id + '&fday=' + day
+      + '&fmonth=' + month + '&fyear=' + year + '&date=' 
+      + yearobj.options[yearobj.selectedIndex].value 
+      + ( curmonth < 10 ? '0' : '' ) + curmonth + '01', 'DateSelection',
+      'width=300,height=180,scrollbars=no,toolbar=no,screenx=' + mX 
+      + ',screeny=' + mY + ',left=' + mX + ',top=' + mY );
+}
+
+function selectColor( color, evt ) {
+  mX = ( document.getElementById ? evt.clientX : evt.pageX ) + 40;
+  var colorWindow = window.open( 'colors.php?color=' + color, 'ColorSelection',
+    + 'width=390,height=365,scrollbars=0,left=' + mX
+    + ',top=100,screenx=' + mX + ',screeny=100' );
+}
+
+function valid_color( str ) {
+  return /^#[0-9a-fA-F]{3}$|^#[0-9a-fA-F]{6}$/.test( str );
+}
+
+// Updates the background-color of a table cell
+// Parameters:
+//   input - element containing the new color value
+//   target - id of sample
+function updateColor( input, target ) {
+  var // The cell to be updated.
+    colorCell = document.getElementById( target ).style.backgroundColor,
+    color = input.value;  // The new color.
+
+  if ( !valid_color( color ) ) {
+    // Color specified is invalid; use black instead.
+    colorCell = '#000000';
+    input.select();
+    input.focus();
+    alert( xlate['invalidColor'] );
+  }
+  else {
+    colorCell = color;
+  }
+}
+
+function toggle_datefields( name, ele ) {
+  if ( document.getElementById( ele.id ).checked ) {
+    makeInvisible( name );
+  }
+  else {
+    makeVisible( name );
+  }
+}
+
+function callEdit() {
+  editwin = window.open( 'edit_entry.php', 'edit_entry',
+    'width=600,height=500,resizable=yes,scrollbars=no' );
+}
