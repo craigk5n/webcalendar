9065447fd88c4a4f075465aacb92b45b918f0d37
bbannon <bbannon>
2009-12-27 14:33:02 +0000

    Removing PHP from JavaScript

--- search.php
+++ search.php
@@ -3,67 +3,70 @@
 include_once 'includes/init.php';
 // Is this user allowed to search the calendars of other users?
 $show_others = false; // show "Advanced Search"
-if ( $single_user == 'Y' )
+
+if( $single_user == 'Y' )
   $show_others = false;
 
-if ( $is_admin )
+if( $is_admin )
   $show_others = true;
-else
-if ( access_is_enabled() )
-  $show_others = access_can_access_function ( ACCESS_ADVANCED_SEARCH );
-else
-if ( $login != '__public__' && ! $is_nonuser && ! empty ( $ALLOW_VIEW_OTHER ) &&
-  $ALLOW_VIEW_OTHER == 'Y' )
+elseif( access_is_enabled() )
+  $show_others = access_can_access_function( ACCESS_ADVANCED_SEARCH );
+elseif( $login != '__public__' && ! $is_nonuser && ! empty( $ALLOW_VIEW_OTHER )
+    && $ALLOW_VIEW_OTHER == 'Y' )
   $show_others = true;
-else
-if ( $login == '__public__' && !
-  empty ( $PUBLIC_ACCESS_OTHERS ) && $PUBLIC_ACCESS_OTHERS == 'Y' )
+elseif( $login == '__public__' && ! empty( $PUBLIC_ACCESS_OTHERS )
+    && $PUBLIC_ACCESS_OTHERS == 'Y' )
   $show_others = true;
 
-$show_advanced = getValue ( 'adv', '[01]' );
+$show_advanced = getValue( 'adv', '[01]' );
 $show_advanced = $show_advanced == '1' ? '1' : '0';
-$avdStyle = array ( 'hidden', 'visible' );
-if ( access_is_enabled() &&
-  ! access_can_access_function ( ACCESS_ADVANCED_SEARCH ) )
+$avdStyle = array( 'hidden', 'visible' );
+if( access_is_enabled()
+    && ! access_can_access_function( ACCESS_ADVANCED_SEARCH ) )
   $show_advanced = false;
 
 load_user_categories();
 $selected = ' selected="selected" ';
 
-$advSearchStr = translate ( 'Advanced Search' );
-$searchStr = translate ( 'Search' );
+$advSearchStr = translate( 'Advanced Search' );
+$searchStr    = translate( 'Search' );
 $INC = array();
-if ( $show_advanced ) $INC[] = 'js/visible.php';
-if ( $show_others ) $INC[] = 'js/search.php/true';
-print_header ( $INC );
+
+if( $show_advanced )
+  $INC[] = 'js/translate.js.php';
+  $INC[] = 'js/visible.js/true',
+
+if( $show_others )
+  $INC[] = 'js/search.js/true';
 
 ob_start();
+print_header( $INC );
 
 echo '    <h2>' . ( $show_advanced ? $advSearchStr : $searchStr ) . '</h2>
     <form action="search_handler.php" method="post" id="searchformentry" '
  . 'name="searchformentry" style="margin-left: 13px;">
       <input type="hidden" name="advanced" value="' . $show_advanced . '" />
-      <table summary=""><tr><td><label for="keywordsadv">' . translate ( 'Keywords' )
+      <table summary=""><tr><td><label for="keywordsadv">' . translate( 'Keywords' )
  . ':&nbsp;</label></td>
         <td><input type="text" name="keywords" id="keywordsadv" size="30" />&nbsp;
         <input type="submit" value="' . $searchStr . '" /></td></tr>';
 
 echo '<tr height="30px"><td>&nbsp;</td><td valign="top">(' .
-  translate ( 'Enter % for all entries' ) . ')</td></tr>';
+  translate( 'Enter % for all entries' ) . ')</td></tr>';
 
-if ( is_array ( $categories ) && $show_advanced ) {
+if( is_array( $categories ) && $show_advanced ) {
   echo '
         <tr id="catfilter" style="visibility:' . $avdStyle[$show_advanced]
    . ';">
-          <td><label for="cat_filter">' . translate ( 'Categories' )
+          <td><label for="cat_filter">' . translate( 'Categories' )
    . ':</label></td>
           <td>
             <select name="cat_filter" id="cat_filter">
-              <option value=""' . $selected . '>' . translate ( 'All' )
+              <option value=""' . $selected . '>' . translate( 'All' )
    . '</option>';
 
-  foreach ( $categories as $K => $V ) {
-    if ( $K > 0 )
+  foreach( $categories as $K => $V ) {
+    if( $K > 0 )
       echo '
               <option value="' . $K . '">' . $V['cat_name'] . '</option>';
   }
@@ -73,61 +76,61 @@ if ( is_array ( $categories ) && $show_advanced ) {
           </td>
         </tr>';
 }
-if ( count ( $site_extras ) > 0 ) {
+if( count( $site_extras ) > 0 ) {
   echo '
         <tr id="extrafilter" style="visibility:' . $avdStyle[$show_advanced]
    . ';">
           <td><label for="extra_filter">'
-   . translate ( 'Include' ) . '<br />' . translate ( 'Site Extras' )
+   . translate( 'Include' ) . '<br />' . translate( 'Site Extras' )
    . ':</label></td>
           <td><input type="checkbox" name="extra_filter" value="Y" />
           </td></tr>';
 }
-if ( $show_advanced ) {
-  $dateYmd = date ( 'Ymd' );
+if( $show_advanced ) {
+  $dateYmd = date( 'Ymd' );
   echo '
         <tr id="datefilter" style="visibility:' . $avdStyle[$show_advanced]
    . ';">
-          <td><label for="date_filter">' . translate ('Filter by Date')
+          <td><label for="date_filter">' . translate('Filter by Date')
    . ':</label></td>
           <td>
             <select name="date_filter" id="date_filter" onchange="toggleDateRange()">
-              <option value="0"' . $selected . '>' . translate ( 'All Dates' )
+              <option value="0"' . $selected . '>' . translate( 'All Dates' )
    . '</option>
-              <option value="1">' . translate ( 'Past' ) . '</option>
-              <option value="2">' . translate ( 'Upcoming' ) . '</option>
-              <option value="3">' . translate ( 'Range' ) . '</option>
+              <option value="1">' . translate( 'Past' ) . '</option>
+              <option value="2">' . translate( 'Upcoming' ) . '</option>
+              <option value="3">' . translate( 'Range' ) . '</option>
             </select>
           </td>
         </tr>
         <tr id="startDate" style="visibility:hidden">
-          <td>&nbsp;&nbsp;<label>' . translate ( 'Start date' )
+          <td>&nbsp;&nbsp;<label>' . translate( 'Start date' )
    . ':</label></td>
           <td>'
    . date_selection( 'from_', $dateYmd ) . '
           </td>
         </tr>
         <tr id="endDate" style="visibility:hidden">
-          <td>&nbsp;&nbsp;<label>' . translate ( 'End date' )
+          <td>&nbsp;&nbsp;<label>' . translate( 'End date' )
    . ':</label></td>
           <td>'
    . date_selection( 'until_', $dateYmd ) . '
           </td>
         </tr>';
 }
-if ( $show_others ) {
-  $users = get_my_users ( '', 'view' );
+if( $show_others ) {
+  $users = get_my_users( '', 'view' );
   // Get non-user calendars (if enabled)
-  if ( ! empty ( $NONUSER_ENABLED ) && $NONUSER_ENABLED == 'Y' ) {
-    $nonusers = get_my_nonusers ( $login, true, 'view' );
-    $users = ( ! empty ( $NONUSER_AT_TOP ) && $NONUSER_AT_TOP == 'Y'
-      ? array_merge ( $nonusers, $users )
-      : array_merge ( $users, $nonusers ) );
+  if( ! empty( $NONUSER_ENABLED ) && $NONUSER_ENABLED == 'Y' ) {
+    $nonusers = get_my_nonusers( $login, true, 'view' );
+    $users = ( ! empty( $NONUSER_AT_TOP ) && $NONUSER_AT_TOP == 'Y'
+      ? array_merge( $nonusers, $users )
+      : array_merge( $users, $nonusers ) );
   }
-  $cnt = count ( $users );
-  if ( $cnt > 50 )
+  $cnt = count( $users );
+  if( $cnt > 50 )
     $size = 15;
-  elseif ( $cnt > 10 )
+  elseif( $cnt > 10 )
     $size = 10;
   else
     $size = $cnt;
@@ -140,12 +143,12 @@ if ( $show_others ) {
         <tr  id="adv" style="visibility:' . $avdStyle[$show_advanced]
    . ';">
           <td class="aligntop"><label for="usersadv">'
-   . translate ( 'Users' ) . ':&nbsp;</label></td>
+   . translate( 'Users' ) . ':&nbsp;</label></td>
           <td>
             <select name="users[]" id="usersadv" size="' . $size
    . '" multiple="multiple">';
 
-  for ( $i = 0; $i < $cnt; $i++ ) {
+  for( $i = 0; $i < $cnt; $i++ ) {
     echo '
               <option value="' . $users[$i]['cal_login'] . '"'
      . ( $users[$i]['cal_login'] == $login ? ' selected="selected"' : '' )
@@ -156,7 +159,7 @@ if ( $show_others ) {
             </select>'
    . ( $GROUPS_ENABLED == 'Y'
     ? '<input type="button" onclick="selectUsers()" value="'
-     . translate ( 'Select' ) . '..." />' : '' ) . '
+     . translate( 'Select' ) . '..." />' : '' ) . '
           </td>
         </tr>';
 }
