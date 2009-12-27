98ea9a94abb942cbe4aae0c369bda0fcbd39e157
bbannon <bbannon>
2009-12-27 06:35:59 +0000

    Removing PHP from JavaScript

---
+++ includes/js/users.js
@@ -0,0 +1,5 @@
+// $Id$
+
+// See the showTab function in includes/js/visible.js
+// for common code shared by all pages using the tabbed GUI.
+var tabs = ['', "users", "groups", "nonusers", "remotes"];
