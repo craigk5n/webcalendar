24fe50f17c732782c31321145e922da90432abff
bbannon <bbannon>
2012-02-01 11:00:58 +0000

    Move some JS to external file. Fix a loop bug. Better for loop.

--- includes/js/assistant_edit.js
+++ includes/js/assistant_edit.js
@@ -1,26 +1,26 @@
 // $Id$
-
-function selectUsers() {
-  // Find id of user selection object.
-  var delim = url = '',
-  dae = document.assistanteditform.elements,
-  listid = 0;
-
-  // We seem to want the last one, so let's start at the end.
-  for (var i = dae.length; i >= 0; i--) {
-    if (dae[i].name == 'users[]') {
-      listid = i;
-      break;
+jq(document).ready(function () {
+  jq(':button').click(function () {
+    // Find id of user selection object.
+    var delim = url = '',
+    dae = document.assistanteditform.elements,
+    listid = 0;
+    // We seem to want the last one, so let's start at the end.
+    for (var i = dae.length - 1; i >= 0; i--) {
+      if (dae[i].name == 'users[]') {
+        listid = i;
+        break;
+      }
     }
-  }
-
-  // add currently selected users
-  for (var i = 0, j = dae[listid].length; i < j; i++) {
-    if (dae[listid].options[i].selected) {
-      url += delim + dae[listid].options[i].value;
-      delim = ',';
+    // add currently selected users
+    for (var i = 0; dae[listid][i]; i++) {
+      if (dae[listid].options[i].selected) {
+        url += delim + dae[listid].options[i].value;
+        delim = ',';
+      }
     }
-  }
-  window.open('usersel.php?form=assistanteditform&listid=' + listid + '&users='
-     + url, 'UserSelection', 'width=500,height=500,resizable=yes,scrollbars=yes');
-}
+    window.open('usersel.php?form=assistanteditform&listid=' + listid + '&users=' + url,
+      'UserSelection', 'width=500,height=500,resizable=yes,scrollbars=yes');
+  });
+});
+// end assistant_edit.js
