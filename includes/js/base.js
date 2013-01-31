21e5e7bcb75fe8f4f599b609e7e1b019cd6c51d1
bbannon <bbannon>
2013-01-31 06:07:25 +0000

    Some new functions that I think will be useful.

--- includes/js/base.js
+++ includes/js/base.js
@@ -31,7 +31,7 @@ function addLoadListener(fn) {
     window.attachEvent('onload', fn);
   } else {
     var oldfn = window.onload;
-
+    
     if (typeof window.onload != 'function') {
       window.onload = fn;
     } else {
@@ -49,13 +49,13 @@ function attachEventListener(target, eventType, functionRef, capture) {
     target.attachEvent('on' + eventType, functionRef);
   } else {
     eventType = 'on' + eventType;
-
+    
     if (typeof target[eventType] == 'function') {
       var oldListener = target[eventType];
-
+      
       target[eventType] = function () {
         oldListener();
-
+        
         return functionRef();
       }
     } else {
@@ -64,15 +64,54 @@ function attachEventListener(target, eventType, functionRef, capture) {
   }
   return true;
 }
+// Remove whitespace, comments and other "junk" nodes from the DOM.
+// http://jspro.com/raw-javascript/removing-useless-nodes-from-the-dom/#.UL2IF65sFTg
+function clean(node) {
+  for (var n = 0; node.childNodes[n]; n++) {
+    var child = node.childNodes[n];
+    if (child.nodeType === 8 || (child.nodeType === 3 && !/\S/.test(child.nodeValue))) {
+      node.removeChild(child);
+      n--;
+    } else if (child.nodeType === 1) {
+      clean(child);
+    }
+  }
+}
+// Javascript version of PHP's "empty()".
+// Kinds of data considered to be empty:
+//  - undefined or null
+//  - a zero-length string
+//  - an array with no members
+//  - an object with no enumerable properties
+// Booleans and numbers are NEVER empty, irrespective of their value.
+// http://jspro.com/raw-javascript/testing-for-empty-values/#.UKx_6a5sFTg
+function empty(data) {
+  if (typeof(data) == 'number' || typeof(data) == 'boolean') {
+    return false;
+  }
+  if (typeof(data) == 'undefined' || data === null) {
+    return true;
+  }
+  if (typeof(data.length) != 'undefined') {
+    return data.length == 0;
+  }
+  var count = 0;
+  for (var i in data) {
+    if (data.hasOwnProperty(i)) {
+      count++;
+    }
+  }
+  return count == 0;
+}
 function getElementsByAttribute(attribute, attributeValue) {
   var elementArray = matchedArray = [];
-
+  
   elementArray = document.getElementsByTagName('*');
-
+  
   for (var i in elementArray) {
     if (attribute == 'class') {
       var pattern = new RegExp('(^| )' + attributeValue + '( |$)');
-
+      
       if (pattern.test(i.className)) {
         matchedArray[matchedArray.length] = i;
       }
@@ -93,26 +132,26 @@ if (typeof document.getElementsBySelector == 'undefined') {
   - returns an array of element objects from the current document
   matching the CSS selector. Selectors can contain element names,
   class names and ids and can be nested. For example:
-
+  
   elements = document.getElementsBySelect('div#main p a.external')
-
+  
   Will return an array of all 'a' elements with 'external' in their
   class attribute that are contained inside 'p' elements that are
   contained inside the 'div' element which has id="main".
-
+  
   New in version 0.4: Support for CSS2 and CSS3 attribute selectors:
   See http://www.w3.org/TR/css3-selectors/#attribute-selectors
-
+  
   Version 0.4 - Simon Willison, March 25th 2003
   -- Works in Phoenix 0.5, Mozilla 1.3, Opera 7, Internet Explorer 6,
   Internet Explorer 5 on Windows.
   -- Opera 7 fails.
    */
-
+  
   function getAllChildren(e) {
     return e.getElementsByTagName('*');
   }
-
+  
   document.getElementsBySelector = function (selector) {
     // Attempt to fail gracefully in lesser browsers.
     if (!document.getElementsByTagName) {
@@ -121,17 +160,17 @@ if (typeof document.getElementsBySelector == 'undefined') {
     // Split selector in to tokens.
     var tokens = selector.split(' ');
     var currentContext = [document];
-
+    
     for (var i in tokens) {
       token = i.replace(/^\s+/, '').replace(/\s+$/, '');
-
+      
       if (token.indexOf('#') > -1) {
         // Token is an ID selector.
         var bits = token.split('#');
         var tagName = bits[0],
         id = bits[1],
         element = document.getElementById(id);
-
+        
         if (tagName && element.nodeName.toLowerCase() != tagName) {
           // Tag with that ID not found, return false.
           return new Array();
@@ -145,27 +184,27 @@ if (typeof document.getElementsBySelector == 'undefined') {
         var bits = token.split('.');
         var tagName = bits[0],
         className = bits[1];
-
+        
         if (!tagName) {
           tagName = '*';
         }
         // Get elements matching tag, filter them for class selector.
         var found = [],
         foundCount = 0;
-
+        
         for (var h in currentContext) {
           var elements;
           elements = (tagName == '*'
              ? getAllChildren(h) : h.getElementsByTagName(tagName));
-
+          
           for (var j in elements) {
             found[foundCount++] = j;
           }
         }
         currentContext = [];
-
+        
         var currentContextIndex = 0;
-
+        
         for (var k in found) {
           if (k.className && k.className.match(new RegExp('\\b' + className + '\\b')))
             currentContext[currentContextIndex++] = k;
@@ -187,29 +226,29 @@ if (typeof document.getElementsBySelector == 'undefined') {
         attrName = RegExp.$2,
         attrOperator = RegExp.$3,
         attrValue = RegExp.$4;
-
+        
         if (!tagName) {
           tagName = '*';
         }
         // Grab all of the tagName elements within current context.
         var found = [],
         foundCount = 0;
-
+        
         for (var h in currentContext) {
           var elements;
-
+          
           elements = (tagName == '*'
              ? getAllChildren(h) : h.getElementsByTagName(tagName));
-
+          
           for (var j in elements) {
             found[foundCount++] = j;
           }
         }
         currentContext = [];
-
+        
         var currentContextIndex = 0,
         checkFunction; // This function will be used to filter the elements.
-
+        
         switch (attrOperator) {
         case '=': // Equality
           checkFunction = function (e) {
@@ -247,9 +286,9 @@ if (typeof document.getElementsBySelector == 'undefined') {
           };
         }
         currentContext = [];
-
+        
         var currentContextIndex = 0;
-
+        
         for (var k in found) {
           if (checkFunction(k)) {
             currentContext[currentContextIndex++] = k;
@@ -259,13 +298,13 @@ if (typeof document.getElementsBySelector == 'undefined') {
       }
       // If we get here, token is JUST an element (not a class or ID selector).
       tagName = token;
-
+      
       var found = [],
       foundCount = 0;
-
+      
       for (var h in currentContext) {
         var elements = h.getElementsByTagName(tagName);
-
+        
         for (var j in elements) {
           found[foundCount++] = j;
         }
@@ -275,20 +314,21 @@ if (typeof document.getElementsBySelector == 'undefined') {
     return currentContext;
   }
 }
-function linkFile(wher, what, desc, medi) {
-  /**
-  Link in an external file (CSS or JavaScript) from JavaScript.
-  And, load it in the background while the main program continues on.
-
-  params:
-  wher = string: Where is the file? (required)
-  what = string: 'script' for javascript (default) or 'link' for CSS.
-  desc = string: The title of an alternative stylesheet.
-  If alternative stylesheets are used,
-  the default stylesheet should be titled as 'default'.
-  medi = string: What media (screen, print, etc.).
-   */
-
+// Compare 2 integers.
+function intcmp(int1, int2) {
+  return (int1 == int2 ? 0 : (int1 < int2 ? -1 : 1))
+}
+/**
+Link in an external file (CSS or JavaScript) from JavaScript.
+And, load it in the background while the main program continues on.
+ */
+function linkFile(
+  wher, // string: Where is the file? (required)
+  what, // string: 'script' for javascript (default) or 'link' for CSS.
+  desc, // string: The title of an alternative stylesheet.
+  // If alternative stylesheets are used, the default stylesheet should be titled 'default'.
+  medi // string: What media (screen, print, etc.).
+) {
   if (typeof wher == 'undefined') {
     return;
   }
@@ -301,21 +341,21 @@ function linkFile(wher, what, desc, medi) {
   if (typeof medi == 'undefined') {
     medi = false;
   }
-
   var s = document.createElement(what);
-
-  // To make IE < 9 play nice.
+  
+  // To make IE < 9 play nice...
   // First, use setAttribute() with '' 2nd param,
   // then use DOM0 to set the real value.
   if (what == 'script') {
     s.setAttribute('src', '');
     s.src = wher;
+    document.getElementsByTagName('body')[0].appendChild(s);
   } else {
     s.setAttribute('href', '');
     s.setAttribute('rel', '');
     s.href = wher;
-    s.rel = (desc && desc != 'default' ? 'alternative ' : '') + 'stylesheet';
-
+    s.rel = (desc && desc != 'default' ? 'alternative' : '') + ' stylesheet';
+    
     if (medi) {
       s.setAttribute('media', '');
       s.media = medi;
@@ -323,64 +363,142 @@ function linkFile(wher, what, desc, medi) {
       s.setAttribute('title', '');
       s.title = desc;
     }
+    document.getElementsByTagName('head')[0].appendChild(s);
   }
-  document.getElementsByTagName('head')[0].appendChild(s);
+}
+// Has page finished loading?
+// If yes, run function.
+function loaded(i, f) {
+  if (jq('#' + i) != null) {
+    f();
+  } else if (!pageLoaded) {
+    setTimeout('loaded(\'' + i + '\',' + f + ')', 100);
+  }
+}
+// A Utility Function for Padding Strings and Numbers
+// http://jspro.com/raw-javascript/a-utility-function-fo-padding-strings-and-numbers/#.UKxyTa5sFTg
+function pad(input, length, padding) {
+  while ((input = input.toString()).length + (padding = padding.toString()).length < length) {
+    padding += padding;
+  }
+  return padding.substr(0, length - input.length) + input;
 }
 function rbetween(min, max) {
   return min + Math.floor(Math.random() * (max - min + 1));
 }
 function stopDefaultAction(event) {
   event.returnValue = false;
-
+  
   if (typeof event.preventDefault != 'undefined') {
     event.preventDefault();
   }
   return true;
 }
-function targeTo(targ, aId) {
-  /**
-  Target Links to New Window.
-   */
-
+// Emulate the C strcmp function
+// -1 if string1 comes first, 1 if string2 comes first, 0 if equal
+function strcmp(string1, string2) {
+  // Handle null values first
+  if (string1 == null && string2 == null) {
+    return 0;
+  }
+  if (string1 == null) {
+    return -1;
+  } else if (string2 == null) {
+    return 1;
+  }
+  // Compare non-null values
+  var str1 = string1.toLowerCase();
+  var str2 = string2.toLowerCase();
+  
+  if (str1 == str2) {
+    return 0;
+  }
+  for (var i = 0; str1[i] && str2[i]; i++) {
+    if (str1.charAt(i) < str2.charAt(i)) {
+      return -1;
+    } else if (str1.charAt(i) > str2.charAt(i)) {
+      return 1;
+    }
+  }
+  if (str1.length < str2.length) {
+    return -1;
+  } else if (str1.length > str2.length) {
+    return 1;
+  }
+  // Shouldn't ever reach here...
+  alert('strcmp bug! string1= "' + str1 + '", string2= "' + str2 + '"');
+}
+/**
+ * Target Links to New Window.
+ */
+function targeTo(
+  targ, // string  target=
+  aId // string  anchor ID (optional - default to all anchors)
+) {
   if (!document.getElementsByTagName) {
     return;
   }
   var a,
   anchors;
-
-  a = (typeof aId === 'undefined'
-     ? document : document.getElementById(aId));
+  
+  a = (typeof aId === 'undefined' ? document : document.getElementById(aId));
   anchors = a.getElementsByTagName('a');
-
+  
   for (var a in anchors) {
     a.setAttribute('target', '');
-    a.target = targ; // If '', turn target off.
+    a.target = targ; // If'', turn target off.
   }
 }
 // Combining functions MakeVisible (x2), MakeInvisible, showFrame and hideFrame.
 function toggleVisible(name, v, d, mom) {
   var ele;
-
+  
   if (typeof mom != 'undefined')
     ele = window.opener.document.getElementById(name).style;
   else
     ele = document.getElementById(name).style;
-
+  
   if (v == 'undefined') {
     v = (ele.display == 'visible' ? 'hidden' : 'visible');
   }
-
+  
   ele.visibility = v;
   if (d) {
     ele.display = d;
   }
 }
+/*
+Javascript < 1.8.1 and IE < 9 have no "trim()" functions,
+and javascript 1.8.1+ trimLeft(), trinRight() and trim() only handles whitespace.
+So, I "borrowed" these from
+http://jspro.com/raw-javascript/trimming-strings-in-javascript/#.UKxuUq5sFTg
+ */
+// Remove charlist from beginning of String.
+// Default is whitespace.
+String.prototype.Ltrim = function (charlist) {
+  if (charlist === undefined) {
+    charlist = '\\s';
+  }
+  return this.replace(new RegExp('^[' + charlist + '] + '), '');
+};
+// Remove charlist from end of String.
+// Default is whitespace.
+String.prototype.Rtrim = function (charlist) {
+  if (charlist === undefined) {
+    charlist = '\\s';
+  }
+  return this.replace(new RegExp('[' + charlist + '] + $'), '');
+};
+String.prototype.Btrim = function (charlist) {
+  return this.Ltrim(charlist).Rtrim(charlist);
+};
+// end trim()s
 function wc_getCookie(Name) {
   var cookies = document.cookie.split(';');
-
+  
   for (var i in cookies) {
     var crumbs = i.split('=');
-
+    
     if (crumbs[0] == Name) {
       return crumbs[1];
     }
