7598e4f2bc4b2ee2bf10d2d97fdb9ee03d9b8987
Craig Knudsen <craig@k5n.us>
2022-04-21 13:51:09 -0400

    Moved rss-style.css into includes/css dir

--- rss_unapproved.php
+++ rss_unapproved.php
@@ -127,7 +127,7 @@ $descr = $appStr . ' - ' . translate ( 'Unapproved Entries' ) . ' - ' .
 // header ( 'Content-type: application/rss+xml');
 header ( 'Content-type: text/xml' );
 echo '<?xml version="1.0" encoding="' . $charset . '"?>
-<?xml-stylesheet href="rss-style.css" ?>
+<?xml-stylesheet href="includes/css/rss-style.css" ?>
 <rss version="2.0" xml:lang="' . $lang . '">
   <channel>
     <title><![CDATA[' . $appStr . ']]></title>
