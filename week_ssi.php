91938380d206e4e0e5b37da8814aa923dcc7961f
Bruce Bannon <brucebannon212@gmail.com>
2024-03-25 17:13:14 -0600

    tag not closed

--- week_ssi.php
+++ week_ssi.php
@@ -79,9 +79,10 @@ for ( $i = 0; $i < 7; $i++ ) {
    . date ( 'd', $days[$i] ) . '</th>';
 
   $tmpOut2 .= '
-              <td style="vertical-align: top; width: 75px; height: 75px; '
-   . 'background: ' . ( $date == date ( 'Ymd' ) ? $TODAYCELLBG : $CELLBG )
-   . print_date_entries ( $date, $login, true, true ) . '&nbsp;</td>';
+              <td style="inline-size: 75px; block-size: 75px; background: ' .
+    ( $date == date ( 'Ymd' ) ? $TODAYCELLBG : $CELLBG ) .
+    '; vertical-align: top;">' .
+    print_date_entries ( $date, $login, true, true ) . '&nbsp;</td>';
 }
 
 echo '
